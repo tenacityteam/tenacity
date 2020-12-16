fc82b1d7cda7a113c86e004cd165f6b4db0f251f
Paul Licameli <paul.licameli@audacityteam.org>
2020-12-16 14:10:44 -0500

    Define abstract playback policy object; create and destroy...
    ... objects of concrete subclasses, in proper places.
    
    No methods moved into it yet, but the intent is to eliminate the play mode
    enumeration, and move logic specific to scrubbing, looping, cut preview, time
    warping, etc. cases, out of AudioIO.cpp and PlaybackSchedule.cpp.
    
    (cherry picked from commit c8a2aa8e3099c1aa15307751a5ec7d59e40f5e75;
    modified for Tenacity)
    Signed-off-by: Avery King <avery98@pm.me>

--- src/tracks/ui/Scrubbing.cpp
+++ src/tracks/ui/Scrubbing.cpp
@@ -25,6 +25,7 @@ Paul Licameli split from TrackPanel.cpp
 #include "../../ProjectWindows.h"
 #include "../../ProjectSettings.h"
 #include "ProjectStatus.h"
+#include "../../ScrubState.h"
 #include "ViewInfo.h"
 #include "../../WaveTrack.h"
 #include "../../prefs/PlaybackPrefs.h"
@@ -346,6 +347,18 @@ void Scrubber::MarkScrubStart(
    mCancelled = false;
 }
 
+static AudioIOStartStreamOptions::PolicyFactory
+ScrubbingPlaybackPolicyFactory()
+{
+   return [](const AudioIOStartStreamOptions &options)
+      -> std::unique_ptr<PlaybackPolicy>
+   {
+      return
+         std::make_unique<ScrubbingPlaybackPolicy>(*options.pScrubbingOptions);
+   };
+}
+
+
 #ifdef EXPERIMENTAL_SCRUBBING_SUPPORT
 // Assume xx is relative to the left edge of TrackPanel!
 bool Scrubber::MaybeStartScrubbing(wxCoord xx)
@@ -463,6 +476,7 @@ bool Scrubber::MaybeStartScrubbing(wxCoord xx)
                   StopPolling();
             });
 
+            options.policyFactory = ScrubbingPlaybackPolicyFactory();
             mScrubToken =
                projectAudioManager.PlayPlayRegion(
                   SelectedRegion(time0, time1), options,
@@ -566,6 +580,7 @@ bool Scrubber::StartSpeedPlay(double speed, double time0, double time1)
    mScrubSpeedDisplayCountdown = 0;
    // Aim to stop within 20 samples of correct position.
    double stopTolerance = 20.0 / options.rate;
+   options.policyFactory = ScrubbingPlaybackPolicyFactory();
    mScrubToken =
       // Reduce time by 'stopTolerance' fudge factor, so that the Play will stop.
       projectAudioManager.PlayPlayRegion(
@@ -633,6 +648,7 @@ bool Scrubber::StartKeyboardScrubbing(double time0, bool backwards)
          StopPolling();
    });
 
+   options.policyFactory = ScrubbingPlaybackPolicyFactory();
    mScrubToken =
       ProjectAudioManager::Get(*mProject).PlayPlayRegion(
          SelectedRegion(time0, backwards ? mOptions.minTime : mOptions.maxTime),
