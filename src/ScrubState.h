70d480a8b292474cf5ad2602ebe5460e5469aaf7
Paul Licameli <paul.licameli@audacityteam.org>
2020-12-17 07:04:31 -0500

    New files src/ScrubState.*, nothing in them yet
    (cherry picked from commit ee5bde4d93ca6a657987d6aa93c6763a81011dc2;
    modified for Tenacity)
    Signed-off-by: Avery King <avery98@pm.me>

---
+++ src/ScrubState.h
@@ -0,0 +1,13 @@
+/*!********************************************************************
+ 
+ Tenacity
+ 
+ @file ScrubState.h
+ 
+ Paul Licameli split from AudioIO.cpp
+ 
+ **********************************************************************/
+
+#ifndef __AUDACITY_SCRUB_STATE__
+#define __AUDACITY_SCRUB_STATE__
+#endif
