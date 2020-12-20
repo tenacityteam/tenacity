9f6ebdd058aaecc5ff50ab617676bc37019b71fc
Paul Licameli <paul.licameli@audacityteam.org>
2020-12-19 19:51:54 -0500

    Remove AudioIOStartStreamOptions::playLooped
    (cherry picked from commit dc3cd51ca43c05632a5d9ecf4c31a301122388c5;
    modified for Tenacity)
    Signed-off-by: Avery King <avery98@pm.me>

--- src/ProjectAudioManager.cpp
+++ src/ProjectAudioManager.cpp
@@ -123,7 +123,7 @@ int ProjectAudioManager::PlayPlayRegion(const SelectedRegion &selectedRegion,
    double t1 = selectedRegion.t1();
    // SelectedRegion guarantees t0 <= t1, so we need another boolean argument
    // to indicate backwards play.
-   const bool looped = options.playLooped;
+   const bool looped = (mode == PlayMode::loopedPlay);
 
    if (backwards)
       std::swap(t0, t1);
@@ -296,13 +296,12 @@ void ProjectAudioManager::PlayCurrentRegion(bool looped /* = false */,
 
       const auto &playRegion = ViewInfo::Get( *p ).playRegion;
 
-      auto options = DefaultPlayOptions( *p );
-      options.playLooped = looped;
+      auto options = DefaultPlayOptions( *p, looped );
       if (cutpreview)
          options.envelope = nullptr;
       auto mode =
          cutpreview ? PlayMode::cutPreviewPlay
-         : options.playLooped ? PlayMode::loopedPlay
+         : looped ? PlayMode::loopedPlay
          : PlayMode::normalPlay;
       PlayPlayRegion(SelectedRegion(playRegion.GetStart(), playRegion.GetEnd()),
                      options,
@@ -993,7 +992,7 @@ const ReservedCommandFlag&
    }; return flag; }
 
 AudioIOStartStreamOptions
-DefaultPlayOptions( TenacityProject &project )
+DefaultPlayOptions( TenacityProject &project, bool looped )
 {
    auto &projectAudioIO = ProjectAudioIO::Get( project );
    AudioIOStartStreamOptions options { project.shared_from_this(),
@@ -1003,6 +1002,10 @@ DefaultPlayOptions( TenacityProject &project )
    auto timeTrack = *TrackList::Get( project ).Any<TimeTrack>().begin();
    options.envelope = timeTrack ? timeTrack->GetEnvelope() : nullptr;
    options.listener = ProjectAudioManager::Get( project ).shared_from_this();
+   
+   if (looped)
+      options.policyFactory = []() -> std::unique_ptr<PlaybackPolicy> {
+         return std::make_unique<LoopingPlaybackPolicy>(); };
    return options;
 }
 
