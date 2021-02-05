e1554491bb5ba83eaf6b751e0328c83a9f4429b1
Paul Licameli <paul.licameli@audacityteam.org>
2021-02-05 09:11:41 -0500

    Envelope does not depend on ZoomInfo
    (cherry picked from commit 5fa44ad2bb1b3d2f3ee17b2a1624e5f1222296c4)
    Signed-off-by: Avery King <avery98@pm.me>

--- src/tracks/playabletrack/wavetrack/ui/WaveformView.cpp
+++ src/tracks/playabletrack/wavetrack/ui/WaveformView.cpp
@@ -707,7 +707,7 @@ void DrawClipWaveform(TrackPanelDrawingContext &context,
 
    std::vector<double> vEnv(mid.width);
    double *const env = &vEnv[0];
-   Envelope::GetValues( *clip->GetEnvelope(),
+   CommonTrackView::GetEnvelopeValues( *clip->GetEnvelope(),
        tOffset,
 
         // PRL: change back to make envelope evaluate only at sample times
@@ -787,7 +787,7 @@ void DrawClipWaveform(TrackPanelDrawingContext &context,
          if (!showIndividualSamples) {
             std::vector<double> vEnv2(rectPortion.width);
             double *const env2 = &vEnv2[0];
-            Envelope::GetValues( *clip->GetEnvelope(),
+            CommonTrackView::GetEnvelopeValues( *clip->GetEnvelope(),
                 tOffset,
 
                  // PRL: change back to make envelope evaluate only at sample times
