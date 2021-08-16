8c32e585fd90360f4cd640e46e0957f955e7375e
Vitaly Sverchinsky <sverchinsky@gmail.com>
2021-08-16 19:21:19 +0300

    Clip name drawing
    ===============================================================================
    
    Cherry picked from Audacity commit
    6bf364af39e8b876c0cbc8a522f72ac1a4cd050f. Modified in order to apply to
    Saucedacity.
    
    Signed-off-by: Avery King <avery98@pm.me>

--- src/TrackArtist.h
+++ src/TrackArtist.h
@@ -42,7 +42,7 @@ namespace TrackArt {
    static constexpr int ClipFrameRadius{ 6 };
 
    SAUCEDACITY_DLL_API
-   void DrawClipAffordance(wxDC& dc, const wxRect& affordanceRect, bool highlight = false, bool selected = false);
+   void DrawClipAffordance(wxDC& dc, const wxRect& affordanceRect, const wxString& title, bool highlight = false, bool selected = false);
 
    SAUCEDACITY_DLL_API
    void DrawClipEdges(wxDC& dc, const wxRect& clipRect, bool selected = false);
@@ -66,6 +66,9 @@ namespace TrackArt {
    SAUCEDACITY_DLL_API
    void DrawNegativeOffsetTrackArrows( TrackPanelDrawingContext &context,
                                        const wxRect & rect );
+
+   SAUCEDACITY_DLL_API
+   wxString TruncateText(wxDC& dc, const wxString& text, const int maxWidth);
 }
 
 class SAUCEDACITY_DLL_API TrackArtist final : private PrefsListener {
