f2385a5d7a1af3236592f9db74085abeae5f11e4
Dmitry Vedenko <dmitry@crsib.me>
2021-11-09 14:15:41 +0300

    Decode now serialized to UTF8 into MemoryStream
    =======================================================================
    
    Cherry-picked from Audacity commit f1886cc. Modified for Tenacity.
    
    Signed-off-by: Avery King <avery98@pm.me>

--- src/ProjectFileIO.cpp
+++ src/ProjectFileIO.cpp
@@ -1778,7 +1778,6 @@ bool ProjectFileIO::LoadProject(const FilePath &fileName, bool ignoreAutosave)
       return false;
    }
 
-   wxString project;
    wxMemoryBuffer buffer;
    bool usedAutosave = true;
 
@@ -1811,8 +1810,9 @@ bool ProjectFileIO::LoadProject(const FilePath &fileName, bool ignoreAutosave)
    }
    else
    {
-      project = ProjectSerializer::Decode(buffer);
-      if (project.empty())
+      MemoryStream stream = ProjectSerializer::Decode(buffer);
+
+      if (stream.IsEmpty())
       {
          SetError(XO("Unable to decode project document"));
 
@@ -1822,7 +1822,7 @@ bool ProjectFileIO::LoadProject(const FilePath &fileName, bool ignoreAutosave)
       XMLFileReader xmlFile;
 
       // Load 'er up
-      success = xmlFile.ParseString(this, project);
+      success = xmlFile.ParseMemoryStream(this, stream);
       if (!success)
       {
          SetError(
