e066175005308346b2a8c92a9262a5dbca9a13bc
Avery King <avery98@pm.me>
2021-12-22 11:41:18 -0800

    Changed name to Saucedacity in log

--- libraries/lib-files/AudacityLogger.cpp
+++ libraries/lib-files/AudacityLogger.cpp
@@ -81,7 +81,7 @@ void AudacityLogger::DoLogText(const wxString & str)
 
       TimeStamp(&stamp);
 
-      mBuffer << stamp << _TS("Audacity ") << AUDACITY_VERSION_STRING << wxT("\n");
+      mBuffer << stamp << _TS("Saucedacity ") << AUDACITY_VERSION_STRING << wxT("\n");
    }
 
    mBuffer << str << wxT("\n");
