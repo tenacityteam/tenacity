f4e3d153b279fb296ad24fa94dd04bb10cde0549
Avery King <avery98@pm.me>
2022-04-30 11:08:54 -0700

    updated Docker build environments (ubuntu-focal)

--- linux/ubuntu-focal/entrypoint.sh
+++ linux/ubuntu-focal/entrypoint.sh
@@ -2,11 +2,10 @@
 
 conan --version
 
-if [ ! -d "audacity" ]
+if [ ! -d "saucedacity" ]
 then
-    git clone https://github.com/audacity/audacity
+    git clone https://github.com/saucedacity/saucedacity
 fi
-
 mkdir -p build
 
 cd build
@@ -14,33 +13,33 @@ cd build
 cmake_options=(
     -G "Unix Makefiles"
     -DCMAKE_BUILD_TYPE=Release
-    -Daudacity_lib_preference=system # Change the libs default to 'system'
-    -Daudacity_obey_system_dependencies=On # And force it!
-    -Daudacity_use_wxwidgets=local # wxWidgets 3.1 is not available
-    -Daudacity_use_expat=system
-    -Daudacity_use_lame=system
-    -Daudacity_use_sndfile=system
-    -Daudacity_use_soxr=system
-    -Daudacity_use_portaudio=local # System portaudio is not yet usable
-    -Daudacity_use_sqlite=local # We prefer using the latest version of SQLite
-    -Daudacity_use_ffmpeg=loaded
-    -Daudacity_use_id3tag=system # This library has bugs, that are fixed in *local* version
-    -Daudacity_use_mad=system # This library has bugs, that are fixed in *local* version
-    -Daudacity_use_nyquist=local # This library is not available
-    -Daudacity_use_vamp=local # The dev package for this library is not available
-    -Daudacity_use_ogg=system 
-    -Daudacity_use_vorbis=system
-    -Daudacity_use_flac=system
-    -Daudacity_use_lv2=system
-    -Daudacity_use_midi=system
-    -Daudacity_use_portmixer=local # This library is not available
-    -Daudacity_use_portsmf=system
-    -Daudacity_use_sbsms=local # We prefer using the latest version of sbsms
-    -Daudacity_use_soundtouch=system
-    -Daudacity_use_twolame=system
+    -Dsaucedacity_lib_preference=system # Change the libs default to 'system'
+    -Dsaucedacity_obey_system_dependencies=On # And force it!
+    -Dsaucedacity_use_wxwidgets=local # wxWidgets 3.1 is not available
+    -Dsaucedacity_use_expat=system
+    -Dsaucedacity_use_lame=system
+    -Dsaucedacity_use_sndfile=system
+    -Dsaucedacity_use_soxr=system
+    -Dsaucedacity_use_portaudio=local # System portaudio is not yet usable
+    -Dsaucedacity_use_sqlite=local # We prefer using the latest version of SQLite
+    -Dsaucedacity_use_ffmpeg=loaded
+    -Dsaucedacity_use_id3tag=system # This library has bugs, that are fixed in *local* version
+    -Dsaucedacity_use_mad=system # This library has bugs, that are fixed in *local* version
+    -Dsaucedacity_use_nyquist=local # This library is not available
+    -Dsaucedacity_use_vamp=local # The dev package for this library is not available
+    -Dsaucedacity_use_ogg=system 
+    -Dsaucedacity_use_vorbis=system
+    -Dsaucedacity_use_flac=system
+    -Dsaucedacity_use_lv2=system
+    -Dsaucedacity_use_midi=system
+    -Dsaucedacity_use_portmixer=local # This library is not available
+    -Dsaucedacity_use_portsmf=system
+    -Dsaucedacity_use_sbsms=local # We prefer using the latest version of sbsms
+    -Dsaucedacity_use_soundtouch=system
+    -Dsaucedacity_use_twolame=system
 )
 
-cmake "${cmake_options[@]}" ../audacity
+cmake "${cmake_options[@]}" ../saucedacity
 
 exit_status=$?
 
