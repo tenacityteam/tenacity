alex74747/audacity

Macros to simplify usage of class template AttachedVirtualFunction ...
... And allow correct linkage when overrides are defined in dynamically loaded
modules

src/tracks/ui/TimeShiftHandle.h
@@ -192,6 +192,7 @@ private:
 struct MakeTrackShifterTag;
 using MakeTrackShifter = AttachedVirtualFunction<
    MakeTrackShifterTag, std::unique_ptr<TrackShifter>, Track, AudacityProject&>;
+DECLARE_EXPORTED_ATTACHED_VIRTUAL(AUDACITY_DLL_API, MakeTrackShifter);
 
 class ViewInfo;
 

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit
48217d5acf5ea8f4ca44c184992ef5150cd7e157. Modified for Saucedacity.
Signed-off-by: <avery98@pm.me>

src/tracks/ui/TimeShiftHandle.h
@@ -198,6 +198,7 @@ private:
 struct MakeTrackShifterTag;
 using MakeTrackShifter = AttachedVirtualFunction<
    MakeTrackShifterTag, std::unique_ptr<TrackShifter>, Track, SaucedacityProject&>;
+DECLARE_EXPORTED_ATTACHED_VIRTUAL(SAUCEDACITY_DLL_API, MakeTrackShifter);
 
 class ViewInfo;
 