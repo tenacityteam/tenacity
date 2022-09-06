Paul-Licameli/audacity

New library for registries

libraries/lib-registries/Registry.cpp
@@ -14,7 +14,7 @@ Paul Licameli split from Menus.cpp
 
 #include <wx/log.h>
 
-#include "widgets/AudacityMessageBox.h"
+#include "BasicUI.h"
 
 namespace {
 
@@ -174,7 +174,7 @@ using Path = std::vector< Identifier >;
         wxLogDebug( msg.Translation() );
 #ifdef IS_ALPHA
         // user-visible message
-        AudacityMessageBox( msg );
+        BasicUI::ShowMessageBox( msg );
 #endif
      }
    }

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit
ed3e4de17be2ba80de3a3bf7c42c2b4bf663015d. Modified to apply to
Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

libraries/lib-registries/Registry.cpp
@@ -14,7 +14,7 @@ Paul Licameli split from Menus.cpp
 
 #include <wx/log.h>
 
-#include "widgets/AudacityMessageBox.h"
+#include "BasicUI.h"
 
 namespace {
 
@@ -174,7 +174,7 @@ using Path = std::vector< Identifier >;
         wxLogDebug( msg.Translation() );
 #ifdef IS_ALPHA
         // user-visible message
-        AudacityMessageBox( msg );
+        GenericUI::ShowMessageBox( msg );
 #endif
      }
    }