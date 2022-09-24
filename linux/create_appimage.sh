audacity/audacity

Workaround for AppImageLauncher not handling env correctly

linux/create_appimage.sh
@@ -79,6 +79,7 @@ linuxdeploy --list-plugins
 # Create symlinks
 #============================================================================
 
+sed -i 's|env UBUNTU_MENUPROXY=0 ||' "${appdir}/share/applications/audacity.desktop"
 ln -sf --no-dereference . "${appdir}/usr"
 ln -sf share/applications/audacity.desktop "${appdir}/audacity.desktop"
 ln -sf share/icons/hicolor/scalable/apps/audacity.svg "${appdir}/audacity.svg"

tenacityteam/tenacity

===============================================================================
Cherry picked from Audacity commit
477c292085f31fcd3785544cec4b46a3dda1f2ae. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

linux/create_appimage.sh
@@ -81,6 +81,7 @@ linuxdeploy --list-plugins
 # Create symlinks
 #============================================================================
 
+sed -i 's|env UBUNTU_MENUPROXY=0 ||' "${appdir}/share/applications/saucedacity.desktop"
 ln -sf --no-dereference . "${appdir}/usr"
 ln -sf share/applications/saucedacity.desktop "${appdir}/saucedacity.desktop"
 ln -sf share/icons/hicolor/scalable/apps/saucedacity.svg "${appdir}/saucedacity.svg"