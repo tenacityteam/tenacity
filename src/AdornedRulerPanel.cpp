audacity/audacity

TrackPanelCell::DoContextMenu takes const wxPoint *

src/AdornedRulerPanel.cpp
@@ -425,7 +425,7 @@ public:
 
    unsigned DoContextMenu
       (const wxRect &rect,
-       wxWindow *pParent, wxPoint *pPosition, AudacityProject*) final override
+       wxWindow *pParent, const wxPoint *pPosition, AudacityProject*) final
    {
       (void)pParent;// Compiler food
       (void)rect;// Compiler food

src/TrackPanelCell.cpp
@@ -47,7 +47,7 @@ unsigned TrackPanelCell::HandleWheelRotation
 }
 
 unsigned TrackPanelCell::DoContextMenu
-   (const wxRect &, wxWindow*, wxPoint *, AudacityProject*)
+   (const wxRect &, wxWindow*, const wxPoint *, AudacityProject*)
 {
    return RefreshCode::RefreshNone;
 }

src/TrackPanelCell.h
@@ -109,7 +109,7 @@ public:
    // Default implementation does nothing
    virtual unsigned DoContextMenu
       (const wxRect &rect,
-       wxWindow *pParent, wxPoint *pPosition, AudacityProject *pProject);
+       wxWindow *pParent, const wxPoint *pPosition, AudacityProject *pProject);
 
    // Return value is a bitwise OR of RefreshCode values
    // Default skips the event and does nothing

src/tracks/ui/CommonTrackControls.cpp
@@ -276,7 +276,8 @@ void TrackMenuTable::OnMoveTrack(wxCommandEvent &event)
 }
 
 unsigned CommonTrackControls::DoContextMenu(
-   const wxRect &rect, wxWindow *pParent, wxPoint *, AudacityProject *pProject)
+   const wxRect &rect, wxWindow *pParent, const wxPoint *,
+   AudacityProject *pProject)
 {
    using namespace RefreshCode;
    wxRect buttonRect;

tenacityteam/tenacity

===============================================================================
Cherry picked from Audacity commit
243fcfc659ee140bfb04c6de4476feedac6b1d3a. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

src/AdornedRulerPanel.cpp
@@ -427,7 +427,7 @@ public:
 
    unsigned DoContextMenu
       (const wxRect &rect,
-       wxWindow *pParent, wxPoint *pPosition, SaucedacityProject*) final override
+       wxWindow *pParent, const wxPoint *pPosition, SaucedacityProject*) final
    {
       (void)pParent;// Compiler food
       (void)rect;// Compiler food

src/TrackPanelCell.cpp
@@ -47,7 +47,7 @@ unsigned TrackPanelCell::HandleWheelRotation
 }
 
 unsigned TrackPanelCell::DoContextMenu
-   (const wxRect &, wxWindow*, wxPoint *, SaucedacityProject*)
+   (const wxRect &, wxWindow*, const wxPoint *, SaucedacityProject*)
 {
    return RefreshCode::RefreshNone;
 }

src/TrackPanelCell.h
@@ -109,7 +109,7 @@ public:
    // Default implementation does nothing
    virtual unsigned DoContextMenu
       (const wxRect &rect,
-       wxWindow *pParent, wxPoint *pPosition, SaucedacityProject *pProject);
+       wxWindow *pParent, const wxPoint *pPosition, SaucedacityProject *pProject);
 
    // Return value is a bitwise OR of RefreshCode values
    // Default skips the event and does nothing

src/tracks/ui/CommonTrackControls.cpp
@@ -275,7 +275,8 @@ void TrackMenuTable::OnMoveTrack(wxCommandEvent &event)
 }
 
 unsigned CommonTrackControls::DoContextMenu(
-   const wxRect &rect, wxWindow *pParent, wxPoint *, SaucedacityProject *pProject)
+   const wxRect &rect, wxWindow *pParent, const wxPoint *,
+   SaucedacityProject *pProject)
 {
    using namespace RefreshCode;
    wxRect buttonRect;