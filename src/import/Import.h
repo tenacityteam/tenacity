audacity/audacity

Use lib-ffmpeg-support

src/import/Import.h
@@ -211,4 +211,6 @@ private:
    DECLARE_EVENT_TABLE()
 };
 
+extern AUDACITY_DLL_API BoolSetting NewImportingSession;
+
 #endif

tenacityteam/tenacity

Cherry picked from Audacity commit
1040ce4a621f646e3dfe24e016a61aefb85f2cd3. Modifications were made to
retrofit lib-ffmpeg-support to Saucedacity's code base and to better
shape Saucedacity's code base around lib-ffmpeg-support.
Signed-off-by: Avery King <avery98@pm.me>

src/import/Import.h
@@ -211,4 +211,6 @@ private:
    DECLARE_EVENT_TABLE()
 };
 
+extern SAUCEDACITY_DLL_API BoolSetting NewImportingSession;
+
 #endif