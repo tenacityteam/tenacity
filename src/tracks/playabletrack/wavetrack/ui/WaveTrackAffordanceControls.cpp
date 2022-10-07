audacity/audacity

Context menu item for renaming of clips

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.cpp
@@ -338,9 +338,15 @@ unsigned WaveTrackAffordanceControls::KeyDown(wxKeyEvent& event, ViewInfo& viewI
             SelectNextClip(viewInfo, project, event.GetModifiers() != wxMOD_SHIFT);
         } break;
         case WXK_NUMPAD_ENTER:
-        case WXK_RETURN:
-            StartEditSelectedClipName(viewInfo, project);
+        case WXK_RETURN: {
+            const auto pred = [&viewInfo](const WaveClip& clip) {
+            return clip.GetStartTime() == viewInfo.selectedRegion.t0() &&
+                clip.GetEndTime() == viewInfo.selectedRegion.t1();
+            };
+            if (project)
+               StartEditNameOfMatchingClip(*project, pred);
             break;
+        }
         }
     }
     return RefreshCode::RefreshCell;
@@ -443,7 +449,8 @@ bool WaveTrackAffordanceControls::SelectNextClip(ViewInfo& viewInfo, AudacityPro
     return true;
 }
 
-bool WaveTrackAffordanceControls::StartEditSelectedClipName(ViewInfo& viewInfo, AudacityProject* project)
+bool WaveTrackAffordanceControls::StartEditNameOfMatchingClip(
+    AudacityProject &project, std::function<bool(WaveClip&)> test )
 {
     //Attempts to invoke name editing if there is a selected clip
     auto waveTrack = std::dynamic_pointer_cast<WaveTrack>(FindTrack());
@@ -451,14 +458,12 @@ bool WaveTrackAffordanceControls::StartEditSelectedClipName(ViewInfo& viewInfo, 
         return false;
     auto clips = waveTrack->GetClips();
 
-    auto it = std::find_if(clips.begin(), clips.end(), [&](const std::shared_ptr<WaveClip>& clip) {
-        return clip->GetStartTime() == viewInfo.selectedRegion.t0() &&
-            clip->GetEndTime() == viewInfo.selectedRegion.t1();
-        });
+    auto it = std::find_if(clips.begin(), clips.end(),
+      [&](auto pClip){ return pClip && test && test(*pClip); });
     if (it != clips.end())
     {
         mFocusClip = *it;
-        return StartEditClipName(project);
+        return StartEditClipName(&project);
     }
     return false;
 }

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -73,11 +73,15 @@ public:
     void OnTextModified(AudacityProject* project, const wxString& text) override;
     void OnTextContextMenu(AudacityProject* project, const wxPoint& position) override;
 
+    bool StartEditNameOfMatchingClip( AudacityProject &project,
+        std::function<bool(WaveClip&)> test /*!<
+            Edit the first clip in the track's list satisfying the test */
+    );
+
 private:
     void OnTrackChanged(TrackListEvent& evt);
 
     bool SelectNextClip(ViewInfo& viewInfo, AudacityProject* project, bool forward);
-    bool StartEditSelectedClipName(ViewInfo& viewInfo, AudacityProject* project);
 
     std::shared_ptr<TextEditHelper> MakeTextEditHelper(const wxString& text);
 };

tenacityteam/tenacity

Cherry-picked from Audacity commit d3dc948. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.cpp
@@ -341,9 +341,15 @@ unsigned WaveTrackAffordanceControls::KeyDown(wxKeyEvent& event, ViewInfo& viewI
             SelectNextClip(viewInfo, project, event.GetModifiers() != wxMOD_SHIFT);
         } break;
         case WXK_NUMPAD_ENTER:
-        case WXK_RETURN:
-            StartEditSelectedClipName(viewInfo, project);
+        case WXK_RETURN: {
+            const auto pred = [&viewInfo](const WaveClip& clip) {
+            return clip.GetStartTime() == viewInfo.selectedRegion.t0() &&
+                clip.GetEndTime() == viewInfo.selectedRegion.t1();
+            };
+            if (project)
+               StartEditNameOfMatchingClip(*project, pred);
             break;
+        }
         }
     }
     return RefreshCode::RefreshCell;
@@ -446,7 +452,8 @@ bool WaveTrackAffordanceControls::SelectNextClip(ViewInfo& viewInfo, Saucedacity
     return true;
 }
 
-bool WaveTrackAffordanceControls::StartEditSelectedClipName(ViewInfo& viewInfo, SaucedacityProject* project)
+bool WaveTrackAffordanceControls::StartEditNameOfMatchingClip(
+    SaucedacityProject &project, std::function<bool(WaveClip&)> test )
 {
     //Attempts to invoke name editing if there is a selected clip
     auto waveTrack = std::dynamic_pointer_cast<WaveTrack>(FindTrack());
@@ -454,14 +461,12 @@ bool WaveTrackAffordanceControls::StartEditSelectedClipName(ViewInfo& viewInfo, 
         return false;
     auto clips = waveTrack->GetClips();
 
-    auto it = std::find_if(clips.begin(), clips.end(), [&](const std::shared_ptr<WaveClip>& clip) {
-        return clip->GetStartTime() == viewInfo.selectedRegion.t0() &&
-            clip->GetEndTime() == viewInfo.selectedRegion.t1();
-        });
+    auto it = std::find_if(clips.begin(), clips.end(),
+      [&](auto pClip){ return pClip && test && test(*pClip); });
     if (it != clips.end())
     {
         mFocusClip = *it;
-        return StartEditClipName(project);
+        return StartEditClipName(&project);
     }
     return false;
 }

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -73,11 +73,15 @@ public:
     void OnTextModified(SaucedacityProject* project, const wxString& text) override;
     void OnTextContextMenu(SaucedacityProject* project, const wxPoint& position) override;
 
+    bool StartEditNameOfMatchingClip( SaucedacityProject &project,
+        std::function<bool(WaveClip&)> test /*!<
+            Edit the first clip in the track's list satisfying the test */
+    );
+
 private:
     void OnTrackChanged(TrackListEvent& evt);
 
     bool SelectNextClip(ViewInfo& viewInfo, SaucedacityProject* project, bool forward);
-    bool StartEditSelectedClipName(ViewInfo& viewInfo, SaucedacityProject* project);
 
     std::shared_ptr<TextEditHelper> MakeTextEditHelper(const wxString& text);
 };