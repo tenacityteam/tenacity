audacity/audacity

Text edit shortcuts available when editing clip name

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.cpp
@@ -397,8 +397,9 @@ const ReservedCommandFlag &SomeClipIsSelectedFlag()
 
 unsigned WaveTrackAffordanceControls::CaptureKey(wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent, AudacityProject* project)
 {
-    if (!mTextEditHelper)
-       // Handle the event if we are already editing clip name text...
+    if (!mTextEditHelper 
+       || !mTextEditHelper->CaptureKey(event.GetKeyCode(), event.GetModifiers()))
+       // Handle the event if it can be processed by the text editor (if any)
        event.Skip();
     return RefreshCode::RefreshNone;
 }
@@ -410,10 +411,11 @@ unsigned WaveTrackAffordanceControls::KeyDown(wxKeyEvent& event, ViewInfo& viewI
     
     if (mTextEditHelper)
     {
-        mTextEditHelper->OnKeyDown(keyCode, event.GetModifiers(), project);
-        if (!TextEditHelper::IsGoodEditKeyCode(keyCode))
-            event.Skip();
-        return RefreshCode::RefreshCell;
+       if (!mTextEditHelper->OnKeyDown(keyCode, event.GetModifiers(), project) 
+          && !TextEditHelper::IsGoodEditKeyCode(keyCode))
+          event.Skip();
+
+       return RefreshCode::RefreshCell;
     }
     return RefreshCode::RefreshNone;
 }
@@ -503,6 +505,46 @@ bool WaveTrackAffordanceControls::StartEditNameOfMatchingClip(
         return StartEditClipName(&project);
     }
     return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextCopy(AudacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->CopySelectedText(project);
+      return true;
+   }
+   return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextCut(AudacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->CutSelectedText(project);
+      return true;
+   }
+   return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextPaste(AudacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->PasteSelectedText(project);
+      return true;
+   }
+   return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextSelect(AudacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->SelectAll();
+      return true;
+   }
+   return false;
 }
 
 unsigned WaveTrackAffordanceControls::OnAffordanceClick(const TrackPanelMouseEvent& event, AudacityProject* project)

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -87,6 +87,11 @@ public:
 
     unsigned OnAffordanceClick(const TrackPanelMouseEvent& event, AudacityProject* project);
 
+    bool OnTextCopy(AudacityProject& project);
+    bool OnTextCut(AudacityProject& project);
+    bool OnTextPaste(AudacityProject& project);
+    bool OnTextSelect(AudacityProject& project);
+
 private:
     void ResetClipNameEdit();
 

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
@@ -1290,6 +1290,66 @@ unsigned WaveTrackView::LoseFocus(AudacityProject *project)
    return result;
 }
 
+bool WaveTrackView::CutSelectedText(AudacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance 
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextCut(project))
+            return true;
+      }
+   }
+   return false;
+}
+
+bool WaveTrackView::CopySelectedText(AudacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextCopy(project))
+            return true;
+      }
+   }
+   return false;
+}
+
+bool WaveTrackView::PasteText(AudacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextPaste(project))
+            return true;
+      }
+   }
+   return false;
+}
+
+bool WaveTrackView::SelectAllText(AudacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextSelect(project))
+            return true;
+      }
+   }
+   return false;
+}
+
 std::vector< std::shared_ptr< WaveTrackSubView > >
 WaveTrackView::GetAllSubViews()
 {

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.h
@@ -174,6 +174,16 @@ public:
 
    unsigned LoseFocus(AudacityProject *project) override;
 
+   //FIXME: These functions do not push state to undo history
+   //because attempt to do so leads to a focus lose which, in
+   //turn finilizes text editing (state is saved after text
+   //editing was intentionally finished instead)
+
+   bool CutSelectedText(AudacityProject& project);
+   bool CopySelectedText(AudacityProject& project);
+   bool PasteText(AudacityProject& project);
+   bool SelectAllText(AudacityProject& project);
+
 private:
    void BuildSubViews() const;
    void DoSetDisplay(Display display, bool exclusive = true);

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 4076f10. Modified for Saucedacity.
Signed-off-by: Avery King <avery98@pm.me>

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.cpp
@@ -385,8 +385,9 @@ const ReservedCommandFlag &SomeClipIsSelectedFlag()
 
 unsigned WaveTrackAffordanceControls::CaptureKey(wxKeyEvent& event, ViewInfo& viewInfo, wxWindow* pParent, SaucedacityProject* project)
 {
-    if (!mTextEditHelper)
-       // Handle the event if we are already editing clip name text...
+    if (!mTextEditHelper 
+       || !mTextEditHelper->CaptureKey(event.GetKeyCode(), event.GetModifiers()))
+       // Handle the event if it can be processed by the text editor (if any)
        event.Skip();
     return RefreshCode::RefreshNone;
 }
@@ -398,10 +399,11 @@ unsigned WaveTrackAffordanceControls::KeyDown(wxKeyEvent& event, ViewInfo& viewI
     
     if (mTextEditHelper)
     {
-        mTextEditHelper->OnKeyDown(keyCode, event.GetModifiers(), project);
-        if (!TextEditHelper::IsGoodEditKeyCode(keyCode))
-            event.Skip();
-        return RefreshCode::RefreshCell;
+       if (!mTextEditHelper->OnKeyDown(keyCode, event.GetModifiers(), project) 
+          && !TextEditHelper::IsGoodEditKeyCode(keyCode))
+          event.Skip();
+
+       return RefreshCode::RefreshCell;
     }
     return RefreshCode::RefreshNone;
 }
@@ -490,6 +492,46 @@ bool WaveTrackAffordanceControls::StartEditNameOfMatchingClip(
         return StartEditClipName(&project);
     }
     return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextCopy(SaucedacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->CopySelectedText(project);
+      return true;
+   }
+   return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextCut(SaucedacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->CutSelectedText(project);
+      return true;
+   }
+   return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextPaste(SaucedacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->PasteSelectedText(project);
+      return true;
+   }
+   return false;
+}
+
+bool WaveTrackAffordanceControls::OnTextSelect(SaucedacityProject& project)
+{
+   if (mTextEditHelper)
+   {
+      mTextEditHelper->SelectAll();
+      return true;
+   }
+   return false;
 }
 
 unsigned WaveTrackAffordanceControls::OnAffordanceClick(const TrackPanelMouseEvent& event, SaucedacityProject* project)

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceControls.h
@@ -86,6 +86,11 @@ public:
 
     unsigned OnAffordanceClick(const TrackPanelMouseEvent& event, SaucedacityProject* project);
 
+    bool OnTextCopy(SaucedacityProject& project);
+    bool OnTextCut(SaucedacityProject& project);
+    bool OnTextPaste(SaucedacityProject& project);
+    bool OnTextSelect(SaucedacityProject& project);
+
 private:
     void ResetClipNameEdit();
 

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.cpp
@@ -1266,6 +1266,66 @@ unsigned WaveTrackView::LoseFocus(SaucedacityProject *project)
    return result;
 }
 
+bool WaveTrackView::CutSelectedText(SaucedacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance 
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextCut(project))
+            return true;
+      }
+   }
+   return false;
+}
+
+bool WaveTrackView::CopySelectedText(SaucedacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextCopy(project))
+            return true;
+      }
+   }
+   return false;
+}
+
+bool WaveTrackView::PasteText(SaucedacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextPaste(project))
+            return true;
+      }
+   }
+   return false;
+}
+
+bool WaveTrackView::SelectAllText(SaucedacityProject& project)
+{
+   for (auto channel : TrackList::Channels(FindTrack().get()))
+   {
+      auto& view = TrackView::Get(*channel);
+      if (auto affordance
+         = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(view.GetAffordanceControls()))
+      {
+         if (affordance->OnTextSelect(project))
+            return true;
+      }
+   }
+   return false;
+}
+
 std::vector< std::shared_ptr< WaveTrackSubView > >
 WaveTrackView::GetAllSubViews()
 {

src/tracks/playabletrack/wavetrack/ui/WaveTrackView.h
@@ -173,6 +173,16 @@ public:
 
    unsigned LoseFocus(SaucedacityProject *project) override;
 
+   //FIXME: These functions do not push state to undo history
+   //because attempt to do so leads to a focus lose which, in
+   //turn finilizes text editing (state is saved after text
+   //editing was intentionally finished instead)
+
+   bool CutSelectedText(SaucedacityProject& project);
+   bool CopySelectedText(SaucedacityProject& project);
+   bool PasteText(SaucedacityProject& project);
+   bool SelectAllText(SaucedacityProject& project);
+
 private:
    void BuildSubViews() const;
    void DoSetDisplay(Display display, bool exclusive = true);