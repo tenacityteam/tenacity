audacity/audacity

WaveTrackAffordanceHandle refactoring
Moved to be reused
Also, disables "clip rename" action when clip details are not visible

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceHandle.cpp
@@ -1,0 +1,51 @@
+/*!********************************************************************
+*
+ Audacity: A Digital Audio Editor
+
+ @file WaveTrackAffordanceHandle.cpp
+
+ Vitaly Sverchinsky
+
+ **********************************************************************/
+
+#include "WaveTrackAffordanceHandle.h"
+#include "WaveTrackAffordanceControls.h"
+#include "WaveTrackView.h"
+#include "ViewInfo.h"
+
+#include "../../../../WaveClip.h"
+#include "../../../../RefreshCode.h"
+#include "../../../../TrackPanelMouseEvent.h"
+#include "../../../../ProjectHistory.h"
+
+WaveTrackAffordanceHandle::WaveTrackAffordanceHandle(const std::shared_ptr<Track>& track, const std::shared_ptr<WaveClip>& target)
+   : AffordanceHandle(track), mTarget(target)
+{ }
+
+UIHandle::Result WaveTrackAffordanceHandle::Click(const TrackPanelMouseEvent& event, AudacityProject* project)
+{
+   Result result = RefreshCode::RefreshNone;
+   if (WaveTrackView::ClipDetailsVisible(*mTarget, ViewInfo::Get(*project), event.rect))
+   {
+      auto affordanceControl = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(event.pCell);
+
+      if (affordanceControl)
+      {
+         result |= affordanceControl->OnAffordanceClick(event, project);
+         if (!event.event.GetSkipped())
+            return result;
+         event.event.Skip(false);
+      }
+   }
+   return result | AffordanceHandle::Click(event, project);
+}
+
+UIHandle::Result WaveTrackAffordanceHandle::SelectAt(const TrackPanelMouseEvent& event, AudacityProject* project)
+{
+   auto& viewInfo = ViewInfo::Get(*project);
+   viewInfo.selectedRegion.setTimes(mTarget->GetPlayStartTime(), mTarget->GetPlayEndTime());
+
+   ProjectHistory::Get(*project).ModifyState(false);
+
+   return RefreshCode::RefreshAll | RefreshCode::Cancelled;
+}

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceHandle.h
@@ -1,0 +1,27 @@
+/*!********************************************************************
+*
+ Audacity: A Digital Audio Editor
+
+ @file WaveTrackAffordanceHandle.h
+
+ Vitaly Sverchinsky
+
+ **********************************************************************/
+
+#pragma once
+
+#include "../../../ui/AffordanceHandle.h"
+
+class WaveClip;
+
+//! Implements some features which are specific to Wave Clips
+class WaveTrackAffordanceHandle final : public AffordanceHandle
+{
+   std::shared_ptr<WaveClip> mTarget;
+public:
+   WaveTrackAffordanceHandle(const std::shared_ptr<Track>& track, const std::shared_ptr<WaveClip>& target);
+
+   Result Click(const TrackPanelMouseEvent& event, AudacityProject* project) override;
+
+   UIHandle::Result SelectAt(const TrackPanelMouseEvent& event, AudacityProject* project) override;
+};

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 4bed88bf2.
Signed-off-by: Avery King <avery98@pm.me>

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceHandle.cpp
@@ -1,0 +1,51 @@
+/*!********************************************************************
+*
+ Audacity: A Digital Audio Editor
+
+ @file WaveTrackAffordanceHandle.cpp
+
+ Vitaly Sverchinsky
+
+ **********************************************************************/
+
+#include "WaveTrackAffordanceHandle.h"
+#include "WaveTrackAffordanceControls.h"
+#include "WaveTrackView.h"
+#include "ViewInfo.h"
+
+#include "../../../../WaveClip.h"
+#include "../../../../RefreshCode.h"
+#include "../../../../TrackPanelMouseEvent.h"
+#include "../../../../ProjectHistory.h"
+
+WaveTrackAffordanceHandle::WaveTrackAffordanceHandle(const std::shared_ptr<Track>& track, const std::shared_ptr<WaveClip>& target)
+   : AffordanceHandle(track), mTarget(target)
+{ }
+
+UIHandle::Result WaveTrackAffordanceHandle::Click(const TrackPanelMouseEvent& event, SaucedacityProject* project)
+{
+   Result result = RefreshCode::RefreshNone;
+   if (WaveTrackView::ClipDetailsVisible(*mTarget, ViewInfo::Get(*project), event.rect))
+   {
+      auto affordanceControl = std::dynamic_pointer_cast<WaveTrackAffordanceControls>(event.pCell);
+
+      if (affordanceControl)
+      {
+         result |= affordanceControl->OnAffordanceClick(event, project);
+         if (!event.event.GetSkipped())
+            return result;
+         event.event.Skip(false);
+      }
+   }
+   return result | AffordanceHandle::Click(event, project);
+}
+
+UIHandle::Result WaveTrackAffordanceHandle::SelectAt(const TrackPanelMouseEvent& event, SaucedacityProject* project)
+{
+   auto& viewInfo = ViewInfo::Get(*project);
+   viewInfo.selectedRegion.setTimes(mTarget->GetPlayStartTime(), mTarget->GetPlayEndTime());
+
+   ProjectHistory::Get(*project).ModifyState(false);
+
+   return RefreshCode::RefreshAll | RefreshCode::Cancelled;
+}

src/tracks/playabletrack/wavetrack/ui/WaveTrackAffordanceHandle.h
@@ -1,0 +1,27 @@
+/*!********************************************************************
+*
+ Audacity: A Digital Audio Editor
+
+ @file WaveTrackAffordanceHandle.h
+
+ Vitaly Sverchinsky
+
+ **********************************************************************/
+
+#pragma once
+
+#include "../../../ui/AffordanceHandle.h"
+
+class WaveClip;
+
+//! Implements some features which are specific to Wave Clips
+class WaveTrackAffordanceHandle final : public AffordanceHandle
+{
+   std::shared_ptr<WaveClip> mTarget;
+public:
+   WaveTrackAffordanceHandle(const std::shared_ptr<Track>& track, const std::shared_ptr<WaveClip>& target);
+
+   Result Click(const TrackPanelMouseEvent& event, SaucedacityProject* project) override;
+
+   UIHandle::Result SelectAt(const TrackPanelMouseEvent& event, SaucedacityProject* project) override;
+};