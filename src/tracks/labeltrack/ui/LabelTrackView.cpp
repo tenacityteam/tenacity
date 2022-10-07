audacity/audacity

Text selection with Ctrl+A in labels

src/tracks/labeltrack/ui/LabelTrackView.cpp
@@ -1197,6 +1197,23 @@ bool LabelTrackView::PasteSelectedText(
    return true;
 }
 
+bool LabelTrackView::SelectAllText(AudacityProject& project)
+{
+    if (!IsValidIndex(mTextEditIndex, project))
+        return false;
+
+    const auto pTrack = FindLabelTrack();
+
+    const auto& mLabels = pTrack->GetLabels();
+    auto labelStruct = mLabels[mTextEditIndex];
+    auto& title = labelStruct.title;
+
+    mInitialCursorPos = 0;
+    mCurrentCursorPos = title.Length();
+
+    return true;
+}
+
 /// @return true if the text data is available in the clipboard, false otherwise
 bool LabelTrackView::IsTextClipSupported()
 {

src/tracks/labeltrack/ui/LabelTrackView.h
@@ -114,6 +114,8 @@ public:
 
    bool CutSelectedText( AudacityProject &project );
    bool CopySelectedText( AudacityProject &project );
+   bool SelectAllText(AudacityProject& project);
+   
    bool PasteSelectedText(
       AudacityProject &project, double sel0, double sel1 );
 

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 249d022.
Signed-off-by: Avery King <avery98@pm.me>

src/tracks/labeltrack/ui/LabelTrackView.cpp
@@ -1193,6 +1193,23 @@ bool LabelTrackView::PasteSelectedText(
    return true;
 }
 
+bool LabelTrackView::SelectAllText(SaucedacityProject& project)
+{
+    if (!IsValidIndex(mTextEditIndex, project))
+        return false;
+
+    const auto pTrack = FindLabelTrack();
+
+    const auto& mLabels = pTrack->GetLabels();
+    auto labelStruct = mLabels[mTextEditIndex];
+    auto& title = labelStruct.title;
+
+    mInitialCursorPos = 0;
+    mCurrentCursorPos = title.Length();
+
+    return true;
+}
+
 /// @return true if the text data is available in the clipboard, false otherwise
 bool LabelTrackView::IsTextClipSupported()
 {

src/tracks/labeltrack/ui/LabelTrackView.h
@@ -116,6 +116,8 @@ public:
 
    bool CutSelectedText( SaucedacityProject &project );
    bool CopySelectedText( SaucedacityProject &project );
+   bool SelectAllText(SaucedacityProject& project);
+   
    bool PasteSelectedText(
       SaucedacityProject &project, double sel0, double sel1 );
 