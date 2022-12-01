d6ac3ae1793b55e3e3747b76b5823419bc529dcd
Avery King <avery98@pm.me>
2022-11-30 19:54:21 -0800

    GitHub actions: fix syntax error in dependency script
    Signed-off-by: Avery King <avery98@pm.me>

--- scripts/ci/dependencies.sh
+++ scripts/ci/dependencies.sh
@@ -4,7 +4,7 @@
 
 set -euxo pipefail
 
-if [["${OSTYPE}" == msys* ]]; then
+if [[ "${OSTYPE}" == msys* ]]; then
     # We don't do anything for Windows package management right now, so
     # exit successfully.
     exit 0
