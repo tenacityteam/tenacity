audacity/audacity

Name changes, comments; ProjectFileIORegistry is a singleton class

src/ProjectFileIORegistry.cpp
@@ -11,34 +11,27 @@
 #include "ProjectFileIORegistry.h"
 
 #include "Identifier.h"
-#include <unordered_map>
 #include <wx/string.h>
 
 
-namespace ProjectFileIORegistry {
-
-namespace {
-   using TagTable = std::unordered_map< wxString, TagHandlerFactory >;
-   static TagTable &sTagTable()
-   {
-      static TagTable theTable;
-      return theTable;
-   }
-}
-
-Entry::Entry(
-   const wxString &tag, const TagHandlerFactory &factory )
+ProjectFileIORegistry::
+ObjectReaderEntry::ObjectReaderEntry( const wxString &tag, ObjectAccessor fn )
 {
-   sTagTable()[ tag ] = factory;
+   Get().mTagTable[ tag ] = move(fn);
 }
 
-TagHandlerFactory Lookup( const wxString &tag )
+XMLTagHandler *ProjectFileIORegistry::CallObjectAccessor(
+   const wxString &tag, AudacityProject &project )
 {
-   const auto &table = sTagTable();
-   auto iter = table.find( tag );
-   if ( iter == table.end() )
-      return {};
-   return iter->second;
+   const auto &table = mTagTable;
+   if (auto iter = table.find( tag ); iter != table.end())
+      if (auto &fn = iter->second)
+         return fn(project);
+   return nullptr;
 }
 
+ProjectFileIORegistry &ProjectFileIORegistry::Get()
+{
+   static ProjectFileIORegistry instance;
+   return instance;
 }

src/ProjectFileIORegistry.h
@@ -11,25 +11,42 @@
 #ifndef __AUDACITY_PROJECT_FILE_IO_REGISTRY__
 #define __AUDACITY_PROJECT_FILE_IO_REGISTRY__
 
+#include <wx/string.h>
 #include <functional>
+#include <unordered_map>
 
 class AudacityProject;
 class XMLTagHandler;
-class wxString;
 
-namespace ProjectFileIORegistry {
+class AUDACITY_DLL_API ProjectFileIORegistry {
+public:
 
-// Type of functions returning objects that interpret a part of the saved XML
-using TagHandlerFactory =
+//! Type of functions returning objects that interpret a part of the saved XML
+/*!
+ This "accessor" may or may not act as a "factory" that builds a new object and
+ may return nullptr.  Caller of the accessor is not responsible for the object
+ lifetime, which is assumed to outlast the project loading procedure.
+ */
+using ObjectAccessor =
    std::function< XMLTagHandler *( AudacityProject & ) >;
 
 // Typically statically constructed
-struct AUDACITY_DLL_API Entry{
-   Entry( const wxString &tag, const TagHandlerFactory &factory );
+struct AUDACITY_DLL_API ObjectReaderEntry {
+   ObjectReaderEntry( const wxString &tag, ObjectAccessor fn );
 };
 
-TagHandlerFactory Lookup( const wxString &tag );
+XMLTagHandler *CallObjectAccessor(
+   const wxString &tag, AudacityProject & );
 
-}
+//! Get the unique instance
+static ProjectFileIORegistry &Get();
+
+private:
+   
+using TagTable =
+   std::unordered_map< wxString, ObjectAccessor >;
+TagTable mTagTable;
+
+};
 
 #endif

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 84b9438. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ProjectFileIORegistry.cpp
@@ -13,34 +13,27 @@
 // Tenacity libraries
 #include <lib-strings/Identifier.h>
 
-#include <unordered_map>
 #include <wx/string.h>
 
 
-namespace ProjectFileIORegistry {
-
-namespace {
-   using TagTable = std::unordered_map< wxString, TagHandlerFactory >;
-   static TagTable &sTagTable()
-   {
-      static TagTable theTable;
-      return theTable;
-   }
-}
-
-Entry::Entry(
-   const wxString &tag, const TagHandlerFactory &factory )
+ProjectFileIORegistry::
+ObjectReaderEntry::ObjectReaderEntry( const wxString &tag, ObjectAccessor fn )
 {
-   sTagTable()[ tag ] = factory;
+   Get().mTagTable[ tag ] = move(fn);
 }
 
-TagHandlerFactory Lookup( const wxString &tag )
+XMLTagHandler *ProjectFileIORegistry::CallObjectAccessor(
+   const wxString &tag, TenacityProject &project )
 {
-   const auto &table = sTagTable();
-   auto iter = table.find( tag );
-   if ( iter == table.end() )
-      return {};
-   return iter->second;
+   const auto &table = mTagTable;
+   if (auto iter = table.find( tag ); iter != table.end())
+      if (auto &fn = iter->second)
+         return fn(project);
+   return nullptr;
 }
 
+ProjectFileIORegistry &ProjectFileIORegistry::Get()
+{
+   static ProjectFileIORegistry instance;
+   return instance;
 }

src/ProjectFileIORegistry.h
@@ -11,25 +11,42 @@
 #ifndef __AUDACITY_PROJECT_FILE_IO_REGISTRY__
 #define __AUDACITY_PROJECT_FILE_IO_REGISTRY__
 
+#include <wx/string.h>
 #include <functional>
+#include <unordered_map>
 
 class TenacityProject;
 class XMLTagHandler;
-class wxString;
 
-namespace ProjectFileIORegistry {
+class TENACITY_DLL_API ProjectFileIORegistry {
+public:
 
-// Type of functions returning objects that interpret a part of the saved XML
-using TagHandlerFactory =
+//! Type of functions returning objects that interpret a part of the saved XML
+/*!
+ This "accessor" may or may not act as a "factory" that builds a new object and
+ may return nullptr.  Caller of the accessor is not responsible for the object
+ lifetime, which is assumed to outlast the project loading procedure.
+ */
+using ObjectAccessor =
    std::function< XMLTagHandler *( TenacityProject & ) >;
 
 // Typically statically constructed
-struct TENACITY_DLL_API Entry{
-   Entry( const wxString &tag, const TagHandlerFactory &factory );
+struct TENACITY_DLL_API ObjectReaderEntry {
+   ObjectReaderEntry( const wxString &tag, ObjectAccessor fn );
 };
 
-TagHandlerFactory Lookup( const wxString &tag );
+XMLTagHandler *CallObjectAccessor(
+   const wxString &tag, TenacityProject & );
 
-}
+//! Get the unique instance
+static ProjectFileIORegistry &Get();
+
+private:
+   
+using TagTable =
+   std::unordered_map< wxString, ObjectAccessor >;
+TagTable mTagTable;
+
+};
 
 #endif