alex74747/audacity

Invert dependency of ViewInfo on TracksBehaviorsPrefs

src/ViewInfo.h
@@ -16,6 +16,7 @@
 #include <wx/weakref.h> // member variable
 #include "SelectedRegion.h"
 #include "MemoryX.h"
+#include "Prefs.h"
 #include "ZoomInfo.h" // to inherit
 
 
@@ -230,4 +231,5 @@ private:
    int mHeight{ 0 };
 };
 
+extern AUDACITY_DLL_API BoolSetting ScrollingPreference;
 #endif

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 0097e7b. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ViewInfo.h
@@ -17,6 +17,7 @@
 #include <wx/weakref.h> // member variable
 #include "SelectedRegion.h"
 #include <memory>
+#include "Prefs.h"
 #include "XMLMethodRegistry.h"
 #include "ZoomInfo.h" // to inherit
 
@@ -228,4 +229,5 @@ private:
    struct ProjectFileIORegistration;
 };
 
+extern TENACITY_DLL_API BoolSetting ScrollingPreference;
 #endif