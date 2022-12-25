audacity/audacity

Make the active project pointer a weak_ptr and add null checks

src/ActiveProject.cpp
@@ -18,17 +18,18 @@
 wxDEFINE_EVENT(EVT_PROJECT_ACTIVATION, wxCommandEvent);
 
 //This is a pointer to the currently-active project.
-static AudacityProject *gActiveProject;
+static std::weak_ptr<AudacityProject> gActiveProject;
 
-AUDACITY_DLL_API AudacityProject *GetActiveProject()
+AUDACITY_DLL_API std::weak_ptr<AudacityProject> GetActiveProject()
 {
    return gActiveProject;
 }
 
 void SetActiveProject(AudacityProject * project)
 {
-   if ( gActiveProject != project ) {
-      gActiveProject = project;
+   auto pProject = project ? nullptr : project->shared_from_this();
+   if ( gActiveProject.lock() != pProject ) {
+      gActiveProject = pProject;
       KeyboardCapture::Capture( nullptr );
       wxTheApp->QueueEvent( safenew wxCommandEvent{ EVT_PROJECT_ACTIVATION } );
    }

src/ActiveProject.h
@@ -13,10 +13,11 @@
 #define __AUDACITY_ACTIVE_PROJECT__
 
 #include <wx/event.h> // to declare custom event type
+#include <memory>
 
 class AudacityProject;
 
-AUDACITY_DLL_API AudacityProject *GetActiveProject();
+AUDACITY_DLL_API std::weak_ptr<AudacityProject> GetActiveProject();
 // For use by ProjectManager only:
 AUDACITY_DLL_API void SetActiveProject(AudacityProject * project);
 

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit dcaf0bc. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ActiveProject.cpp
@@ -18,17 +18,18 @@
 wxDEFINE_EVENT(EVT_PROJECT_ACTIVATION, wxCommandEvent);
 
 //This is a pointer to the currently-active project.
-static TenacityProject *gActiveProject;
+static std::weak_ptr<TenacityProject> gActiveProject;
 
-TENACITY_DLL_API TenacityProject *GetActiveProject()
+TENACITY_DLL_API std::weak_ptr<TenacityProject> GetActiveProject()
 {
    return gActiveProject;
 }
 
 void SetActiveProject(TenacityProject * project)
 {
-   if ( gActiveProject != project ) {
-      gActiveProject = project;
+   auto pProject = project ? nullptr : project->shared_from_this();
+   if ( gActiveProject.lock() != pProject ) {
+      gActiveProject = pProject;
       KeyboardCapture::Capture( nullptr );
       wxTheApp->QueueEvent( safenew wxCommandEvent{ EVT_PROJECT_ACTIVATION } );
    }

src/ActiveProject.h
@@ -13,10 +13,11 @@
 #define __AUDACITY_ACTIVE_PROJECT__
 
 #include <wx/event.h> // to declare custom event type
+#include <memory>
 
 class TenacityProject;
 
-TENACITY_DLL_API TenacityProject *GetActiveProject();
+TENACITY_DLL_API std::weak_ptr<TenacityProject> GetActiveProject();
 // For use by ProjectManager only:
 TENACITY_DLL_API void SetActiveProject(TenacityProject * project);
 