audacity/audacity

Base class of ProjectFileIORegistry holds type-erased functions...
... trust me, this is going somewhere.

src/ProjectFileIORegistry.h
@@ -18,34 +18,55 @@
 class AudacityProject;
 class XMLTagHandler;
 
-class AUDACITY_DLL_API ProjectFileIORegistry {
+//! Implementation helper for ProjectFileIORegistry.
+/*! It makes most of the work non-inline and is used by derived classes that
+ supply thin inline type-erasing wrappers. */
+class AUDACITY_DLL_API XMLMethodRegistryBase {
+public:
+   XMLMethodRegistryBase();
+   ~XMLMethodRegistryBase();
+protected:
+   using TypeErasedObjectAccessor = std::function< XMLTagHandler *( void* ) >;
+   using TagTable =
+      std::unordered_map< wxString, TypeErasedObjectAccessor >;
+   TagTable mTagTable;
+
+   void Register( const wxString &tag, TypeErasedObjectAccessor accessor );
+   XMLTagHandler *CallObjectAccessor( const wxString &tag, void *p );
+};
+
+class AUDACITY_DLL_API ProjectFileIORegistry : public XMLMethodRegistryBase {
 public:
 
-//! Type of functions returning objects that interpret a part of the saved XML
+   // Typically statically constructed
+struct ObjectReaderEntry {
+   template <
 /*!
  This "accessor" may or may not act as a "factory" that builds a new object and
  may return nullptr.  Caller of the accessor is not responsible for the object
  lifetime, which is assumed to outlast the project loading procedure.
  */
-using ObjectAccessor =
-   std::function< XMLTagHandler *( AudacityProject & ) >;
-
-// Typically statically constructed
-struct AUDACITY_DLL_API ObjectReaderEntry {
-   ObjectReaderEntry( const wxString &tag, ObjectAccessor fn );
+      typename ObjectAccessor /*!< Often a lambda.
+         A function from AudacityProject& to XMLTagHandler */
+   >
+   ObjectReaderEntry( const wxString &tag, ObjectAccessor fn )
+   {
+      // Remember the function, type-erased
+      Get().Register( tag, [ fn = std::move(fn) ] (void *p) {
+         // CallObjectAccessor will guarantee p is not null
+         return fn( *static_cast<AudacityProject *>(p) );
+      } );
+   }
 };
 
 XMLTagHandler *CallObjectAccessor(
-   const wxString &tag, AudacityProject & );
+   const wxString &tag, AudacityProject &project )
+{
+   return XMLMethodRegistryBase::CallObjectAccessor( tag, &project );
+}
 
 //! Get the unique instance
 static ProjectFileIORegistry &Get();
-
-private:
-   
-using TagTable =
-   std::unordered_map< wxString, ObjectAccessor >;
-TagTable mTagTable;
 
 };
 

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit dca7b34. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ProjectFileIORegistry.h
@@ -18,34 +18,55 @@
 class TenacityProject;
 class XMLTagHandler;
 
-class TENACITY_DLL_API ProjectFileIORegistry {
+//! Implementation helper for ProjectFileIORegistry.
+/*! It makes most of the work non-inline and is used by derived classes that
+ supply thin inline type-erasing wrappers. */
+class TENACITY_DLL_API XMLMethodRegistryBase {
+public:
+   XMLMethodRegistryBase();
+   ~XMLMethodRegistryBase();
+protected:
+   using TypeErasedObjectAccessor = std::function< XMLTagHandler *( void* ) >;
+   using TagTable =
+      std::unordered_map< wxString, TypeErasedObjectAccessor >;
+   TagTable mTagTable;
+
+   void Register( const wxString &tag, TypeErasedObjectAccessor accessor );
+   XMLTagHandler *CallObjectAccessor( const wxString &tag, void *p );
+};
+
+class TENACITY_DLL_API ProjectFileIORegistry : public XMLMethodRegistryBase {
 public:
 
-//! Type of functions returning objects that interpret a part of the saved XML
+   // Typically statically constructed
+struct ObjectReaderEntry {
+   template <
 /*!
  This "accessor" may or may not act as a "factory" that builds a new object and
  may return nullptr.  Caller of the accessor is not responsible for the object
  lifetime, which is assumed to outlast the project loading procedure.
  */
-using ObjectAccessor =
-   std::function< XMLTagHandler *( TenacityProject & ) >;
-
-// Typically statically constructed
-struct TENACITY_DLL_API ObjectReaderEntry {
-   ObjectReaderEntry( const wxString &tag, ObjectAccessor fn );
+      typename ObjectAccessor /*!< Often a lambda.
+         A function from TenacityProject& to XMLTagHandler */
+   >
+   ObjectReaderEntry( const wxString &tag, ObjectAccessor fn )
+   {
+      // Remember the function, type-erased
+      Get().Register( tag, [ fn = std::move(fn) ] (void *p) {
+         // CallObjectAccessor will guarantee p is not null
+         return fn( *static_cast<TenacityProject *>(p) );
+      } );
+   }
 };
 
 XMLTagHandler *CallObjectAccessor(
-   const wxString &tag, TenacityProject & );
+   const wxString &tag, TenacityProject &project )
+{
+   return XMLMethodRegistryBase::CallObjectAccessor( tag, &project );
+}
 
 //! Get the unique instance
 static ProjectFileIORegistry &Get();
-
-private:
-   
-using TagTable =
-   std::unordered_map< wxString, ObjectAccessor >;
-TagTable mTagTable;
 
 };
 