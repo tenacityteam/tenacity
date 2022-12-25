audacity/audacity

Adds an ability to change project's user_version based on features used
This commit introduces a new mechanism to allow projects components to specify the Audacity version they require to be loaded. 

It is possible to set the version based on the features that are currently in use. 

Smart Clips use this new approach to require at least Audacity 3.1 if there are trimmed clips in the project.

libraries/lib-project/ProjectFormatExtensionsRegistry.cpp
@@ -1,0 +1,59 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ProjectFormatExtensionRegistry.cpp
+
+  Dmitry Vedenko
+
+**********************************************************************/
+
+
+#include "ProjectFormatExtensionsRegistry.h"
+
+#include <algorithm>
+
+#include "Project.h"
+
+ProjectFormatExtensionsRegistry& GetProjectFormatExtensionsRegistry()
+{
+   static ProjectFormatExtensionsRegistry instance;
+   return instance;
+}
+
+const ProjectFormatExtensionsRegistry& ProjectFormatExtensionsRegistry::Get()
+{
+   return GetProjectFormatExtensionsRegistry();
+}
+
+void ProjectFormatExtensionsRegistry::Register(
+   ProjectVersionResolver formatExtension)
+{
+   mRegisteredExtensions.emplace_back(std::move(formatExtension));
+}
+
+ProjectFormatVersion ProjectFormatExtensionsRegistry::GetRequiredVersion(
+   const AudacityProject& project) const
+{
+   ProjectFormatVersion minVersion = BaseProjectFormatVersion;
+
+   for (auto formatExtension : mRegisteredExtensions)
+   {
+      if (!formatExtension)
+         continue;
+      
+      const auto formatExtensionVersion = formatExtension(project);
+
+      if (minVersion < formatExtensionVersion)
+         minVersion = formatExtensionVersion;
+   }
+
+   return minVersion;
+}
+
+ProjectFormatExtensionsRegistry::Extension::Extension(
+   ProjectVersionResolver resolver)
+{
+   if (resolver)
+      GetProjectFormatExtensionsRegistry().Register(std::move(resolver));
+}

libraries/lib-project/ProjectFormatExtensionsRegistry.h
@@ -1,0 +1,43 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ProjectFormatExtensionsRegistry.h
+
+  Dmitry Vedenko
+
+**********************************************************************/
+
+#pragma once
+
+#include <mutex>
+#include <functional>
+
+#include "ClientData.h"
+#include "ProjectFormatVersion.h"
+
+class AudacityProject;
+
+//! The class that allows evaluating the minimum version of Audacity required to support the project
+class PROJECT_API ProjectFormatExtensionsRegistry final
+   : public ClientData::Base
+{
+public:
+   using ProjectVersionResolver = ProjectFormatVersion (*)(const AudacityProject&);
+
+   static const ProjectFormatExtensionsRegistry& Get();
+
+   //! Returns the minimum possible version that can be used to save the project
+   ProjectFormatVersion GetRequiredVersion(const AudacityProject& project) const;
+
+   //! A struct to register the project extension that requires a different project version
+   struct PROJECT_API Extension final
+   {
+      Extension(ProjectVersionResolver resolver);
+   };
+
+private:
+   void Register(ProjectVersionResolver resolver);
+
+   std::vector<ProjectVersionResolver> mRegisteredExtensions;
+};

src/WaveTrack.cpp
@@ -63,6 +63,8 @@ from the project that will own the track.
 #include "tracks/ui/TrackView.h"
 #include "tracks/ui/TrackControls.h"
 
+#include "ProjectFormatExtensionsRegistry.h"
+
 using std::max;
 
 namespace {
@@ -111,7 +113,9 @@ static ProjectFileIORegistry::ObjectReaderEntry readerEntry{
 
 WaveTrack::Holder WaveTrackFactory::DuplicateWaveTrack(const WaveTrack &orig)
 {
-   return std::static_pointer_cast<WaveTrack>( orig.Duplicate() );
+   auto waveTrack = std::static_pointer_cast<WaveTrack>( orig.Duplicate() );
+
+   return waveTrack;
 }
 
 
@@ -121,7 +125,10 @@ WaveTrack::Holder WaveTrackFactory::NewWaveTrack(sampleFormat format, double rat
       format = QualitySettings::SampleFormatChoice();
    if (rate == 0)
       rate = mRate.GetRate();
-   return std::make_shared<WaveTrack> ( mpFactory, format, rate );
+
+   auto waveTrack = std::make_shared<WaveTrack> ( mpFactory, format, rate );
+
+   return waveTrack;
 }
 
 WaveTrack::WaveTrack( const SampleBlockFactoryPtr &pFactory,
@@ -2908,3 +2915,21 @@ void WaveTrackFactory::Destroy( AudacityProject &project )
 {
    project.AttachedObjects::Assign( key2, nullptr );
 }
+
+ProjectFormatExtensionsRegistry::Extension smartClipsExtension(
+   [](const AudacityProject& project) -> ProjectFormatVersion
+   {
+      const TrackList& trackList = TrackList::Get(project);
+
+      for (auto wt : trackList.Any<const WaveTrack>())
+      {
+         for (const auto& clip : wt->GetAllClips())
+         {
+            if (clip->GetTrimLeft() > 0.0 || clip->GetTrimRight() > 0.0)
+               return { 3, 1, 0, 0 };
+         }
+      }
+
+      return BaseProjectFormatVersion;
+   }
+);

tenacityteam/tenacity

This commit introduces a new mechanism to allow projects components to specify the Audacity version they require to be loaded.
It is possible to set the version based on the features that are currently in use.
===============================================================================
Cherry-picked from Audacity commit afb4f24. Modified for Tenacity.
* We can also use these features in case we introduce our own
  Tenacity-specific features for the AUP3 file format in the future.
Signed-off-by: Avery King <avery98@pm.me>

libraries/lib-project/ProjectFormatExtensionsRegistry.cpp
@@ -1,0 +1,59 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ProjectFormatExtensionRegistry.cpp
+
+  Dmitry Vedenko
+
+**********************************************************************/
+
+
+#include "ProjectFormatExtensionsRegistry.h"
+
+#include <algorithm>
+
+#include "Project.h"
+
+ProjectFormatExtensionsRegistry& GetProjectFormatExtensionsRegistry()
+{
+   static ProjectFormatExtensionsRegistry instance;
+   return instance;
+}
+
+const ProjectFormatExtensionsRegistry& ProjectFormatExtensionsRegistry::Get()
+{
+   return GetProjectFormatExtensionsRegistry();
+}
+
+void ProjectFormatExtensionsRegistry::Register(
+   ProjectVersionResolver formatExtension)
+{
+   mRegisteredExtensions.emplace_back(std::move(formatExtension));
+}
+
+ProjectFormatVersion ProjectFormatExtensionsRegistry::GetRequiredVersion(
+   const TenacityProject& project) const
+{
+   ProjectFormatVersion minVersion = BaseProjectFormatVersion;
+
+   for (auto formatExtension : mRegisteredExtensions)
+   {
+      if (!formatExtension)
+         continue;
+      
+      const auto formatExtensionVersion = formatExtension(project);
+
+      if (minVersion < formatExtensionVersion)
+         minVersion = formatExtensionVersion;
+   }
+
+   return minVersion;
+}
+
+ProjectFormatExtensionsRegistry::Extension::Extension(
+   ProjectVersionResolver resolver)
+{
+   if (resolver)
+      GetProjectFormatExtensionsRegistry().Register(std::move(resolver));
+}

libraries/lib-project/ProjectFormatExtensionsRegistry.h
@@ -1,0 +1,43 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  ProjectFormatExtensionsRegistry.h
+
+  Dmitry Vedenko
+
+**********************************************************************/
+
+#pragma once
+
+#include <mutex>
+#include <functional>
+
+#include "ClientData.h"
+#include "ProjectFormatVersion.h"
+
+class TenacityProject;
+
+//! The class that allows evaluating the minimum version of Audacity required to support the project
+class PROJECT_API ProjectFormatExtensionsRegistry final
+   : public ClientData::Base
+{
+public:
+   using ProjectVersionResolver = ProjectFormatVersion (*)(const TenacityProject&);
+
+   static const ProjectFormatExtensionsRegistry& Get();
+
+   //! Returns the minimum possible version that can be used to save the project
+   ProjectFormatVersion GetRequiredVersion(const TenacityProject& project) const;
+
+   //! A struct to register the project extension that requires a different project version
+   struct PROJECT_API Extension final
+   {
+      Extension(ProjectVersionResolver resolver);
+   };
+
+private:
+   void Register(ProjectVersionResolver resolver);
+
+   std::vector<ProjectVersionResolver> mRegisteredExtensions;
+};

src/WaveTrack.cpp
@@ -60,6 +60,8 @@ from the project that will own the track.
 #include "tracks/ui/TrackView.h"
 #include "tracks/ui/TrackControls.h"
 
+#include "ProjectFormatExtensionsRegistry.h"
+
 using std::max;
 
 namespace {
@@ -108,7 +110,9 @@ static ProjectFileIORegistry::ObjectReaderEntry readerEntry{
 
 WaveTrack::Holder WaveTrackFactory::DuplicateWaveTrack(const WaveTrack &orig)
 {
-   return std::static_pointer_cast<WaveTrack>( orig.Duplicate() );
+   auto waveTrack = std::static_pointer_cast<WaveTrack>( orig.Duplicate() );
+
+   return waveTrack;
 }
 
 
@@ -118,7 +122,10 @@ WaveTrack::Holder WaveTrackFactory::NewWaveTrack(sampleFormat format, double rat
       format = QualitySettings::SampleFormatChoice();
    if (rate == 0)
       rate = mRate.GetRate();
-   return std::make_shared<WaveTrack> ( mpFactory, format, rate );
+
+   auto waveTrack = std::make_shared<WaveTrack> ( mpFactory, format, rate );
+
+   return waveTrack;
 }
 
 WaveTrack::WaveTrack( const SampleBlockFactoryPtr &pFactory,
@@ -2900,3 +2907,21 @@ void WaveTrackFactory::Destroy( TenacityProject &project )
 {
    project.AttachedObjects::Assign( key2, nullptr );
 }
+
+ProjectFormatExtensionsRegistry::Extension smartClipsExtension(
+   [](const TenacityProject& project) -> ProjectFormatVersion
+   {
+      const TrackList& trackList = TrackList::Get(project);
+
+      for (auto wt : trackList.Any<const WaveTrack>())
+      {
+         for (const auto& clip : wt->GetAllClips())
+         {
+            if (clip->GetTrimLeft() > 0.0 || clip->GetTrimRight() > 0.0)
+               return { 3, 1, 0, 0 };
+         }
+      }
+
+      return BaseProjectFormatVersion;
+   }
+);