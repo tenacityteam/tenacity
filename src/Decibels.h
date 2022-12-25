Paul-Licameli/audacity

A settings object DecibelScaleCutoff in a new file

src/Decibels.h
@@ -1,0 +1,22 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+@file Decibels.h
+
+Paul Licameli
+   Moved a constant here from Envelope.h where it did not belong
+   Define the key string in one place here too
+ 
+   Split from GUISettings.h
+
+**********************************************************************/
+#ifndef __AUDACITY_DECIBELS__
+#define __AUDACITY_DECIBELS__
+
+#include "Prefs.h"
+
+//! Negation of this value is the lowest dB level that should be shown in dB scales
+extern AUDACITY_DLL_API IntSetting DecibelScaleCutoff;
+
+#endif

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 01a53e7. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/Decibels.h
@@ -1,0 +1,23 @@
+/*!********************************************************************
+
+   Audacity: A Digital Audio Editor
+
+   @file Decibels.h
+
+   Paul Licameli
+
+   Moved a constant here from Envelope.h where it did not belong
+   Define the key string in one place here too
+ 
+   Split from GUISettings.h
+
+**********************************************************************/
+#ifndef __AUDACITY_DECIBELS__
+#define __AUDACITY_DECIBELS__
+
+#include "Prefs.h"
+
+//! Negation of this value is the lowest dB level that should be shown in dB scales
+extern TENACITY_DLL_API IntSetting DecibelScaleCutoff;
+
+#endif