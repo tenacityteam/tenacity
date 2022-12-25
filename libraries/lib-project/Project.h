Paul-Licameli/audacity

New library for Project

libraries/lib-project/Project.h
@@ -11,8 +11,6 @@
 #ifndef __AUDACITY_PROJECT__
 #define __AUDACITY_PROJECT__
 
-#include "Identifier.h"
-
 #include "ClientData.h" // to inherit
 
 #include <memory>
@@ -21,12 +19,14 @@
 
 class AudacityProject;
 
+using FilePath = wxString;
+
 /// \brief an object of class AllProjects acts like a standard library
 /// container, but refers to a global array of open projects.  So you can
 /// iterate easily over shared pointers to them with range-for :
 /// for (auto pProject : AllProjects{}) { ... }
 /// The pointers are never null.
-class AUDACITY_DLL_API AllProjects
+class PROJECT_API AllProjects
 {
    // Use shared_ptr to projects, because elsewhere we need weak_ptr
    using AProjectHolder = std::shared_ptr< AudacityProject >;
@@ -62,7 +62,7 @@ public:
 };
 
 // Abstract base class for attached objects
-struct AUDACITY_DLL_API AttachedProjectObject {
+struct PROJECT_API AttachedProjectObject {
    virtual ~AttachedProjectObject();
    // Default implementation does nothing
    virtual void Destroy();
@@ -75,7 +75,7 @@ using AttachedProjectObjects = ClientData::Site<
    std::shared_ptr
 >;
 
-wxDECLARE_EXPORTED_EVENT(AUDACITY_DLL_API,
+wxDECLARE_EXPORTED_EVENT(PROJECT_API,
                          EVT_TRACK_PANEL_TIMER, wxCommandEvent);
 enum{ TrackPanelTimerInterval_ms = 50 };
 
@@ -85,7 +85,7 @@ enum{ TrackPanelTimerInterval_ms = 50 };
 /// message and a few other things.
 /// There is very little in this class, most of the intelligence residing in
 /// the cooperating attached objects.
-class AUDACITY_DLL_API AudacityProject final
+class PROJECT_API AudacityProject final
    : public wxEvtHandler
    , public AttachedProjectObjects
    , public std::enable_shared_from_this<AudacityProject>

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 4715ea8. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

libraries/lib-project/Project.h
@@ -31,7 +31,7 @@ The pointers are never null.
 However iterators will be invalid if addition or deletion of projects occur
 during traversal.
 */
-class TENACITY_DLL_API AllProjects
+class PROJECT_API AllProjects
 {
 
    // Use shared_ptr to projects, because elsewhere we need weak_ptr
@@ -76,7 +76,7 @@ using AttachedProjectObjects = ClientData::Site<
    TenacityProject, ClientData::Base, ClientData::SkipCopying, std::shared_ptr
 >;
 
-wxDECLARE_EXPORTED_EVENT(TENACITY_DLL_API,
+wxDECLARE_EXPORTED_EVENT(PROJECT_API,
                          EVT_TRACK_PANEL_TIMER, wxCommandEvent);
 
 ///\brief The top-level handle to an Audacity project.  It serves as a source
@@ -85,7 +85,7 @@ wxDECLARE_EXPORTED_EVENT(TENACITY_DLL_API,
 /// message and a few other things.
 /// There is very little in this class, most of the intelligence residing in
 /// the cooperating attached objects.
-class TENACITY_DLL_API TenacityProject final
+class PROJECT_API TenacityProject final
    : public wxEvtHandler
    , public AttachedProjectObjects
    , public std::enable_shared_from_this<TenacityProject>
@@ -131,8 +131,8 @@ private:
 
 // Generate a registry for serialized data attached to the project
 #include "XMLMethodRegistry.h"
-class TenacityProject;
+class AudacityProject;
 using ProjectFileIORegistry = XMLMethodRegistry<TenacityProject>;
-DECLARE_XML_METHOD_REGISTRY( TENACITY_DLL_API, ProjectFileIORegistry );
+DECLARE_XML_METHOD_REGISTRY( PROJECT_API, ProjectFileIORegistry );
 
 #endif