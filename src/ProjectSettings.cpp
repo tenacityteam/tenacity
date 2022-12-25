audacity/audacity

Add AttributeHandlers to ProjectFileIORegistry...
... And ProjectFileIO sheds more dependencies.

src/ProjectSettings.cpp
@@ -221,3 +221,32 @@ static ProjectFileIORegistry::WriterEntry entry {
                      settings.GetBandwidthSelectionFormatName().Internal());
 }
 };
+
+static ProjectFileIORegistry::AttributeReaderEntries entries {
+// Just a pointer to function, but needing overload resolution as non-const:
+(ProjectSettings& (*)(AudacityProject &)) &ProjectSettings::Get, {
+   { L"rate", [](auto &settings, auto value){
+      double rate;
+      Internat::CompatibleToDouble(value, &rate);
+      settings.SetRate( rate );
+   } },
+
+   // PRL:  The following have persisted as per-project settings for long.
+   // Maybe that should be abandoned.  Enough to save changes in the user
+   // preference file.
+   { L"snapto", [](auto &settings, auto value){
+      settings.SetSnapTo(wxString(value) == wxT("on") ? true : false);
+   } },
+   { L"selectionformat", [](auto &settings, auto value){
+      settings.SetSelectionFormat(
+         NumericConverter::LookupFormat( NumericConverter::TIME, value) );
+   } },
+   { L"frequencyformat", [](auto &settings, auto value){
+      settings.SetFrequencySelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::FREQUENCY, value ) );
+   } },
+   { L"bandwidthformat", [](auto &settings, auto value){
+      settings.SetBandwidthSelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::BANDWIDTH, value ) );
+   } },
+} };

src/ViewInfo.cpp
@@ -33,15 +33,22 @@ wxEvent *SelectedRegionEvent::Clone() const
    return safenew SelectedRegionEvent{ *this };
 }
 
-bool NotifyingSelectedRegion::HandleXMLAttribute
-   (const wxChar *attr, const wxChar *value,
-    const wxChar *legacyT0Name, const wxChar *legacyT1Name)
+XMLMethodRegistryBase::Mutators<NotifyingSelectedRegion>
+NotifyingSelectedRegion::Mutators(
+   const wxString &legacyT0Name, const wxString &legacyT1Name)
 {
-   auto result = mRegion.HandleXMLAttribute(
-      attr, value, legacyT0Name, legacyT1Name );
-   if ( result )
-      Notify( true );
-   return result;
+   XMLMethodRegistryBase::Mutators<NotifyingSelectedRegion> results;
+   // Get serialization methods of contained SelectedRegion, and wrap each
+   for (auto &delegate: SelectedRegion::Mutators(legacyT0Name, legacyT1Name)) {
+      results.emplace_back(
+         delegate.first,
+         [fn = std::move(delegate.second)](auto &region, auto value) {
+            fn( region.mRegion, value );
+            region.Notify( true );
+         }
+      );
+   }
+   return results;
 }
 
 NotifyingSelectedRegion& NotifyingSelectedRegion::operator =
@@ -216,30 +223,37 @@ void ViewInfo::WriteXMLAttributes(XMLWriter &xmlFile) const
    xmlFile.WriteAttr(wxT("zoom"), zoom, 10);
 }
 
-bool ViewInfo::ReadXMLAttribute(const wxChar *attr, const wxChar *value)
-{
-   if (selectedRegion.HandleXMLAttribute(attr, value, wxT("sel0"), wxT("sel1")))
-      return true;
+//! Construct once at static intialization time to hook project file IO
+static struct ViewInfo::ProjectFileIORegistration {
 
-   if (!wxStrcmp(attr, wxT("vpos"))) {
+ProjectFileIORegistry::AttributeReaderEntries entries {
+[](AudacityProject &project) -> NotifyingSelectedRegion &
+{
+   return ViewInfo::Get(project).selectedRegion;
+},
+NotifyingSelectedRegion::Mutators(L"sel0", L"sel1")
+};
+
+ProjectFileIORegistry::AttributeReaderEntries entries2 {
+// Just a pointer to function, but needing overload resolution as non-const:
+(ViewInfo& (*)(AudacityProject &)) &ViewInfo::Get,
+{
+   { L"vpos", [](auto &viewInfo, auto value){
       long longVpos;
       wxString(value).ToLong(&longVpos);
-      vpos = (int)(longVpos);
-      return true;
-   }
+      viewInfo.vpos = (int)(longVpos);
+      // Note that (other than in import of old .aup files) there is no other
+      // reassignment of vpos, except in handling the vertical scroll.
+   } },
+   { L"h", [](auto &viewInfo, auto value){
+      Internat::CompatibleToDouble(value, &viewInfo.h);
+   } },
+   { L"zoom", [](auto &viewInfo, auto value){
+      Internat::CompatibleToDouble(value, &viewInfo.zoom);
+   } },
+} };
 
-   if (!wxStrcmp(attr, wxT("h"))) {
-      Internat::CompatibleToDouble(value, &h);
-      return true;
-   }
-
-   if (!wxStrcmp(attr, wxT("zoom"))) {
-      Internat::CompatibleToDouble(value, &zoom);
-      return true;
-   }
-
-   return false;
-}
+} projectFileIORegistration;
 
 int ViewInfo::UpdateScrollPrefsID()
 {

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit c848d01. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ProjectSettings.cpp
@@ -220,3 +220,32 @@ static ProjectFileIORegistry::WriterEntry entry {
                      settings.GetBandwidthSelectionFormatName().Internal());
 }
 };
+
+static ProjectFileIORegistry::AttributeReaderEntries entries {
+// Just a pointer to function, but needing overload resolution as non-const:
+(ProjectSettings& (*)(TenacityProject &)) &ProjectSettings::Get, {
+   { L"rate", [](auto &settings, auto value){
+      double rate;
+      Internat::CompatibleToDouble(value, &rate);
+      settings.SetRate( rate );
+   } },
+
+   // PRL:  The following have persisted as per-project settings for long.
+   // Maybe that should be abandoned.  Enough to save changes in the user
+   // preference file.
+   { L"snapto", [](auto &settings, auto value){
+      settings.SetSnapTo(wxString(value) == wxT("on") ? true : false);
+   } },
+   { L"selectionformat", [](auto &settings, auto value){
+      settings.SetSelectionFormat(
+         NumericConverter::LookupFormat( NumericConverter::TIME, value) );
+   } },
+   { L"frequencyformat", [](auto &settings, auto value){
+      settings.SetFrequencySelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::FREQUENCY, value ) );
+   } },
+   { L"bandwidthformat", [](auto &settings, auto value){
+      settings.SetBandwidthSelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::BANDWIDTH, value ) );
+   } },
+} };

src/ViewInfo.cpp
@@ -34,15 +34,22 @@ wxEvent *SelectedRegionEvent::Clone() const
    return safenew SelectedRegionEvent{ *this };
 }
 
-bool NotifyingSelectedRegion::HandleXMLAttribute
-   (const wxChar *attr, const wxChar *value,
-    const wxChar *legacyT0Name, const wxChar *legacyT1Name)
+XMLMethodRegistryBase::Mutators<NotifyingSelectedRegion>
+NotifyingSelectedRegion::Mutators(
+   const wxString &legacyT0Name, const wxString &legacyT1Name)
 {
-   auto result = mRegion.HandleXMLAttribute(
-      attr, value, legacyT0Name, legacyT1Name );
-   if ( result )
-      Notify( true );
-   return result;
+   XMLMethodRegistryBase::Mutators<NotifyingSelectedRegion> results;
+   // Get serialization methods of contained SelectedRegion, and wrap each
+   for (auto &delegate: SelectedRegion::Mutators(legacyT0Name, legacyT1Name)) {
+      results.emplace_back(
+         delegate.first,
+         [fn = std::move(delegate.second)](auto &region, auto value) {
+            fn( region.mRegion, value );
+            region.Notify( true );
+         }
+      );
+   }
+   return results;
 }
 
 NotifyingSelectedRegion& NotifyingSelectedRegion::operator =
@@ -217,30 +224,37 @@ void ViewInfo::WriteXMLAttributes(XMLWriter &xmlFile) const
    xmlFile.WriteAttr(wxT("zoom"), zoom, 10);
 }
 
-bool ViewInfo::ReadXMLAttribute(const wxChar *attr, const wxChar *value)
-{
-   if (selectedRegion.HandleXMLAttribute(attr, value, wxT("sel0"), wxT("sel1")))
-      return true;
+//! Construct once at static intialization time to hook project file IO
+static struct ViewInfo::ProjectFileIORegistration {
 
-   if (!wxStrcmp(attr, wxT("vpos"))) {
+ProjectFileIORegistry::AttributeReaderEntries entries {
+[](TenacityProject &project) -> NotifyingSelectedRegion &
+{
+   return ViewInfo::Get(project).selectedRegion;
+},
+NotifyingSelectedRegion::Mutators(L"sel0", L"sel1")
+};
+
+ProjectFileIORegistry::AttributeReaderEntries entries2 {
+// Just a pointer to function, but needing overload resolution as non-const:
+(ViewInfo& (*)(TenacityProject &)) &ViewInfo::Get,
+{
+   { L"vpos", [](auto &viewInfo, auto value){
       long longVpos;
       wxString(value).ToLong(&longVpos);
-      vpos = (int)(longVpos);
-      return true;
-   }
+      viewInfo.vpos = (int)(longVpos);
+      // Note that (other than in import of old .aup files) there is no other
+      // reassignment of vpos, except in handling the vertical scroll.
+   } },
+   { L"h", [](auto &viewInfo, auto value){
+      Internat::CompatibleToDouble(value, &viewInfo.h);
+   } },
+   { L"zoom", [](auto &viewInfo, auto value){
+      Internat::CompatibleToDouble(value, &viewInfo.zoom);
+   } },
+} };
 
-   if (!wxStrcmp(attr, wxT("h"))) {
-      Internat::CompatibleToDouble(value, &h);
-      return true;
-   }
-
-   if (!wxStrcmp(attr, wxT("zoom"))) {
-      Internat::CompatibleToDouble(value, &zoom);
-      return true;
-   }
-
-   return false;
-}
+} projectFileIORegistration;
 
 int ViewInfo::UpdateScrollPrefsID()
 {