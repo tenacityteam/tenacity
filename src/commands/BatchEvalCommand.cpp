audacity/audacity

Change some signatures assuming non-null project; add a null check

src/commands/BatchEvalCommand.cpp
@@ -39,10 +39,10 @@ void BatchEvalCommandType::BuildSignature(CommandSignature &signature)
    signature.AddParameter(wxT("MacroName"), wxT(""), std::move(macroValidator));
 }
 
-OldStyleCommandPointer BatchEvalCommandType::Create( AudacityProject *project,
+OldStyleCommandPointer BatchEvalCommandType::Create( AudacityProject &project,
    std::unique_ptr<CommandOutputTargets> && WXUNUSED(target))
 {
-   return std::make_shared<BatchEvalCommand>(*project, *this);
+   return std::make_shared<BatchEvalCommand>(project, *this);
 }
 
 bool BatchEvalCommand::Apply(const CommandContext & context)

src/commands/BatchEvalCommand.h
@@ -33,7 +33,7 @@ class BatchEvalCommandType final : public OldStyleCommandType
 public:
    ComponentInterfaceSymbol BuildName() override;
    void BuildSignature(CommandSignature &signature) override;
-   OldStyleCommandPointer Create( AudacityProject *project,
+   OldStyleCommandPointer Create( AudacityProject &project,
       std::unique_ptr<CommandOutputTargets> &&target) override;
 };
 

src/commands/CommandBuilder.cpp
@@ -31,13 +31,13 @@ system by constructing BatchCommandEval objects.
 #include "../Shuttle.h"
 
 CommandBuilder::CommandBuilder(
-   AudacityProject *project, const wxString &cmdString)
+   AudacityProject &project, const wxString &cmdString)
    : mValid(false)
 {
    BuildCommand(project, cmdString);
 }
 
-CommandBuilder::CommandBuilder(AudacityProject *project,
+CommandBuilder::CommandBuilder(AudacityProject &project,
    const wxString &cmdName, const wxString &params)
    : mValid(false)
 {
@@ -82,7 +82,7 @@ void CommandBuilder::Success(const OldStyleCommandPointer &cmd)
    mValid = true;
 }
 
-void CommandBuilder::BuildCommand(AudacityProject *project,
+void CommandBuilder::BuildCommand(AudacityProject &project,
                                   const wxString &cmdName,
                                   const wxString &cmdParamsArg)
 {
@@ -188,7 +188,7 @@ void CommandBuilder::BuildCommand(AudacityProject *project,
 }
 
 void CommandBuilder::BuildCommand(
-   AudacityProject *project, const wxString &cmdStringArg)
+   AudacityProject &project, const wxString &cmdStringArg)
 {
    wxString cmdString(cmdStringArg);
 

src/commands/CommandBuilder.h
@@ -39,12 +39,12 @@ class CommandBuilder
 
       void Failure(const wxString &msg = {});
       void Success(const OldStyleCommandPointer &cmd);
-      void BuildCommand( AudacityProject *project,
+      void BuildCommand( AudacityProject &project,
          const wxString &cmdName, const wxString &cmdParams);
-      void BuildCommand( AudacityProject *project, const wxString &cmdString);
+      void BuildCommand( AudacityProject &project, const wxString &cmdString);
    public:
-      CommandBuilder(AudacityProject *project, const wxString &cmdString);
-      CommandBuilder(AudacityProject *project, const wxString &cmdName,
+      CommandBuilder(AudacityProject &project, const wxString &cmdString);
+      CommandBuilder(AudacityProject &project, const wxString &cmdName,
                      const wxString &cmdParams);
       ~CommandBuilder();
       bool WasValid();

src/commands/CommandType.h
@@ -64,7 +64,7 @@ public:
 
    // Create a command instance with the specified output target
    virtual OldStyleCommandPointer Create(
-      AudacityProject *project, std::unique_ptr<CommandOutputTargets> &&target) = 0;
+      AudacityProject &project, std::unique_ptr<CommandOutputTargets> &&target) = 0;
 };
 
 #endif /* End of include guard: __COMMANDTYPE__ */

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 0caa754.
Signed-off-by: Avery King <avery98@pm.me>

src/commands/BatchEvalCommand.cpp
@@ -39,10 +39,10 @@ void BatchEvalCommandType::BuildSignature(CommandSignature &signature)
    signature.AddParameter(wxT("MacroName"), wxT(""), std::move(macroValidator));
 }
 
-OldStyleCommandPointer BatchEvalCommandType::Create( TenacityProject *project,
+OldStyleCommandPointer BatchEvalCommandType::Create( TenacityProject &project,
    std::unique_ptr<CommandOutputTargets> && WXUNUSED(target))
 {
-   return std::make_shared<BatchEvalCommand>(*project, *this);
+   return std::make_shared<BatchEvalCommand>(project, *this);
 }
 
 bool BatchEvalCommand::Apply(const CommandContext & context)

src/commands/BatchEvalCommand.h
@@ -33,7 +33,7 @@ class BatchEvalCommandType final : public OldStyleCommandType
 public:
    ComponentInterfaceSymbol BuildName() override;
    void BuildSignature(CommandSignature &signature) override;
-   OldStyleCommandPointer Create( TenacityProject *project,
+   OldStyleCommandPointer Create( TenacityProject &project,
       std::unique_ptr<CommandOutputTargets> &&target) override;
 };
 

src/commands/CommandBuilder.cpp
@@ -31,13 +31,13 @@ system by constructing BatchCommandEval objects.
 #include "../shuttle/Shuttle.h"
 
 CommandBuilder::CommandBuilder(
-   TenacityProject *project, const wxString &cmdString)
+   TenacityProject &project, const wxString &cmdString)
    : mValid(false)
 {
    BuildCommand(project, cmdString);
 }
 
-CommandBuilder::CommandBuilder(TenacityProject *project,
+CommandBuilder::CommandBuilder(TenacityProject &project,
    const wxString &cmdName, const wxString &params)
    : mValid(false)
 {
@@ -82,7 +82,7 @@ void CommandBuilder::Success(const OldStyleCommandPointer &cmd)
    mValid = true;
 }
 
-void CommandBuilder::BuildCommand(TenacityProject *project,
+void CommandBuilder::BuildCommand(TenacityProject &project,
                                   const wxString &cmdName,
                                   const wxString &cmdParamsArg)
 {
@@ -188,7 +188,7 @@ void CommandBuilder::BuildCommand(TenacityProject *project,
 }
 
 void CommandBuilder::BuildCommand(
-   TenacityProject *project, const wxString &cmdStringArg)
+   TenacityProject &project, const wxString &cmdStringArg)
 {
    wxString cmdString(cmdStringArg);
 

src/commands/CommandBuilder.h
@@ -39,12 +39,12 @@ class CommandBuilder
 
       void Failure(const wxString &msg = {});
       void Success(const OldStyleCommandPointer &cmd);
-      void BuildCommand( TenacityProject *project,
+      void BuildCommand( TenacityProject &project,
          const wxString &cmdName, const wxString &cmdParams);
-      void BuildCommand( TenacityProject *project, const wxString &cmdString);
+      void BuildCommand( TenacityProject &project, const wxString &cmdString);
    public:
-      CommandBuilder(TenacityProject *project, const wxString &cmdString);
-      CommandBuilder(TenacityProject *project, const wxString &cmdName,
+      CommandBuilder(TenacityProject &project, const wxString &cmdString);
+      CommandBuilder(TenacityProject &project, const wxString &cmdName,
                      const wxString &cmdParams);
       ~CommandBuilder();
       bool WasValid();

src/commands/CommandType.h
@@ -66,7 +66,7 @@ public:
 
    // Create a command instance with the specified output target
    virtual OldStyleCommandPointer Create(
-      TenacityProject *project, std::unique_ptr<CommandOutputTargets> &&target) = 0;
+      TenacityProject &project, std::unique_ptr<CommandOutputTargets> &&target) = 0;
 };
 
 #endif /* End of include guard: __COMMANDTYPE__ */