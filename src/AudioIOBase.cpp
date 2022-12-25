alex74747/audacity

Separate Meter from MeterPanelBase...
... so the object pointed to by AudioIOBase need not be a window, and so
other implementations of Meter may be possible

src/AudioIOBase.cpp
@@ -309,32 +309,36 @@ void AudioIOBase::HandleDeviceChange()
 #endif   // USE_PORTMIXER
 }
 
-void AudioIOBase::SetCaptureMeter(AudacityProject *project, MeterPanelBase *meter)
+void AudioIOBase::SetCaptureMeter(
+   AudacityProject *project, const std::weak_ptr<Meter> &wMeter)
 {
    if (( mOwningProject ) && ( mOwningProject != project))
       return;
 
+   auto meter = wMeter.lock();
    if (meter)
    {
       mInputMeter = meter;
-      mInputMeter->Reset(mRate, true);
+      meter->Reset(mRate, true);
    }
    else
-      mInputMeter.Release();
+      mInputMeter.reset();
 }
 
-void AudioIOBase::SetPlaybackMeter(AudacityProject *project, MeterPanelBase *meter)
+void AudioIOBase::SetPlaybackMeter(
+   AudacityProject *project, const std::weak_ptr<Meter> &wMeter)
 {
    if (( mOwningProject ) && ( mOwningProject != project))
       return;
 
+   auto meter = wMeter.lock();
    if (meter)
    {
       mOutputMeter = meter;
-      mOutputMeter->Reset(mRate, true);
+      meter->Reset(mRate, true);
    }
    else
-      mOutputMeter.Release();
+      mOutputMeter.reset();
 }
 
 bool AudioIOBase::IsPaused() const

src/AudioIOBase.h
@@ -20,10 +20,10 @@ Paul Licameli split from AudioIO.h
 #include <memory>
 #include <vector>
 #include <wx/string.h>
-#include <wx/weakref.h> // member variable
 
 struct PaDeviceInfo;
 typedef void PaStream;
+#include "portaudio.h"
 
 #if USE_PORTMIXER
 typedef void PxMixer;
@@ -34,7 +34,7 @@ class AudioIOBase;
 class AudacityProject;
 class AudioIOListener;
 class BoundedEnvelope;
-class MeterPanelBase;
+class Meter;
 using PRCrossfadeData = std::vector< std::vector < float > >;
 
 #define BAD_STREAM_TIME (-DBL_MAX)
@@ -87,7 +87,7 @@ struct AudioIOStartStreamOptions
    {}
 
    AudacityProject *pProject{};
-   MeterPanelBase *captureMeter{}, *playbackMeter{};
+   std::weak_ptr<Meter> captureMeter, playbackMeter;
    const BoundedEnvelope *envelope; // for time warping
    std::shared_ptr< AudioIOListener > listener;
    double rate;
@@ -122,8 +122,10 @@ public:
 
    virtual ~AudioIOBase();
 
-   void SetCaptureMeter(AudacityProject *project, MeterPanelBase *meter);
-   void SetPlaybackMeter(AudacityProject *project, MeterPanelBase *meter);
+   void SetCaptureMeter(
+      AudacityProject *project, const std::weak_ptr<Meter> &meter);
+   void SetPlaybackMeter(
+      AudacityProject *project, const std::weak_ptr<Meter> &meter);
 
    /** \brief update state after changing what audio devices are selected
     *
@@ -281,8 +283,8 @@ protected:
 
    PaStream           *mPortStreamV19;
 
-   wxWeakRef<MeterPanelBase> mInputMeter{};
-   wxWeakRef<MeterPanelBase> mOutputMeter{};
+   std::weak_ptr<Meter> mInputMeter{};
+   std::weak_ptr<Meter> mOutputMeter{};
 
    #if USE_PORTMIXER
    PxMixer            *mPortMixer;

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit
f2f26db4abcc4d6390f763fb114052eacaf5844a. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/AudioIOBase.cpp
@@ -11,16 +11,12 @@ Paul Licameli split from AudioIO.cpp
 
 #include "AudioIOBase.h"
 
-
 #include <wx/log.h>
 #include <wx/sstream.h>
 #include <wx/txtstrm.h>
 
-// Tenacity libraries
-#include <lib-preferences/Prefs.h>
-#include <lib-utility/MemoryX.h>
-
-#include "widgets/MeterPanelBase.h"
+#include "Meter.h"
+#include "Prefs.h"
 
 #include <portaudio.h>
 
@@ -125,32 +121,36 @@ void AudioIOBase::HandleDeviceChange()
 
 }
 
-void AudioIOBase::SetCaptureMeter(TenacityProject *project, MeterPanelBase *meter)
+void AudioIOBase::SetCaptureMeter(
+   TenacityProject *project, const std::weak_ptr<Meter> &wMeter)
 {
    if (( mOwningProject ) && ( mOwningProject != project))
       return;
 
+   auto meter = wMeter.lock();
    if (meter)
    {
       mInputMeter = meter;
-      mInputMeter->Reset(mRate, true);
+      meter->Reset(mRate, true);
    }
    else
-      mInputMeter.Release();
+      mInputMeter.reset();
 }
 
-void AudioIOBase::SetPlaybackMeter(TenacityProject *project, MeterPanelBase *meter)
+void AudioIOBase::SetPlaybackMeter(
+   TenacityProject *project, const std::weak_ptr<Meter> &wMeter)
 {
    if (( mOwningProject ) && ( mOwningProject != project))
       return;
 
+   auto meter = wMeter.lock();
    if (meter)
    {
       mOutputMeter = meter;
-      mOutputMeter->Reset(mRate, true);
+      meter->Reset(mRate, true);
    }
    else
-      mOutputMeter.Release();
+      mOutputMeter.reset();
 }
 
 bool AudioIOBase::IsPaused() const

src/AudioIOBase.h
@@ -17,7 +17,6 @@ Paul Licameli split from AudioIO.h
 #include <vector>
 #include <limits>
 #include <wx/string.h>
-#include <wx/weakref.h> // member variable
 
 // Tenacity libraries
 #include <lib-utility/MemoryX.h>
@@ -32,9 +31,7 @@ class AudioIOBase;
 class TenacityProject;
 class AudioIOListener;
 class BoundedEnvelope;
-// Windows build needs complete type for parameter of wxWeakRef
-// class MeterPanelBase;
-#include "widgets/MeterPanelBase.h"
+class Meter;
 using PRCrossfadeData = std::vector< std::vector < float > >;
 
 constexpr double BAD_STREAM_TIME = -(std::numeric_limits<double>::max());
@@ -87,7 +84,7 @@ struct AudioIOStartStreamOptions
    {}
 
    TenacityProject *pProject{};
-   MeterPanelBase *captureMeter{}, *playbackMeter{};
+   std::weak_ptr<Meter> captureMeter, playbackMeter;
    const BoundedEnvelope *envelope; // for time warping
    std::shared_ptr< AudioIOListener > listener;
    double rate;
@@ -123,8 +120,10 @@ public:
 
    virtual ~AudioIOBase();
 
-   void SetCaptureMeter(TenacityProject *project, MeterPanelBase *meter);
-   void SetPlaybackMeter(TenacityProject *project, MeterPanelBase *meter);
+   void SetCaptureMeter(
+      TenacityProject *project, const std::weak_ptr<Meter> &meter);
+   void SetPlaybackMeter(
+      TenacityProject *project, const std::weak_ptr<Meter> &meter);
 
    /** \brief update state after changing what audio devices are selected
     *
@@ -266,8 +265,8 @@ protected:
 
    PaStream           *mPortStreamV19;
 
-   wxWeakRef<MeterPanelBase> mInputMeter{};
-   wxWeakRef<MeterPanelBase> mOutputMeter{};
+   std::weak_ptr<Meter> mInputMeter{};
+   std::weak_ptr<Meter> mOutputMeter{};
 
    // For cacheing supported sample rates
    static int mCachedPlaybackIndex;