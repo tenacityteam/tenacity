audacity/audacity

ProjectFileIORegistry is generated from a class template...
... And macros to help generate the one necessary function in it with linkage,
so that it works on Windows builds too.

Cause that function to be generated in Project.cpp.

Rename the files defining the template and its base class.

Now the base classes make no mention of class AudacityProject, not even as an
incomplete type.

Will this generality be needed?  Yes!  To merge Leland's experimental stackable
effects branch, but also to break some of the dependency cycles in it, this
template will be reused with Host = WaveTrack

src/Project.h
@@ -188,4 +188,10 @@ AUDACITY_DLL_API wxWindow &GetProjectPanel( AudacityProject &project );
 AUDACITY_DLL_API const wxWindow &GetProjectPanel(
    const AudacityProject &project );
 
+// Generate a registry for serialized data attached to the project
+#include "XMLMethodRegistry.h"
+class AudacityProject;
+using ProjectFileIORegistry = XMLMethodRegistry<AudacityProject>;
+DECLARE_XML_METHOD_REGISTRY( AUDACITY_DLL_API, ProjectFileIORegistry );
+
 #endif

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 8276b99. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/Project.h
@@ -189,4 +189,10 @@ TENACITY_DLL_API wxWindow &GetProjectPanel( TenacityProject &project );
 TENACITY_DLL_API const wxWindow &GetProjectPanel(
    const TenacityProject &project );
 
+// Generate a registry for serialized data attached to the project
+#include "XMLMethodRegistry.h"
+class TenacityProject;
+using ProjectFileIORegistry = XMLMethodRegistry<TenacityProject>;
+DECLARE_XML_METHOD_REGISTRY( TENACITY_DLL_API, ProjectFileIORegistry );
+
 #endif