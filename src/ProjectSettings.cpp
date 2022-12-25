alex74747/audacity

Move some project XML write steps into registry...
... and ProjectFileIO not dependent on Tags

src/ProjectSettings.cpp
@@ -14,8 +14,10 @@ Paul Licameli split from AudacityProject.cpp
 
 #include "AudioIOBase.h"
 #include "Project.h"
+#include "ProjectFileIORegistry.h"
 #include "widgets/NumericTextCtrl.h"
 #include "prefs/TracksBehaviorsPrefs.h"
+#include "xml/XMLWriter.h"
 
 wxDEFINE_EVENT(EVT_PROJECT_SETTINGS_CHANGE, wxCommandEvent);
 
@@ -205,3 +207,16 @@ void ProjectSettings::SetSyncLock(bool flag)
    }
 }
 
+static ProjectFileIORegistry::WriterEntry entry {
+[](const AudacityProject &project, XMLWriter &xmlFile){
+   auto &settings = ProjectSettings::Get(project);
+   xmlFile.WriteAttr(wxT("rate"), settings.GetRate());
+   xmlFile.WriteAttr(wxT("snapto"), settings.GetSnapTo() ? wxT("on") : wxT("off"));
+   xmlFile.WriteAttr(wxT("selectionformat"),
+                     settings.GetSelectionFormat().Internal());
+   xmlFile.WriteAttr(wxT("frequencyformat"),
+                     settings.GetFrequencySelectionFormatName().Internal());
+   xmlFile.WriteAttr(wxT("bandwidthformat"),
+                     settings.GetBandwidthSelectionFormatName().Internal());
+}
+};

src/Tags.cpp
@@ -568,3 +568,9 @@ void Tags::WriteXML(XMLWriter &xmlFile) const
 
    xmlFile.EndTag(wxT("tags"));
 }
+
+static ProjectFileIORegistry::WriterEntry entry {
+[](const AudacityProject &project, XMLWriter &xmlFile){
+   Tags::Get(project).WriteXML(xmlFile);
+}
+};

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 2388d8f. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ProjectSettings.cpp
@@ -16,6 +16,7 @@ Paul Licameli split from TenacityProject.cpp
 #include "prefs/QualitySettings.h"
 #include "prefs/TracksBehaviorsPrefs.h"
 #include "widgets/NumericTextCtrl.h"
+#include "XMLWriter.h"
 
 wxDEFINE_EVENT(EVT_PROJECT_SETTINGS_CHANGE, wxCommandEvent);
 
@@ -206,3 +207,16 @@ void ProjectSettings::SetSyncLock(bool flag)
    }
 }
 
+static ProjectFileIORegistry::WriterEntry entry {
+[](const TenacityProject &project, XMLWriter &xmlFile){
+   auto &settings = ProjectSettings::Get(project);
+   xmlFile.WriteAttr(wxT("rate"), settings.GetRate());
+   xmlFile.WriteAttr(wxT("snapto"), settings.GetSnapTo() ? wxT("on") : wxT("off"));
+   xmlFile.WriteAttr(wxT("selectionformat"),
+                     settings.GetSelectionFormat().Internal());
+   xmlFile.WriteAttr(wxT("frequencyformat"),
+                     settings.GetFrequencySelectionFormatName().Internal());
+   xmlFile.WriteAttr(wxT("bandwidthformat"),
+                     settings.GetBandwidthSelectionFormatName().Internal());
+}
+};

src/Tags.cpp
@@ -1538,3 +1538,9 @@ bool TagsEditorDialog::IsWindowRectValid(const wxRect *windowRect) const
 
    return true;
 }
+
+static ProjectFileIORegistry::WriterEntry entry {
+[](const TenacityProject &project, XMLWriter &xmlFile){
+   Tags::Get(project).WriteXML(xmlFile);
+}
+};