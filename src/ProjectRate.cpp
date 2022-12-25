audacity/audacity

Duplicate ProjectSettings.* in ProjectRate.*

src/ProjectRate.cpp
@@ -1,0 +1,252 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+ProjectSettings.cpp
+
+Paul Licameli split from AudacityProject.cpp
+
+**********************************************************************/
+
+#include "ProjectSettings.h"
+
+
+
+#include "AudioIOBase.h"
+#include "Project.h"
+#include "prefs/QualitySettings.h"
+#include "widgets/NumericTextCtrl.h"
+#include "prefs/TracksBehaviorsPrefs.h"
+#include "XMLWriter.h"
+
+wxDEFINE_EVENT(EVT_PROJECT_SETTINGS_CHANGE, wxCommandEvent);
+
+namespace {
+   void Notify( AudacityProject &project, ProjectSettings::EventCode code )
+   {
+      wxCommandEvent e{ EVT_PROJECT_SETTINGS_CHANGE };
+      e.SetInt( static_cast<int>( code ) );
+      project.ProcessEvent( e );
+   }
+}
+
+static const AudacityProject::AttachedObjects::RegisteredFactory
+sProjectSettingsKey{
+  []( AudacityProject &project ){
+     auto result = std::make_shared< ProjectSettings >( project );
+     return result;
+   }
+};
+
+ProjectSettings &ProjectSettings::Get( AudacityProject &project )
+{
+   return project.AttachedObjects::Get< ProjectSettings >(
+      sProjectSettingsKey );
+}
+
+const ProjectSettings &ProjectSettings::Get( const AudacityProject &project )
+{
+   return Get( const_cast< AudacityProject & >( project ) );
+}
+
+ProjectSettings::ProjectSettings(AudacityProject &project)
+   : mProject{ project }
+   , mSelectionFormat{ NumericTextCtrl::LookupFormat(
+      NumericConverter::TIME,
+      gPrefs->Read(wxT("/SelectionFormat"), wxT("")))
+}
+, mAudioTimeFormat{ NumericTextCtrl::LookupFormat(
+   NumericConverter::TIME,
+   gPrefs->Read(wxT("/AudioTimeFormat"), wxT("hh:mm:ss")))
+}
+, mFrequencySelectionFormatName{ NumericTextCtrl::LookupFormat(
+   NumericConverter::FREQUENCY,
+   gPrefs->Read(wxT("/FrequencySelectionFormatName"), wxT("")) )
+}
+, mBandwidthSelectionFormatName{ NumericTextCtrl::LookupFormat(
+   NumericConverter::BANDWIDTH,
+   gPrefs->Read(wxT("/BandwidthSelectionFormatName"), wxT("")) )
+}
+, mSnapTo( gPrefs->Read(wxT("/SnapTo"), SNAP_OFF) )
+{
+   int intRate = 0;
+   bool wasDefined = QualitySettings::DefaultSampleRate.Read( &intRate );
+   mRate = intRate;
+   if ( !wasDefined ) {
+      // The default given above can vary with host/devices. So unless there is
+      // an entry for the default sample rate in audacity.cfg, Audacity can open
+      // with a rate which is different from the rate with which it closed.
+      // See bug 1879.
+      QualitySettings::DefaultSampleRate.Write( mRate );
+      gPrefs->Flush();
+   }
+   gPrefs->Read(wxT("/GUI/SyncLockTracks"), &mIsSyncLocked, false);
+
+   bool multiToolActive = false;
+   gPrefs->Read(wxT("/GUI/ToolBars/Tools/MultiToolActive"), &multiToolActive);
+
+   if (multiToolActive)
+      mCurrentTool = ToolCodes::multiTool;
+   else
+      mCurrentTool = ToolCodes::selectTool;
+
+   UpdatePrefs();
+}
+
+void ProjectSettings::UpdatePrefs()
+{
+   gPrefs->Read(wxT("/AudioFiles/ShowId3Dialog"), &mShowId3Dialog, true);
+   gPrefs->Read(wxT("/GUI/EmptyCanBeDirty"), &mEmptyCanBeDirty, true);
+   gPrefs->Read(wxT("/GUI/ShowSplashScreen"), &mShowSplashScreen, true);
+   mSoloPref = TracksBehaviorsSolo.Read();
+   // Update the old default to the NEW default.
+   if (mSoloPref == wxT("Standard"))
+      mSoloPref = wxT("Simple");
+   gPrefs->Read(wxT("/GUI/TracksFitVerticallyZoomed"),
+      &mTracksFitVerticallyZoomed, false);
+   //   gPrefs->Read(wxT("/GUI/UpdateSpectrogram"),
+   //     &mViewInfo.bUpdateSpectrogram, true);
+
+   // This code to change an empty projects rate is currently disabled, after
+   // discussion.  The rule 'Default sample rate' only affects newly created
+   // projects was felt to be simpler and better.
+#if 0
+   // The DefaultProjectSample rate is the rate for new projects.
+   // Do not change this project's rate, unless there are no tracks.
+   if( TrackList::Get( *this ).size() == 0){
+      mRate = QualityDefaultSampleRate.Read();
+      // If necessary, we change this rate in the selection toolbar too.
+      auto bar = SelectionBar::Get( *this );
+      bar.SetRate( mRate );
+   }
+#endif
+}
+
+const NumericFormatSymbol &
+ProjectSettings::GetFrequencySelectionFormatName() const
+{
+   return mFrequencySelectionFormatName;
+}
+
+void ProjectSettings::SetFrequencySelectionFormatName(
+   const NumericFormatSymbol & formatName)
+{
+   mFrequencySelectionFormatName = formatName;
+}
+
+const NumericFormatSymbol &
+ProjectSettings::GetBandwidthSelectionFormatName() const
+{
+   return mBandwidthSelectionFormatName;
+}
+
+void ProjectSettings::SetBandwidthSelectionFormatName(
+   const NumericFormatSymbol & formatName)
+{
+   mBandwidthSelectionFormatName = formatName;
+}
+
+void ProjectSettings::SetSelectionFormat(const NumericFormatSymbol & format)
+{
+   mSelectionFormat = format;
+}
+
+const NumericFormatSymbol & ProjectSettings::GetSelectionFormat() const
+{
+   return mSelectionFormat;
+}
+
+void ProjectSettings::SetAudioTimeFormat(const NumericFormatSymbol & format)
+{
+   mAudioTimeFormat = format;
+}
+
+const NumericFormatSymbol & ProjectSettings::GetAudioTimeFormat() const
+{
+   return mAudioTimeFormat;
+}
+
+double ProjectSettings::GetRate() const
+{
+   return mRate;
+}
+
+void ProjectSettings::SetRate(double rate)
+{
+   auto &project = mProject;
+   if (rate != mRate) {
+      mRate = rate;
+      Notify( project, ChangedProjectRate );
+   }
+}
+
+void ProjectSettings::SetSnapTo(int snap)
+{
+   mSnapTo = snap;
+}
+   
+int ProjectSettings::GetSnapTo() const
+{
+   return mSnapTo;
+}
+
+bool ProjectSettings::IsSyncLocked() const
+{
+#ifdef EXPERIMENTAL_SYNC_LOCK
+   return mIsSyncLocked;
+#else
+   return false;
+#endif
+}
+
+void ProjectSettings::SetSyncLock(bool flag)
+{
+   auto &project = mProject;
+   if (flag != mIsSyncLocked) {
+      mIsSyncLocked = flag;
+      Notify( project, ChangedSyncLock );
+   }
+}
+
+static ProjectFileIORegistry::WriterEntry entry {
+[](const AudacityProject &project, XMLWriter &xmlFile){
+   auto &settings = ProjectSettings::Get(project);
+   xmlFile.WriteAttr(wxT("rate"), settings.GetRate());
+   xmlFile.WriteAttr(wxT("snapto"), settings.GetSnapTo() ? wxT("on") : wxT("off"));
+   xmlFile.WriteAttr(wxT("selectionformat"),
+                     settings.GetSelectionFormat().Internal());
+   xmlFile.WriteAttr(wxT("frequencyformat"),
+                     settings.GetFrequencySelectionFormatName().Internal());
+   xmlFile.WriteAttr(wxT("bandwidthformat"),
+                     settings.GetBandwidthSelectionFormatName().Internal());
+}
+};
+
+static ProjectFileIORegistry::AttributeReaderEntries entries {
+// Just a pointer to function, but needing overload resolution as non-const:
+(ProjectSettings& (*)(AudacityProject &)) &ProjectSettings::Get, {
+   { L"rate", [](auto &settings, auto value){
+      double rate;
+      Internat::CompatibleToDouble(value, &rate);
+      settings.SetRate( rate );
+   } },
+
+   // PRL:  The following have persisted as per-project settings for long.
+   // Maybe that should be abandoned.  Enough to save changes in the user
+   // preference file.
+   { L"snapto", [](auto &settings, auto value){
+      settings.SetSnapTo(wxString(value) == wxT("on") ? true : false);
+   } },
+   { L"selectionformat", [](auto &settings, auto value){
+      settings.SetSelectionFormat(
+         NumericConverter::LookupFormat( NumericConverter::TIME, value) );
+   } },
+   { L"frequencyformat", [](auto &settings, auto value){
+      settings.SetFrequencySelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::FREQUENCY, value ) );
+   } },
+   { L"bandwidthformat", [](auto &settings, auto value){
+      settings.SetBandwidthSelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::BANDWIDTH, value ) );
+   } },
+} };

src/ProjectRate.h
@@ -1,0 +1,151 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+ProjectSettings.h
+
+Paul Licameli split from AudacityProject.h
+
+**********************************************************************/
+
+#ifndef __AUDACITY_PROJECT_SETTINGS__
+#define __AUDACITY_PROJECT_SETTINGS__
+
+#include <atomic>
+#include <wx/event.h> // to declare custom event type
+
+#include "ClientData.h" // to inherit
+#include "Prefs.h" // to inherit
+#include "audacity/Types.h"
+
+class AudacityProject;
+
+// Sent to the project when certain settings change
+wxDECLARE_EXPORTED_EVENT(AUDACITY_DLL_API,
+   EVT_PROJECT_SETTINGS_CHANGE, wxCommandEvent);
+
+enum
+{
+   SNAP_OFF,
+   SNAP_NEAREST,
+   SNAP_PRIOR
+};
+
+namespace ToolCodes {
+enum {
+   selectTool,
+   envelopeTool,
+   drawTool,
+   zoomTool,
+   slideTool,
+   multiTool,
+   numTools,
+   
+   firstTool = selectTool,
+   lastTool = multiTool,
+};
+}
+
+///\brief Holds various per-project settings values, including the sample rate,
+/// and sends events to the project when certain values change
+class AUDACITY_DLL_API ProjectSettings final
+   : public ClientData::Base
+   , private PrefsListener
+{
+public:
+   static ProjectSettings &Get( AudacityProject &project );
+   static const ProjectSettings &Get( const AudacityProject &project );
+   
+   // Values retrievable from GetInt() of the event for settings change
+   enum EventCode : int {
+      ChangedSyncLock,
+      ChangedProjectRate
+   };
+
+   explicit ProjectSettings( AudacityProject &project );
+   ProjectSettings( const ProjectSettings & ) PROHIBITED;
+   ProjectSettings &operator=( const ProjectSettings & ) PROHIBITED;
+
+
+   bool GetTracksFitVerticallyZoomed() const { return mTracksFitVerticallyZoomed; } //lda
+   void SetTracksFitVerticallyZoomed(bool flag) { mTracksFitVerticallyZoomed = flag; } //lda
+
+   bool GetShowId3Dialog() const { return mShowId3Dialog; } //lda
+   void SetShowId3Dialog(bool flag) { mShowId3Dialog = flag; } //lda
+
+   bool IsSyncLocked() const;
+   void SetSyncLock(bool flag);
+   
+   // Rate
+
+   void SetRate(double rate);
+   double GetRate() const;
+
+   // Snap To
+
+   void SetSnapTo(int snap);
+   int GetSnapTo() const;
+
+   // Current tool
+
+   void SetTool(int tool) { mCurrentTool = tool; }
+   int GetTool() const { return mCurrentTool; }
+
+   // Speed play
+   double GetPlaySpeed() const {
+      return mPlaySpeed.load( std::memory_order_relaxed ); }
+   void SetPlaySpeed( double value ) {
+      mPlaySpeed.store( value, std::memory_order_relaxed ); }
+
+   // Selection Format
+   void SetSelectionFormat(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetSelectionFormat() const;
+
+   // AudioTime format
+   void SetAudioTimeFormat(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetAudioTimeFormat() const;
+
+   // Spectral Selection Formats
+   void SetFrequencySelectionFormatName(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetFrequencySelectionFormatName() const;
+
+   void SetBandwidthSelectionFormatName(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetBandwidthSelectionFormatName() const;
+
+   bool IsSoloSimple() const { return mSoloPref == wxT("Simple"); }
+   bool IsSoloNone() const { return mSoloPref == wxT("None"); }
+
+   bool EmptyCanBeDirty() const { return mEmptyCanBeDirty; }
+
+   bool GetShowSplashScreen() const { return mShowSplashScreen; }
+
+private:
+   void UpdatePrefs() override;
+
+   AudacityProject &mProject;
+
+   NumericFormatSymbol mSelectionFormat;
+   NumericFormatSymbol mFrequencySelectionFormatName;
+   NumericFormatSymbol mBandwidthSelectionFormatName;
+   NumericFormatSymbol mAudioTimeFormat;
+
+   wxString mSoloPref;
+
+   double mRate;
+
+   // This is atomic because scrubber may read it in a separate thread from
+   // the main
+   std::atomic<double> mPlaySpeed{};
+
+   int mSnapTo;
+
+   int mCurrentTool;
+   
+   bool mTracksFitVerticallyZoomed{ false };  //lda
+   bool mShowId3Dialog{ true }; //lda
+   bool mIsSyncLocked{ false };
+   bool mEmptyCanBeDirty;
+   bool mShowSplashScreen;
+};
+
+#endif

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit dbc5673. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ProjectRate.cpp
@@ -1,0 +1,252 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+ProjectSettings.cpp
+
+Paul Licameli split from TenacityProject.cpp
+
+**********************************************************************/
+
+#include "ProjectSettings.h"
+
+
+
+#include "AudioIOBase.h"
+#include "Project.h"
+#include "prefs/QualitySettings.h"
+#include "widgets/NumericTextCtrl.h"
+#include "prefs/TracksBehaviorsPrefs.h"
+#include "XMLWriter.h"
+
+wxDEFINE_EVENT(EVT_PROJECT_SETTINGS_CHANGE, wxCommandEvent);
+
+namespace {
+   void Notify( TenacityProject &project, ProjectSettings::EventCode code )
+   {
+      wxCommandEvent e{ EVT_PROJECT_SETTINGS_CHANGE };
+      e.SetInt( static_cast<int>( code ) );
+      project.ProcessEvent( e );
+   }
+}
+
+static const TenacityProject::AttachedObjects::RegisteredFactory
+sProjectSettingsKey{
+  []( TenacityProject &project ){
+     auto result = std::make_shared< ProjectSettings >( project );
+     return result;
+   }
+};
+
+ProjectSettings &ProjectSettings::Get( TenacityProject &project )
+{
+   return project.AttachedObjects::Get< ProjectSettings >(
+      sProjectSettingsKey );
+}
+
+const ProjectSettings &ProjectSettings::Get( const TenacityProject &project )
+{
+   return Get( const_cast< TenacityProject & >( project ) );
+}
+
+ProjectSettings::ProjectSettings(TenacityProject &project)
+   : mProject{ project }
+   , mSelectionFormat{ NumericTextCtrl::LookupFormat(
+      NumericConverter::TIME,
+      gPrefs->Read(wxT("/SelectionFormat"), wxT("")))
+}
+, mAudioTimeFormat{ NumericTextCtrl::LookupFormat(
+   NumericConverter::TIME,
+   gPrefs->Read(wxT("/AudioTimeFormat"), wxT("hh:mm:ss")))
+}
+, mFrequencySelectionFormatName{ NumericTextCtrl::LookupFormat(
+   NumericConverter::FREQUENCY,
+   gPrefs->Read(wxT("/FrequencySelectionFormatName"), wxT("")) )
+}
+, mBandwidthSelectionFormatName{ NumericTextCtrl::LookupFormat(
+   NumericConverter::BANDWIDTH,
+   gPrefs->Read(wxT("/BandwidthSelectionFormatName"), wxT("")) )
+}
+, mSnapTo( gPrefs->Read(wxT("/SnapTo"), SNAP_OFF) )
+{
+   int intRate = 0;
+   bool wasDefined = QualitySettings::DefaultSampleRate.Read( &intRate );
+   mRate = intRate;
+   if ( !wasDefined ) {
+      // The default given above can vary with host/devices. So unless there is
+      // an entry for the default sample rate in audacity.cfg, Audacity can open
+      // with a rate which is different from the rate with which it closed.
+      // See bug 1879.
+      QualitySettings::DefaultSampleRate.Write( mRate );
+      gPrefs->Flush();
+   }
+   gPrefs->Read(wxT("/GUI/SyncLockTracks"), &mIsSyncLocked, false);
+
+   bool multiToolActive = false;
+   gPrefs->Read(wxT("/GUI/ToolBars/Tools/MultiToolActive"), &multiToolActive);
+
+   if (multiToolActive)
+      mCurrentTool = ToolCodes::multiTool;
+   else
+      mCurrentTool = ToolCodes::selectTool;
+
+   UpdatePrefs();
+}
+
+void ProjectSettings::UpdatePrefs()
+{
+   gPrefs->Read(wxT("/AudioFiles/ShowId3Dialog"), &mShowId3Dialog, true);
+   gPrefs->Read(wxT("/GUI/EmptyCanBeDirty"), &mEmptyCanBeDirty, true);
+   gPrefs->Read(wxT("/GUI/ShowSplashScreen"), &mShowSplashScreen, true);
+   mSoloPref = TracksBehaviorsSolo.Read();
+   // Update the old default to the NEW default.
+   if (mSoloPref == wxT("Standard"))
+      mSoloPref = wxT("Simple");
+   gPrefs->Read(wxT("/GUI/TracksFitVerticallyZoomed"),
+      &mTracksFitVerticallyZoomed, false);
+   //   gPrefs->Read(wxT("/GUI/UpdateSpectrogram"),
+   //     &mViewInfo.bUpdateSpectrogram, true);
+
+   // This code to change an empty projects rate is currently disabled, after
+   // discussion.  The rule 'Default sample rate' only affects newly created
+   // projects was felt to be simpler and better.
+#if 0
+   // The DefaultProjectSample rate is the rate for new projects.
+   // Do not change this project's rate, unless there are no tracks.
+   if( TrackList::Get( *this ).size() == 0){
+      mRate = QualityDefaultSampleRate.Read();
+      // If necessary, we change this rate in the selection toolbar too.
+      auto bar = SelectionBar::Get( *this );
+      bar.SetRate( mRate );
+   }
+#endif
+}
+
+const NumericFormatSymbol &
+ProjectSettings::GetFrequencySelectionFormatName() const
+{
+   return mFrequencySelectionFormatName;
+}
+
+void ProjectSettings::SetFrequencySelectionFormatName(
+   const NumericFormatSymbol & formatName)
+{
+   mFrequencySelectionFormatName = formatName;
+}
+
+const NumericFormatSymbol &
+ProjectSettings::GetBandwidthSelectionFormatName() const
+{
+   return mBandwidthSelectionFormatName;
+}
+
+void ProjectSettings::SetBandwidthSelectionFormatName(
+   const NumericFormatSymbol & formatName)
+{
+   mBandwidthSelectionFormatName = formatName;
+}
+
+void ProjectSettings::SetSelectionFormat(const NumericFormatSymbol & format)
+{
+   mSelectionFormat = format;
+}
+
+const NumericFormatSymbol & ProjectSettings::GetSelectionFormat() const
+{
+   return mSelectionFormat;
+}
+
+void ProjectSettings::SetAudioTimeFormat(const NumericFormatSymbol & format)
+{
+   mAudioTimeFormat = format;
+}
+
+const NumericFormatSymbol & ProjectSettings::GetAudioTimeFormat() const
+{
+   return mAudioTimeFormat;
+}
+
+double ProjectSettings::GetRate() const
+{
+   return mRate;
+}
+
+void ProjectSettings::SetRate(double rate)
+{
+   auto &project = mProject;
+   if (rate != mRate) {
+      mRate = rate;
+      Notify( project, ChangedProjectRate );
+   }
+}
+
+void ProjectSettings::SetSnapTo(int snap)
+{
+   mSnapTo = snap;
+}
+   
+int ProjectSettings::GetSnapTo() const
+{
+   return mSnapTo;
+}
+
+bool ProjectSettings::IsSyncLocked() const
+{
+#ifdef EXPERIMENTAL_SYNC_LOCK
+   return mIsSyncLocked;
+#else
+   return false;
+#endif
+}
+
+void ProjectSettings::SetSyncLock(bool flag)
+{
+   auto &project = mProject;
+   if (flag != mIsSyncLocked) {
+      mIsSyncLocked = flag;
+      Notify( project, ChangedSyncLock );
+   }
+}
+
+static ProjectFileIORegistry::WriterEntry entry {
+[](const TenacityProject &project, XMLWriter &xmlFile){
+   auto &settings = ProjectSettings::Get(project);
+   xmlFile.WriteAttr(wxT("rate"), settings.GetRate());
+   xmlFile.WriteAttr(wxT("snapto"), settings.GetSnapTo() ? wxT("on") : wxT("off"));
+   xmlFile.WriteAttr(wxT("selectionformat"),
+                     settings.GetSelectionFormat().Internal());
+   xmlFile.WriteAttr(wxT("frequencyformat"),
+                     settings.GetFrequencySelectionFormatName().Internal());
+   xmlFile.WriteAttr(wxT("bandwidthformat"),
+                     settings.GetBandwidthSelectionFormatName().Internal());
+}
+};
+
+static ProjectFileIORegistry::AttributeReaderEntries entries {
+// Just a pointer to function, but needing overload resolution as non-const:
+(ProjectSettings& (*)(TenacityProject &)) &ProjectSettings::Get, {
+   { L"rate", [](auto &settings, auto value){
+      double rate;
+      Internat::CompatibleToDouble(value, &rate);
+      settings.SetRate( rate );
+   } },
+
+   // PRL:  The following have persisted as per-project settings for long.
+   // Maybe that should be abandoned.  Enough to save changes in the user
+   // preference file.
+   { L"snapto", [](auto &settings, auto value){
+      settings.SetSnapTo(wxString(value) == wxT("on") ? true : false);
+   } },
+   { L"selectionformat", [](auto &settings, auto value){
+      settings.SetSelectionFormat(
+         NumericConverter::LookupFormat( NumericConverter::TIME, value) );
+   } },
+   { L"frequencyformat", [](auto &settings, auto value){
+      settings.SetFrequencySelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::FREQUENCY, value ) );
+   } },
+   { L"bandwidthformat", [](auto &settings, auto value){
+      settings.SetBandwidthSelectionFormatName(
+         NumericConverter::LookupFormat( NumericConverter::BANDWIDTH, value ) );
+   } },
+} };

src/ProjectRate.h
@@ -1,0 +1,151 @@
+/**********************************************************************
+
+Audacity: A Digital Audio Editor
+
+ProjectSettings.h
+
+Paul Licameli split from TenacityProject.h
+
+**********************************************************************/
+
+#ifndef __AUDACITY_PROJECT_SETTINGS__
+#define __AUDACITY_PROJECT_SETTINGS__
+
+#include <atomic>
+#include <wx/event.h> // to declare custom event type
+
+#include "ClientData.h" // to inherit
+#include "Prefs.h" // to inherit
+#include "audacity/Types.h"
+
+class TenacityProject;
+
+// Sent to the project when certain settings change
+wxDECLARE_EXPORTED_EVENT(TENACITY_DLL_API,
+   EVT_PROJECT_SETTINGS_CHANGE, wxCommandEvent);
+
+enum
+{
+   SNAP_OFF,
+   SNAP_NEAREST,
+   SNAP_PRIOR
+};
+
+namespace ToolCodes {
+enum {
+   selectTool,
+   envelopeTool,
+   drawTool,
+   zoomTool,
+   slideTool,
+   multiTool,
+   numTools,
+   
+   firstTool = selectTool,
+   lastTool = multiTool,
+};
+}
+
+///\brief Holds various per-project settings values, including the sample rate,
+/// and sends events to the project when certain values change
+class TENACITY_DLL_API ProjectSettings final
+   : public ClientData::Base
+   , private PrefsListener
+{
+public:
+   static ProjectSettings &Get( TenacityProject &project );
+   static const ProjectSettings &Get( const TenacityProject &project );
+   
+   // Values retrievable from GetInt() of the event for settings change
+   enum EventCode : int {
+      ChangedSyncLock,
+      ChangedProjectRate
+   };
+
+   explicit ProjectSettings( TenacityProject &project );
+   ProjectSettings( const ProjectSettings & ) PROHIBITED;
+   ProjectSettings &operator=( const ProjectSettings & ) PROHIBITED;
+
+
+   bool GetTracksFitVerticallyZoomed() const { return mTracksFitVerticallyZoomed; } //lda
+   void SetTracksFitVerticallyZoomed(bool flag) { mTracksFitVerticallyZoomed = flag; } //lda
+
+   bool GetShowId3Dialog() const { return mShowId3Dialog; } //lda
+   void SetShowId3Dialog(bool flag) { mShowId3Dialog = flag; } //lda
+
+   bool IsSyncLocked() const;
+   void SetSyncLock(bool flag);
+   
+   // Rate
+
+   void SetRate(double rate);
+   double GetRate() const;
+
+   // Snap To
+
+   void SetSnapTo(int snap);
+   int GetSnapTo() const;
+
+   // Current tool
+
+   void SetTool(int tool) { mCurrentTool = tool; }
+   int GetTool() const { return mCurrentTool; }
+
+   // Speed play
+   double GetPlaySpeed() const {
+      return mPlaySpeed.load( std::memory_order_relaxed ); }
+   void SetPlaySpeed( double value ) {
+      mPlaySpeed.store( value, std::memory_order_relaxed ); }
+
+   // Selection Format
+   void SetSelectionFormat(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetSelectionFormat() const;
+
+   // AudioTime format
+   void SetAudioTimeFormat(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetAudioTimeFormat() const;
+
+   // Spectral Selection Formats
+   void SetFrequencySelectionFormatName(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetFrequencySelectionFormatName() const;
+
+   void SetBandwidthSelectionFormatName(const NumericFormatSymbol & format);
+   const NumericFormatSymbol & GetBandwidthSelectionFormatName() const;
+
+   bool IsSoloSimple() const { return mSoloPref == wxT("Simple"); }
+   bool IsSoloNone() const { return mSoloPref == wxT("None"); }
+
+   bool EmptyCanBeDirty() const { return mEmptyCanBeDirty; }
+
+   bool GetShowSplashScreen() const { return mShowSplashScreen; }
+
+private:
+   void UpdatePrefs() override;
+
+   TenacityProject &mProject;
+
+   NumericFormatSymbol mSelectionFormat;
+   NumericFormatSymbol mFrequencySelectionFormatName;
+   NumericFormatSymbol mBandwidthSelectionFormatName;
+   NumericFormatSymbol mAudioTimeFormat;
+
+   wxString mSoloPref;
+
+   double mRate;
+
+   // This is atomic because scrubber may read it in a separate thread from
+   // the main
+   std::atomic<double> mPlaySpeed{};
+
+   int mSnapTo;
+
+   int mCurrentTool;
+   
+   bool mTracksFitVerticallyZoomed{ false };  //lda
+   bool mShowId3Dialog{ true }; //lda
+   bool mIsSyncLocked{ false };
+   bool mEmptyCanBeDirty;
+   bool mShowSplashScreen;
+};
+
+#endif