audacity/audacity

Project has fewer dependencies...
... when we move the check at construction time for sufficient disk space into
one of the attached objects instead.  The BuildAll call will now include this
check.

src/ProjectFileIO.cpp
@@ -283,6 +283,24 @@ ProjectFileIO::ProjectFileIO(AudacityProject &project)
    mTemporary = true;
 
    UpdatePrefs();
+
+   // Make sure there is plenty of space for Sqlite files
+   wxLongLong freeSpace = 0;
+
+   auto path = TempDirectory::TempDir();
+   if (wxGetDiskSpace(path, NULL, &freeSpace)) {
+      if (freeSpace < wxLongLong(wxLL(100 * 1048576))) {
+         auto volume = FileNames::AbbreviatePath( path );
+         /* i18n-hint: %s will be replaced by the drive letter (on Windows) */
+         BasicUI::ShowErrorDialog( {},
+            XO("Warning"),
+            XO("There is very little free disk space left on %s\n"
+               "Please select a bigger temporary directory location in\n"
+               "Directories Preferences.").Format( volume ),
+            "Error:_Disk_full_or_not_writable"
+            );
+      }
+   }
 }
 
 ProjectFileIO::~ProjectFileIO()

tenacityteam/tenacity

===============================================================================
Cherry-picked from Audacity commit 30e7434. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/ProjectFileIO.cpp
@@ -287,6 +287,24 @@ ProjectFileIO::ProjectFileIO(TenacityProject &project)
    mTemporary = true;
 
    UpdatePrefs();
+
+   // Make sure there is plenty of space for Sqlite files
+   wxLongLong freeSpace = 0;
+
+   auto path = TempDirectory::TempDir();
+   if (wxGetDiskSpace(path, NULL, &freeSpace)) {
+      if (freeSpace < wxLongLong(wxLL(100 * 1048576))) {
+         auto volume = FileNames::AbbreviatePath( path );
+         /* i18n-hint: %s will be replaced by the drive letter (on Windows) */
+         GenericUI::ShowErrorDialog( {},
+            XO("Warning"),
+            XO("There is very little free disk space left on %s\n"
+               "Please select a bigger temporary directory location in\n"
+               "Directories Preferences.").Format( volume ),
+            "Error:_Disk_full_or_not_writable"
+            );
+      }
+   }
 }
 
 ProjectFileIO::~ProjectFileIO()