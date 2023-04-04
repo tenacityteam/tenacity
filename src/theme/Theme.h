audacity/audacity

Eliminate string literal repetition with a BoolSetting object

src/Theme.h
@@ -20,6 +20,8 @@
 #include <wx/defs.h>
 #include <wx/window.h> // to inherit
 
+#include "Prefs.h"
+
 class wxArrayString;
 class wxBitmap;
 class wxColour;
@@ -192,6 +194,10 @@ public:
 };
 
 extern AUDACITY_DLL_API Theme theTheme;
+
+extern AUDACITY_DLL_API BoolSetting
+     GUIBlendThemes
+;
 
 extern AUDACITY_DLL_API ChoiceSetting
      GUITheme

tenacityteam/tenacity

=======================================================================
Cherry-picked from Audacity commit 7bf437d. Modified for Tenacity.
Signed-off-by: Avery King <avery98@pm.me>

src/theme/Theme.h
@@ -21,6 +21,9 @@
 #include "ThemeFlags.h"
 #include "FlowPacker.h"
 
+// Tenacity libraries
+#include <lib-preferences/Prefs.h>
+
 class wxArrayString;
 class wxBitmap;
 class wxColour;
@@ -114,6 +117,10 @@ class wxString;
 class wxPaintEvent;
 
 extern TENACITY_DLL_API Theme theTheme;
+
+extern TENACITY_DLL_API BoolSetting
+     GUIBlendThemes
+;
 
 extern TENACITY_DLL_API ChoiceSetting
      GUITheme