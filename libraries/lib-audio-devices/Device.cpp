a7c8c7601276152c7992f8890b8fcc58dc800647
Avery King <avery98@pm.me>
2023-07-17 14:33:30 -0700

    Add Device class to replace DeviceSourceMap
    Signed-off-by: Avery King <avery98@pm.me>

---
+++ libraries/lib-audio-devices/Device.cpp
@@ -0,0 +1,96 @@
+/**********************************************************************
+
+  Tenacity
+
+  Device.cpp
+
+  Avery King
+
+  License: GPL v2 or later
+
+**********************************************************************/
+
+#include "Device.h"
+
+Device::Device() : mDeviceType{Device::Type::Null}, mName{"(null)"},
+                   mHostName{"(null)"}, mDeviceIndex{-1}, mHostIndex{-1},
+                   mNumChannels{-1}
+{
+}
+
+Device::Device(const Device& other)
+{
+    mDeviceType  = other.mDeviceType;
+    mName        = other.mName;
+    mHostName    = other.mHostName;
+    mDeviceIndex = other.mDeviceIndex;
+    mHostIndex   = other.mHostIndex;
+    mNumChannels = other.mNumChannels;
+}
+
+Device::operator bool() const
+{
+    return mDeviceType  != Device::Type::Null || mName != "(null)"   ||
+           mHostName    != "(null)"           || mDeviceIndex  != -1 ||
+           mNumChannels != -1;
+}
+
+void Device::SetDeviceType(Type type) noexcept
+{
+    mDeviceType = type;
+}
+
+void Device::SetName(const std::string& name) noexcept
+{
+    mName = name;
+}
+
+void Device::SetHostName(const std::string& hostName) noexcept
+{
+    mHostName = hostName;
+}
+
+void Device::SetNumChannels(int channels) noexcept
+{
+    mNumChannels = channels;
+}
+
+void Device::SetDeviceIndex(int index) noexcept
+{
+    mDeviceIndex = index;
+}
+
+void Device::SetHostIndex(int hostIndex) noexcept
+{
+    mHostIndex = hostIndex;
+}
+
+Device::Type Device::GetDeviceType() const
+{
+    return mDeviceType;
+}
+
+std::string Device::GetName() const
+{
+    return mName;
+}
+
+std::string Device::GetHostName() const
+{
+    return mHostName;
+}
+
+int Device::GetNumChannels() const
+{
+    return mNumChannels;
+}
+
+int Device::GetDeviceIndex() const
+{
+    return mDeviceIndex;
+}
+
+int Device::GetHostIndex() const
+{
+    return mHostIndex;
+}
---
+++ libraries/lib-audio-devices/Device.h
@@ -0,0 +1,122 @@
+/**********************************************************************
+
+  Tenacity
+
+  Device.h
+
+  Avery King
+
+  License: GPL v2 or later
+
+**********************************************************************/
+
+#pragma once
+
+#include <string>
+
+/** @brief Represents an input or output device in Tenacity.
+ * 
+ * This is modified from DeviceSourceMap in Tenacity. This is basically a
+ * glorified version of that class apart from the addition of device types.
+ * 
+ */
+class Device
+{
+    public:
+        enum class Type
+        {
+            Input,
+            Output,
+            Null,   /// The Device object is invalid
+            Dummy   /// Not a real device, but still valid
+        };
+
+    private:
+        Type mDeviceType; /// Device type.
+        std::string mName; /// Name of the device, previously deviceString.
+        std::string mHostName; /// Name of the host, previously hostString.
+        int mDeviceIndex; /// PortAudio device index.
+        int mHostIndex; /// PortAudio host index for the device.
+        int mNumChannels; /// Number of input or output channels.
+
+    public:
+        Device();
+        ~Device() = default;
+
+        Device(const Device& other);
+        Device(const Device&&) = delete;
+
+        operator bool() const;
+
+        /** @brief Sets the device type.
+         * 
+         * @warning This function is only meant for DeviceManager or unless
+         * you're constructing a dummy device. Only use it if you know what
+         * you're doing!
+         * 
+        */
+        void SetDeviceType(Type type) noexcept;
+
+        /** @brief Sets the device name.
+         * 
+         * @warning This function is only meant for DeviceManager or unless
+         * you're constructing a dummy device. Only use it if you know what
+         * you're doing!
+         * 
+        */
+        void SetName(const std::string& name) noexcept;
+
+        /** @brief Sets the device's host name.
+         * 
+         * @warning This function is only meant for DeviceManager or unless
+         * you're constructing a dummy device. Only use it if you know what
+         * you're doing!
+         * 
+        */
+        void SetHostName(const std::string& hostName) noexcept;
+
+        /** @brief Sets the device's number of channels.
+         * 
+         * @warning This function is only meant for DeviceManager or unless
+         * you're constructing a dummy device. Only use it if you know what
+         * you're doing!
+         * 
+        */
+        void SetNumChannels(int channels) noexcept;
+
+        /** @brief Sets the device's PortAudio device index.
+         * 
+         * @warning This function is only meant for DeviceManager or unless
+         * you're constructing a dummy device. Only use it if you know what
+         * you're doing!
+         * 
+        */
+        void SetDeviceIndex(int index) noexcept;
+
+        /** @brief Sets the device's PortAudio host index.
+         * 
+         * @warning This function is only meant for DeviceManager or unless
+         * you're constructing a dummy device. Only use it if you know what
+         * you're doing!
+         * 
+        */
+        void SetHostIndex(int hostIndex) noexcept;
+
+        /// Returns the device's type. 
+        Type GetDeviceType() const;
+
+        /// Returns the device's name.
+        std::string GetName() const;
+
+        /// Return's the host name of the current device.
+        std::string GetHostName() const;
+
+        /// Returns the number of channels for the device.
+        int GetNumChannels() const;
+
+        /// Returns the device's PortAudio device index.
+        int GetDeviceIndex() const;
+
+        /// Returns the device's PortAudio host index.
+        int GetHostIndex() const;
+};
