tenacityteam/tenacity

commit 05bdc06309a29a0c9609867cf32d79f6e5475fe4

Update images
Signed-off-by: Avery King <avery98@pm.me>

win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake
@@ -4,18 +4,21 @@
 # SOURCE_DIR - should be set to CMAKE_SOURCE_DIR by teh caller
 # OUTPUT_DIR - directory, where installer will be built
 # INNO_SETUP_COMPILER - InnoSetup compiler executable
-# BUILDING_64_BIT - Flag, that indicates that we are building a 64-bit installer
+# IS_64_BIT - Flag, that indicates that we are building a 64-bit installer
 # EMBED_MANUAL - embed a fresh copy of manual
 # SIGN - sign the installer
 # WINDOWS_CERTIFICATE - path to PFX file. If not present, env:WINDOWS_CERTIFICATE will be used
 # WINDOWS_CERTIFICATE_PASSWORD - password for the PFX file. If not present, env:WINDOWS_CERTIFICATE_PASSWORD will be used
 
-if( BUILDING_64_BIT )
+# Allow if statements to use the new IN_LIST operator (compatibility override for CMake <3.3)
+cmake_policy( SET CMP0057 NEW )
+
+if( IS_64_BIT )
     set( INSTALLER_SUFFIX "x64" )
-    set( INSTALLER_X64_MODE "ArchitecturesInstallIn64BitMode=x64" )
+    set( INSTALLER_X64_MODE "ArchitecturesInstallIn64BitMode=x64")
 else()
     set( INSTALLER_SUFFIX "x86" )
-    set( INSTALLER_X64_MODE "" )
+    set( INSTALLER_X64_MODE "ArchitecturesInstallIn64BitMode=x64")
 endif()
 
 if( SIGN )
@@ -48,32 +51,76 @@ configure_file("${OUTPUT_DIR}/tenacity.iss.in" "${OUTPUT_DIR}/tenacity.iss")
 
 file(COPY "${SOURCE_DIR}/presets" DESTINATION "${OUTPUT_DIR}/Additional")
 
-file(COPY 
+file(COPY
         "${SOURCE_DIR}/LICENSE.txt"
-        "${SOURCE_DIR}/README.md"
+        "${SOURCE_DIR}/win/README.rtf"
         "${SOURCE_DIR}/win/tenacity.ico"
-    DESTINATION 
+        "${SOURCE_DIR}/win/darktenacity.ico"
+    DESTINATION
         "${OUTPUT_DIR}/Additional"
 )
 
+if( DEFINED CMAKE_BUILD_TYPE )
+  if( EXISTS "${BUILD_DIR}/bin/${CMAKE_BUILD_TYPE}" )
+    # Use CMAKE_BUILD_TYPE value if defined and previously built
+    set( INNOSETUP_BUILD_CONFIGURATIONS "${CMAKE_BUILD_TYPE};" )
+  else()
+    message( FATAL_ERROR "You defined a value for CMAKE_BUILD_TYPE which has not already been built, please build it and try again" )
+  endif()
+elseif( DEFINED CMAKE_INSTALL_CONFIG_NAME )
+  if( EXISTS "${BUILD_DIR}/bin/${CMAKE_INSTALL_CONFIG_NAME}" )
+    # Use install --config value if defined and previously built
+    set( INNOSETUP_BUILD_CONFIGURATIONS "${CMAKE_INSTALL_CONFIG_NAME};" )
+  else()
+    message( FATAL_ERROR "You defined a value for CMAKE_INSTALL_CONFIG_NAME which has not already been built, please build it and try again" )
+  endif()
+else()
+  # These are listed in order of preference in case more than one of them are built
+  # It must include all four configurations for this to work correctly on multi-config
+  set( INNOSETUP_BUILD_CONFIGURATIONS "MinSizeRel;Release;RelWithDebInfo;Debug" )
+  message( STATUS "You did not define a build type manually so we will attempt to find build types in this order: ${INNOSETUP_BUILD_CONFIGURATIONS}" )
+endif()
+
 # "Install" prebuilt package
 
-execute_process(
-    COMMAND 
-        ${CMAKE_COMMAND}
-            --install ${BUILD_DIR}
-            --prefix "${OUTPUT_DIR}/Package"
-)
+set( VALID_BUILD_TYPE_FOUND FALSE )
+foreach( config ${INNOSETUP_BUILD_CONFIGURATIONS} )
+  file( GLOB VALID_BUILD
+          "${BUILD_DIR}/bin/${config}/Tenacity" "${BUILD_DIR}/bin/${config}/Tenacity.*"
+          "${BUILD_DIR}/bin/${config}/tenacity" "${BUILD_DIR}/bin/${config}/tenacity.*"
+  )
+  if( VALID_BUILD )
+    message ( STATUS "Using build type ${config} to create innosetup installer" )
+    execute_process(
+      COMMAND
+          ${CMAKE_COMMAND}
+              --install ${BUILD_DIR}
+              --prefix "${OUTPUT_DIR}/Package"
+              --config ${config}
+              # When we upgrade to CMake min version 3.19 we can use this
+              # COMMAND_ERROR_IS_FATAL ANY
+    )
+    set( VALID_BUILD_TYPE_FOUND TRUE FORCE )
+    set( VALID_BUILD_TYPE ${config} )
+    break()
+  endif()
+endforeach()
 
-# Build the installer
+if( NOT VALID_BUILD_TYPE_FOUND )
+  message( FATAL_ERROR "You must build the project successfully before building the innosetup target" )
+endif()
 
 execute_process(
     COMMAND
-        ${INNO_SETUP_COMPILER} /Sbyparam=$p "tenacity.iss"
+        ${INNO_SETUP_COMPILER} /Sbyparam=$p "tenacity.iss" /Qp
     WORKING_DIRECTORY
         ${OUTPUT_DIR}
+    # When we upgrade to CMake min version 3.19 we can use this
+    # COMMAND_ERROR_IS_FATAL ANY
 )
 
-# Emulate CPack behavior
+# If we enable COMMAND_ERROR_IS_FATAL then if we reach here we are sure we successfully created the installer
+# message ( STATUS "Successfully created innosetup installer using build type ${VALID_BUILD_TYPE}" )
 
+# Emulate CPack behavior
 file( COPY "${OUTPUT_DIR}/Output/" DESTINATION "${BUILD_DIR}/package" )

images/Tenacity_Logo_large.svg
@@ -1,152 +1,0 @@
-<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!-- Created with Inkscape (http://www.inkscape.org/) -->
-
-<svg
-   version="1.0"
-   width="512"
-   height="512"
-   id="svg2"
-   style="display:inline"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:svg="http://www.w3.org/2000/svg"
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:cc="http://creativecommons.org/ns#"
-   xmlns:dc="http://purl.org/dc/elements/1.1/">
-  <metadata
-     id="metadata7">
-    <rdf:RDF>
-      <cc:Work
-         rdf:about="">
-        <dc:format>image/svg+xml</dc:format>
-        <dc:type
-           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-        <cc:license
-           rdf:resource="GPL v2 or later" />
-        <dc:rights>
-          <cc:Agent>
-            <dc:title>Audacity development team</dc:title>
-          </cc:Agent>
-        </dc:rights>
-        <dc:creator>
-          <cc:Agent>
-            <dc:title>Aaron Spike</dc:title>
-          </cc:Agent>
-        </dc:creator>
-        <dc:date>2009-06-14</dc:date>
-      </cc:Work>
-    </rdf:RDF>
-  </metadata>
-  <defs
-     id="defs5">
-    <rect
-       x="0.3695004"
-       y="0.37702075"
-       width="512.87227"
-       height="512.03685"
-       id="rect7351" />
-    <clipPath
-       id="clipPath3126">
-      <path
-         d="m 30,94 2,2 3,10 1,-12 3,15 0,-19 2,5 3,14 0,-19 2,2 3,17 3,-23 6.615678,25.91971 L 59,96 l 2,-6 2,25 6,-25 2,21 5,-22 5,24 1,-18 3,-6 1,23 5,-22 3,10 1,-9 3,6 0,-19 -3,8 L 94,69 92,82 90,78 89,64 84,83 83,79 80,49 77,75 75,83 71,56 67,79 65,85 64,43.695313 58,90 52,48 49,84 47,80 45,56 44,80 41,87 39,63 37,82 34,77 34,91 30,76 30,94 Z"
-         id="path3128"
-         style="fill:none;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" />
-    </clipPath>
-    <clipPath
-       id="clipPath2798">
-      <path
-         d="M 64,6.875 C 50.736931,6.8749995 38.641338,11.015795 29.71875,17.96875 20.796162,24.921705 15.000001,34.870138 15,45.9375 l 0,4 C 6.141206,54.425978 -2.65625e-7,66.200002 0,80 c 3.5e-7,17.663999 10.049452,32 23,32 l 0,-66.0625 c 1e-6,-8.28809 4.289844,-15.915956 11.65625,-21.65625 C 42.022656,18.540956 52.424603,14.875 64,14.875 c 11.575394,0 21.977345,3.665955 29.34375,9.40625 C 100.71016,30.021545 105,37.649411 105,45.9375 L 105,112 c 12.95055,0 23,-14.336001 23,-32 0,-13.799998 -6.14121,-25.574022 -15,-30.0625 l 0,-4 C 113,34.870138 107.20384,24.921705 98.28125,17.96875 89.358662,11.015795 77.263067,6.8750003 64,6.875 Z M 26,49 l 0,63 c 2,0 4,-1 5,-3 l 0,-58 c -1,-1 -3,-2 -5,-2 z m 76,0 c -2,0 -4,1 -5,2 l 0,58 c 1,2 3,3 5,3 l 0,-63 z"
-         id="path2800"
-         style="display:inline;overflow:visible;visibility:visible;fill:#000000;fill-opacity:1;fill-rule:nonzero;stroke:none;stroke-width:18.98999977;marker:none;enable-background:accumulate" />
-    </clipPath>
-    <filter
-       style="color-interpolation-filters:sRGB"
-       id="filter10749"
-       x="-0.25757873"
-       y="-0.23542933"
-       width="1.5151575"
-       height="1.4708587">
-      <feMorphology
-         radius="4"
-         in="SourceGraphic"
-         result="result0"
-         id="feMorphology10751" />
-      <feGaussianBlur
-         in="result0"
-         stdDeviation="8"
-         result="result91"
-         id="feGaussianBlur10753" />
-      <feComposite
-         operator="in"
-         in="SourceGraphic"
-         in2="result91"
-         id="feComposite10755" />
-    </filter>
-    <filter
-       style="color-interpolation-filters:sRGB"
-       id="filter10809"
-       x="-0.028125"
-       y="-0.034244946"
-       width="1.05625"
-       height="1.0684899">
-      <feGaussianBlur
-         in="SourceGraphic"
-         stdDeviation="1.5"
-         result="blur"
-         id="feGaussianBlur10811" />
-      <feDiffuseLighting
-         diffuseConstant="1"
-         surfaceScale="10"
-         lighting-color="#ffffff"
-         result="diffuse"
-         id="feDiffuseLighting10813">
-        <feDistantLight
-           elevation="25"
-           azimuth="90"
-           id="feDistantLight10815" />
-      </feDiffuseLighting>
-      <feComposite
-         in="diffuse"
-         in2="diffuse"
-         operator="arithmetic"
-         k1="1"
-         result="composite1"
-         id="feComposite10817"
-         k2="0"
-         k3="0"
-         k4="0" />
-      <feComposite
-         in="composite1"
-         in2="SourceGraphic"
-         k1="0.886275"
-         operator="arithmetic"
-         k3="1"
-         result="composite2"
-         id="feComposite10819"
-         k2="0"
-         k4="0" />
-    </filter>
-    <rect
-       x="0.3695004"
-       y="0.37702075"
-       width="512.87225"
-       height="512.03687"
-       id="rect7351-5" />
-  </defs>
-  <rect
-     style="fill:#ffffff;fill-opacity:0.992348;stroke:#000000;stroke-width:0.534506;stroke-linejoin:round"
-     id="rect1917"
-     width="520"
-     height="520"
-     x="0"
-     y="0"
-     rx="10"
-     ry="10" />
-  <text
-     xml:space="preserve"
-     id="text7349"
-     style="font-style:normal;font-weight:normal;font-size:192px;line-height:1.25;font-family:sans-serif;white-space:pre;shape-inside:url(#rect7351-5);display:inline;fill:#000000;fill-opacity:1;stroke:none"
-     transform="matrix(1.6978042,0,0,1.6978042,149.02175,85.606116)"><tspan
-       x="0.36914062"
-       y="170.25195"
-       id="tspan2535">S</tspan></text>
-</svg>

win/Inno_Setup_Wizard/tenacity.iss.in
@@ -1,83 +1,73 @@
-﻿;   Tenacity: A Digital Audio Editor
-;   Tenacity is copyright (c) 2021-2022 Tenacity Team.
+;   Tenacity: A Digital Audio Editor
+;   Tenacity(R) is copyright (c) 1999-2022 Tenacity Team.
 ;   License: GPL v2.  See License.txt.
 ;
-;   audacity.iss
-;   Originally by Vaughan Johnson, Leland Lucius, Martyn Shaw, Richard Ash, &
-;   others. Modified by Avery King.
-;
+;   tenacity.iss
 
-; This requires that the ISS Preprocessor be installed
-
-#define AppExe "Package\tenacity.exe" 
+#define AppExe "Package\tenacity.exe"
 #define AppMajor ""
 #define AppMinor ""
 #define AppRev ""
 #define AppBuild ""
-#define FullVersion ParseVersion(AppExe, AppMajor, AppMinor, AppRev, AppBuild)
+#define FullVersion GetVersionComponents(AppExe, AppMajor, AppMinor, AppRev, AppBuild)
 #define AppVersion Str(AppMajor) + "." + Str(AppMinor) + "." + Str(AppRev)
 #define AppName GetStringFileInfo(AppExe, PRODUCT_NAME)
 
 [UninstallRun]
 ; Uninstall prior installations.
-Filename: "{app}\unins*.*"; 
+Filename: "{app}\unins*.*"; RunOnceId: "UninstallPrior"
 
 [Setup]
-; compiler-related directives
-OutputBaseFilename=tenacity-win-{#AppVersion}-@INSTALLER_SUFFIX@
+; Icons
+SetupIconFile="Additional\tenacity.ico"
+UninstallDisplayIcon="{app}\tenacity.exe"
+WizardImageFile=".\tenacity_InnoWizardImage_100.bmp"
+WizardSmallImageFile=".\tenacity_InnoWizardSmallImage_100.bmp"
 
-WizardImageFile=".\tenacity_InnoWizardImage.bmp"
-WizardSmallImageFile=".\tenacity_InnoWizardSmallImage.bmp"
-
-SolidCompression=yes
-
-; installer-related directives
-; From Inno 5.5.7, Inno defaults to disabling the Welcome page as recommended 
-; by Microsoft's desktop applications guideline, but we don't want to do that.
-DisableWelcomePage=no
-
+; App/Version information
 AppName={#AppName}
 AppVerName=Tenacity {#AppVersion}
-; Specify AppVersion as well, so it appears in the Add/Remove Programs entry. 
 AppVersion={#AppVersion}
 AppPublisher="Tenacity Team"
-AppPublisherURL=https://tenacityaudio.org
-AppSupportURL=https://codeberg.org/tenacityteam/tenacity/issues
-AppUpdatesURL=https://codeberg.org/tenacityteam/tenacity/releases
-ChangesAssociations=yes
-@INSTALLER_X64_MODE@
-
-DefaultDirName={commonpf}\Tenacity
-
+AppPublisherURL=https://tenacityaudio.org/
+AppSupportURL=https://tenacityaudio.org/
+AppUpdatesURL=https://tenacityaudio.org/
 VersionInfoProductName={#AppName}
 VersionInfoProductTextVersion={#GetFileProductVersion(AppExe)}
 VersionInfoDescription={#AppName + " " + AppVersion + " Setup"}
-VersionInfoVersion={#GetFileVersion(AppExe)}
+VersionInfoVersion={#GetVersionNumbersString(AppExe)}
 VersionInfoCopyright={#GetFileCopyright(AppExe)}
 
-; Don't disable the "Select Destination Location" wizard, even if 
+; Default install location
+DefaultDirName={commonpf}\Tenacity
+
+; Tells explorer to refresh file assocations to pickup any changes
+ChangesAssociations=yes
+
+; Does package->compress instead of compress->package
+SolidCompression=yes
+
+; Don't disable the "Select Destination Location" wizard, even if
 ; Tenacity is already installed.
 DisableDirPage=no
 
 ; Always warn if dir exists, because we'll overwrite previous Tenacity.
 DirExistsWarning=yes
 DisableProgramGroupPage=yes
-UninstallDisplayIcon="{app}\tenacity.exe"
 
-; No longer force them to accept the license, just display it.   LicenseFile=..\LICENSE.txt
+; Always show welcome page
+DisableWelcomePage=no
+
+; Display license information before install
 InfoBeforeFile=".\tenacity_InnoWizard_InfoBefore.rtf"
-InfoAfterFile=Additional\README.md
 
-; We no longer produce new ANSI builds. 
-; As we use Inno Setup (u), the Unicode version, to build this script, 
-; the MinVersion will automatically be set to what we need. 
-; We no longer explicitly set it.
-;   MinVersion=4.0,5.0
+; Display README after install
+InfoAfterFile=Additional\README.rtf
 
-; cosmetic-related directives
-SetupIconFile="Additional\tenacity.ico"
-
+; Directives using information passed by CMake
+@INSTALLER_X64_MODE@
 @SIGN_TOOL@
+OutputBaseFilename=tenacity-win-{#AppVersion}-@INSTALLER_SUFFIX@
 
 [INI]
 Filename: "{app}\FirstTime.ini"; Section: "FromInno"; Key: "ResetPrefs"; String: "1"; Tasks: resetPrefs;
@@ -86,20 +76,20 @@ Filename: "{app}\FirstTime.ini"; Section: "FromInno"; Key: "Language"; String: "
 [Tasks]
 Name: desktopicon; Description: "{cm:CreateDesktopIcon}"; GroupDescription: "{cm:AdditionalIcons}"
 Name: resetPrefs; Description:  "{cm:ResetPrefs}"; Flags: unchecked
-; No longer allow user to choose whether to associate AUP file type with Audacity.
-; Name: associate_aup; Description: "&Associate Audacity project files"; GroupDescription: "Other tasks:"; Flags: checkedonce
+; No longer allow user to choose whether to associate AUP file type with Tenacity.
+; Name: associate_aup; Description: "&Associate Tenacity project files"; GroupDescription: "Other tasks:"; Flags: checkedonce
 
 [Files]
 ; Prime the first time .ini file so the permissions can be set
 Source: ".\FirstTimeModel.ini"; DestDir: "{app}"; DestName: "FirstTime.ini"; Permissions: users-modify
 
-; Don't display in separate window, rather as InfoAfterFile.   Source: "..\README.md"; DestDir: "{app}"; Flags: ignoreversion isreadme
-Source: "Additional\README.md"; DestDir: "{app}"; Flags: ignoreversion
+; Manually create RTF version of README
+Source: "Additional\README.rtf"; DestDir: "{app}"; Flags: ignoreversion
 
 Source: "Additional\LICENSE.txt"; DestDir: "{app}"; Flags: ignoreversion
-Source: "Package\*.exe"; DestDir: "{app}"; Flags: ignoreversion
+Source: "{#AppExe}"; DestDir: "{app}"; Flags: ignoreversion
 
-; Manual, which should be got from the manual wiki using ..\scripts\mw2html_audacity\wiki2htm.bat
+; Manual, which should be got from the manual wiki using ..\scripts\mw2html_tenacity\wiki2htm.bat
 @MANUAL@
 
 Source: "Additional\presets\*"; DestDir: "{app}"; Flags: ignoreversion recursesubdirs
@@ -115,27 +105,90 @@ Source: "Package\nyquist\*"; DestDir: "{app}\Nyquist\"; Flags: ignoreversion rec
 Source: "Package\plug-ins\*"; DestDir: "{app}\Plug-Ins\"; Flags: ignoreversion
 Source: "Package\modules\*"; DestDir: "{app}\modules\"; Flags: ignoreversion recursesubdirs
 
+; Include scaled WizardImages
+Source: "tenacity_InnoWizardImage_*.bmp"; Excludes: "*100.bmp"; Flags: dontcopy
+Source: "tenacity_InnoWizardSmallImage_*.bmp"; Excludes: "*100.bmp"; Flags: dontcopy
+
+; load images based on monitor scale
+; by Martin Prikryl (CC BY-SA 3.0)
+; https://stackoverflow.com/a/31003152/13384771
+
+[Code]
+
+function GetScalingFactor: Integer;
+begin
+  if WizardForm.Font.PixelsPerInch >= 192 then Result := 200
+    else
+  if WizardForm.Font.PixelsPerInch >= 144 then Result := 150
+    else
+  if WizardForm.Font.PixelsPerInch >= 120 then Result := 125
+    else Result := 100;
+end;
+
+procedure LoadEmbededScaledBitmap(Image: TBitmapImage; NameBase: string);
+var
+  Name: String;
+  FileName: String;
+begin
+  Name := Format('%s_%d.bmp', [NameBase, GetScalingFactor]);
+  ExtractTemporaryFile(Name);
+  FileName := ExpandConstant('{tmp}\' + Name);
+  Image.Bitmap.LoadFromFile(FileName);
+  DeleteFile(FileName);
+end;
+
+procedure InitializeWizard;
+begin
+  { If using larger scaling, load the correct size of images }
+  if GetScalingFactor > 100 then
+  begin
+    LoadEmbededScaledBitmap(WizardForm.WizardBitmapImage, 'tenacity_InnoWizardImage');
+    LoadEmbededScaledBitmap(WizardForm.WizardBitmapImage2, 'tenacity_InnoWizardImage');
+    LoadEmbededScaledBitmap(WizardForm.WizardSmallBitmapImage, 'tenacity_InnoWizardSmallImage');
+  end;
+end;
+
 [Icons]
 Name: "{commonprograms}\Tenacity"; Filename: "{app}\tenacity.exe"
 Name: "{commondesktop}\Tenacity"; Filename: "{app}\tenacity.exe"; Tasks: desktopicon
 
 [InstallDelete]
 
-; Get rid of previous versions of MSVC runtimes.
+; Get rid of previous versions of MSVC runtimes
+; Currently MSVC runtime versions 8, 9, 10, 11, 12, 13
 Type: files; Name: "{app}\Microsoft.VC80.CRT.manifest"
-Type: files; Name: "{app}\Microsoft.VC90.CRT.manifest"
 Type: files; Name: "{app}\msvcp80.dll"
 Type: files; Name: "{app}\msvcr80.dll"
+Type: files; Name: "{app}\Microsoft.VC90.CRT.manifest"
 Type: files; Name: "{app}\msvcp90.dll"
 Type: files; Name: "{app}\msvcr90.dll"
+Type: files; Name: "{app}\Microsoft.VC100.CRT.manifest"
+Type: files; Name: "{app}\msvcp100.dll"
+Type: files; Name: "{app}\msvcr100.dll"
+Type: files; Name: "{app}\Microsoft.VC110.CRT.manifest"
+Type: files; Name: "{app}\msvcp110.dll"
+Type: files; Name: "{app}\msvcr110.dll"
+Type: files; Name: "{app}\Microsoft.VC120.CRT.manifest"
 Type: files; Name: "{app}\msvcp120.dll"
 Type: files; Name: "{app}\msvcr120.dll"
+Type: files; Name: "{app}\Microsoft.VC130.CRT.manifest"
+Type: files; Name: "{app}\msvcp130.dll"
+Type: files; Name: "{app}\msvcr130.dll"
 
 ; Get rid of previous help folder.
 Type: filesandordirs; Name: "{app}\help"
 
 ;Get rid of previous uninstall item
 Type: files; Name: "{app}\unins*.*"
+
+; Get rid of no longer used test.lsp.
+Type: files; Name: "{app}\Nyquist\test.lsp"
+
+; Get rid of specific LADSPA plug-ins that we now ship with different names.
+Type: files; Name: "{app}\Plug-Ins\GVerb.dll"
+Type: files; Name: "{app}\Plug-Ins\Hard Limiter.dll"
+Type: files; Name: "{app}\Plug-Ins\hard_limiter_1413.dll"
+Type: files; Name: "{app}\Plug-Ins\sc4.dll"
 
 ;Get rid of any modules that we have ever installed
 Type: files; Name: "{app}\Modules\mod-script-pipe.dll"
@@ -146,25 +199,28 @@ Type: files; Name: "{app}\Modules\mod-nyq-bench.dll"
 ;get rid of the Modules dir, if it is empty
 Type: dirifempty; Name: "{app}\Modules"
 
+; Get rid of gverb that we no longer ship
+Type: files; Name: "{app}\Plug-Ins\gverb_1216.dll"
+
 ; Get rid of old nyquist plugins that we no longer ship
 Type: files; Name: "{app}\Plug-Ins\crossfadein.ny"
 Type: files; Name: "{app}\Plug-Ins\crossfadeout.ny"
 Type: files; Name: "{app}\Plug-Ins\clicktrack.ny"
-                                            
+
 [Registry]
-; No longer allow user to choose whether to associate AUP file type with Audacity.
+; No longer allow user to choose whether to associate AUP file type with Tenacity.
 ; Leaving this one commented out example of the old way.
-; Root: HKCR; Subkey: ".AUP"; ValueType: string; ValueData: "Audacity.Project"; Flags: createvalueifdoesntexist uninsdeletekey; Tasks: associate_aup
-Root: HKCR; Subkey: ".AUP"; ValueType: string; ValueData: "Audacity.Project"; Flags: createvalueifdoesntexist uninsdeletekey;
-Root: HKCR; Subkey: ".AUP3"; ValueType: string; ValueData: "Audacity.Project"; Flags: createvalueifdoesntexist uninsdeletekey;
-Root: HKCR; Subkey: "Audacity.Project\OpenWithList\tenacity.exe"; Flags: createvalueifdoesntexist uninsdeletekey;
-Root: HKCR; Subkey: "Audacity.Project"; ValueType: string; ValueData: "Audacity Project File"; Flags: createvalueifdoesntexist uninsdeletekey;
-Root: HKCR; Subkey: "Audacity.Project\shell"; ValueType: string; ValueData: ""; Flags: createvalueifdoesntexist uninsdeletekey;
-Root: HKCR; Subkey: "Audacity.Project\shell\open"; Flags: createvalueifdoesntexist uninsdeletekey;
-Root: HKCR; Subkey: "Audacity.Project\shell\open\command"; ValueType: string; ValueData: """{app}\tenacity.exe"" ""%1"""; Flags: uninsdeletekey;
+; Root: HKCR; Subkey: ".AUP"; ValueType: string; ValueData: "Tenacity.Project"; Flags: createvalueifdoesntexist uninsdeletekey; Tasks: associate_aup
+Root: HKCR; Subkey: ".AUP"; ValueType: string; ValueData: "Tenacity.Project"; Flags: createvalueifdoesntexist uninsdeletekey;
+Root: HKCR; Subkey: ".AUP3"; ValueType: string; ValueData: "Tenacity.Project"; Flags: createvalueifdoesntexist uninsdeletekey;
+Root: HKCR; Subkey: "Tenacity.Project\OpenWithList\tenacity.exe"; Flags: createvalueifdoesntexist uninsdeletekey;
+Root: HKCR; Subkey: "Tenacity.Project"; ValueType: string; ValueData: "Tenacity Project File"; Flags: createvalueifdoesntexist uninsdeletekey;
+Root: HKCR; Subkey: "Tenacity.Project\shell"; ValueType: string; ValueData: ""; Flags: createvalueifdoesntexist uninsdeletekey;
+Root: HKCR; Subkey: "Tenacity.Project\shell\open"; Flags: createvalueifdoesntexist uninsdeletekey;
+Root: HKCR; Subkey: "Tenacity.Project\shell\open\command"; ValueType: string; ValueData: """{app}\tenacity.exe"" ""%1"""; Flags: uninsdeletekey;
 
 ;The following would allow a following 'help' installer to know where to put the 'help' files.
-;Root: HKCR; Subkey: "Audacity.Project\Path";  ValueType: string; ValueData: {app}; Flags: createvalueifdoesntexist uninsdeletekey;
+;Root: HKCR; Subkey: "Tenacity.Project\Path";  ValueType: string; ValueData: {app}; Flags: createvalueifdoesntexist uninsdeletekey;
 
 [Run]
 Filename: "{app}\tenacity.exe"; Description: "{cm:LaunchProgram,Tenacity}"; Flags: nowait postinstall skipifsilent
@@ -181,7 +237,7 @@ Filename: "{app}\tenacity.exe"; Description: "{cm:LaunchProgram,Tenacity}"; Flag
 ; http://www.jrsoftware.org/files/istrans/
 ;
 ; Set this to the base of the unofficial  Inno Setup translations
-#define UrlBase "http://raw.github.com/jrsoftware/issrc/master/Files/Languages/Unofficial/"
+#define UrlBase "https://raw.githubusercontent.com/jrsoftware/issrc/main/Files/Languages/Unofficial/"
 
 ; This macro will use the Windows PowerShell to download the given translation into
 ; the Inno Setup Languages folder if it hasn't already been downloaded.
@@ -230,7 +286,7 @@ Name: "gl"; MessagesFile: "{#Get('Galician.isl')}"
 Name: "he"; MessagesFile: "compiler:Languages\Hebrew.isl"
 Name: "hi"; MessagesFile: "{#Get('Hindi.islu')}"
 Name: "hr"; MessagesFile: "{#Get('Croatian.isl')}"
-;Name: "hu"; MessagesFile: "{#Get('Hungarian.isl')}"
+Name: "hu"; MessagesFile: "{#Get('Hungarian.isl')}"
 Name: "hy"; MessagesFile: "compiler:Languages\Armenian.isl"
 Name: "id"; MessagesFile: "{#Get('Indonesian.isl')}"
 Name: "it"; MessagesFile: "compiler:Languages\Italian.isl"
@@ -292,7 +348,7 @@ bn.ResetPrefs=Reset Preferences
 bs.ResetPrefs=Reset Preferences
 ca.ResetPrefs=Voleu restablir les preferències?
 ca_ES0valencia.ResetPrefs=Reset Preferences
-co.ResetPrefs=Reinizià e preferenze ?
+co.ResetPrefs=Reinizià e preferenze ?
 cs.ResetPrefs=Vynulovat nastavení?
 cy.ResetPrefs=Reset Preferences
 da.ResetPrefs=Gendan indstillinger?
@@ -309,7 +365,7 @@ gl.ResetPrefs=Restabelecer as preferencias?
 he.ResetPrefs=?אתה רוצה לשחזר העדפות
 hi.ResetPrefs=वरीयताएँ रीसेट करें?
 hr.ResetPrefs=Resetirati Postavke?
-;hu.ResetPrefs=Alapra állítja a beállításokat?
+hu.ResetPrefs=Alapra állítja a beállításokat?
 hy.ResetPrefs=Վերափոխե՞լ կարգավորումները:
 id.ResetPrefs=Reset Preferences
 it.ResetPrefs=Ripristino Preferenze?

commit abb8ca04399d99603fe3d097aff46f82da7d947e

Inno Setup: Fix wrong installer arch on x64
For some reason, commit 05bdc06309 broke the Windows installer
architecture, so a 32-bit installer was being produced for 64-bit builds
instead of a 64-bit installer. This has now been fixed.

Signed-off-by: Avery King <avery98@pm.me>

win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake
@@ -13,7 +13,7 @@
 # Allow if statements to use the new IN_LIST operator (compatibility override for CMake <3.3)
 cmake_policy( SET CMP0057 NEW )
 
-if( IS_64_BIT )
+if( BUILDING_64_BIT )
     set( INSTALLER_SUFFIX "x64" )
     set( INSTALLER_X64_MODE "ArchitecturesInstallIn64BitMode=x64")
 else()