alex74747/audacity

Introduce per-project and per-track RealtimeEffectList objects

src/effects/RealtimeEffectList.cpp
@@ -1,0 +1,56 @@
+/**********************************************************************
+ 
+  Audacity: A Digital Audio Editor
+ 
+  RealtimeEffectList.cpp
+ 
+ *********************************************************************/
+
+#include "RealtimeEffectList.h"
+
+#include "Project.h"
+#include "Track.h"
+
+RealtimeEffectList::RealtimeEffectList()
+{
+}
+
+RealtimeEffectList::~RealtimeEffectList()
+{
+}
+
+static const AttachedProjectObjects::RegisteredFactory masterEffects
+{
+   [](AudacityProject &project)
+   {
+      return std::make_shared<RealtimeEffectList>();
+   }
+};
+
+RealtimeEffectList &RealtimeEffectList::Get(AudacityProject &project)
+{
+   return project.AttachedObjects::Get<RealtimeEffectList>(masterEffects);
+}
+
+const RealtimeEffectList &RealtimeEffectList::Get(const AudacityProject &project)
+{
+   return Get(const_cast<AudacityProject &>(project));
+}
+
+static const AttachedTrackObjects::RegisteredFactory trackEffects
+{
+   [](Track &track)
+   {
+      return std::make_shared<RealtimeEffectList>();
+   }
+};
+
+RealtimeEffectList &RealtimeEffectList::Get(Track &track)
+{
+   return track.AttachedObjects::Get<RealtimeEffectList>(trackEffects);
+}
+
+const RealtimeEffectList &RealtimeEffectList::Get(const Track &track)
+{
+   return Get(const_cast<Track &>(track));
+}

tenacityteam/tenacity

(Modified for Tenacity)
(cherry picked from commit 9f8dae4bcfc5febf9393db6291b58f6a34646c6f)
Signed-off-by: Avery King <avery98@pm.me>

src/effects/RealtimeEffectList.cpp
@@ -1,0 +1,56 @@
+/**********************************************************************
+ 
+  Audacity: A Digital Audio Editor
+ 
+  RealtimeEffectList.cpp
+ 
+ *********************************************************************/
+
+#include "RealtimeEffectList.h"
+
+#include "Project.h"
+#include "Track.h"
+
+RealtimeEffectList::RealtimeEffectList()
+{
+}
+
+RealtimeEffectList::~RealtimeEffectList()
+{
+}
+
+static const AttachedProjectObjects::RegisteredFactory masterEffects
+{
+   [](TenacityProject &project)
+   {
+      return std::make_shared<RealtimeEffectList>();
+   }
+};
+
+RealtimeEffectList &RealtimeEffectList::Get(TenacityProject &project)
+{
+   return project.AttachedObjects::Get<RealtimeEffectList>(masterEffects);
+}
+
+const RealtimeEffectList &RealtimeEffectList::Get(const TenacityProject &project)
+{
+   return Get(const_cast<TenacityProject &>(project));
+}
+
+static const AttachedTrackObjects::RegisteredFactory trackEffects
+{
+   [](Track &track)
+   {
+      return std::make_shared<RealtimeEffectList>();
+   }
+};
+
+RealtimeEffectList &RealtimeEffectList::Get(Track &track)
+{
+   return track.AttachedObjects::Get<RealtimeEffectList>(trackEffects);
+}
+
+const RealtimeEffectList &RealtimeEffectList::Get(const Track &track)
+{
+   return Get(const_cast<Track &>(track));
+}