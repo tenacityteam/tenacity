audacity/audacity

AudioIO stores weak pointer to project for safety

libraries/lib-audio-devices/AudioIOBase.cpp
@@ -307,9 +307,10 @@ void AudioIOBase::HandleDeviceChange()
 }
 
 void AudioIOBase::SetCaptureMeter(
-   AudacityProject *project, const std::weak_ptr<Meter> &wMeter)
+   const std::shared_ptr<AudacityProject> &project, const std::weak_ptr<Meter> &wMeter)
 {
-   if (( mOwningProject ) && ( mOwningProject != project))
+   if (auto pOwningProject = mOwningProject.lock();
+       ( pOwningProject ) && ( pOwningProject != project))
       return;
 
    auto meter = wMeter.lock();
@@ -323,9 +324,10 @@ void AudioIOBase::SetCaptureMeter(
 }
 
 void AudioIOBase::SetPlaybackMeter(
-   AudacityProject *project, const std::weak_ptr<Meter> &wMeter)
+   const std::shared_ptr<AudacityProject> &project, const std::weak_ptr<Meter> &wMeter)
 {
-   if (( mOwningProject ) && ( mOwningProject != project))
+   if (auto pOwningProject = mOwningProject.lock();
+       ( pOwningProject ) && ( pOwningProject != project))
       return;
 
    auto meter = wMeter.lock();

libraries/lib-audio-devices/AudioIOBase.h
@@ -43,15 +43,16 @@ class PlaybackPolicy;
 struct AudioIOStartStreamOptions
 {
    explicit
-   AudioIOStartStreamOptions(AudacityProject *pProject_, double rate_)
-      : pProject{ pProject_ }
+   AudioIOStartStreamOptions(
+      const std::shared_ptr<AudacityProject> &pProject, double rate_)
+      : pProject{ pProject }
       , envelope(nullptr)
       , rate(rate_)
       , pStartTime(NULL)
       , preRoll(0.0)
    {}
 
-   AudacityProject *pProject{};
+   std::shared_ptr<AudacityProject> pProject;
    std::weak_ptr<Meter> captureMeter, playbackMeter;
    const BoundedEnvelope *envelope; // for time warping
    std::shared_ptr< AudioIOListener > listener;
@@ -107,9 +108,9 @@ public:
    AudioIOBase &operator=(const AudioIOBase &) = delete;
 
    void SetCaptureMeter(
-      AudacityProject *project, const std::weak_ptr<Meter> &meter);
+      const std::shared_ptr<AudacityProject> &project, const std::weak_ptr<Meter> &meter);
    void SetPlaybackMeter(
-      AudacityProject *project, const std::weak_ptr<Meter> &meter);
+      const std::shared_ptr<AudacityProject> &project, const std::weak_ptr<Meter> &meter);
 
    /** \brief update state after changing what audio devices are selected
     *
@@ -239,7 +240,7 @@ protected:
    static wxString DeviceName(const PaDeviceInfo* info);
    static wxString HostName(const PaDeviceInfo* info);
 
-   AudacityProject    *mOwningProject;
+   std::weak_ptr<AudacityProject> mOwningProject;
 
    /// True if audio playback is paused
    bool                mPaused;

src/AudioIO.cpp
@@ -309,7 +309,6 @@ AudioIO::AudioIO()
    mUpdateMeters = false;
    mUpdatingMeters = false;
 
-   mOwningProject = NULL;
    mOutputMeter.reset();
 
    PaError err = Pa_Initialize();
@@ -491,7 +490,7 @@ bool AudioIO::StartPortAudioStream(const AudioIOStartStreamOptions &options,
 
    // PRL:  Protection from crash reported by David Bailes, involving starting
    // and stopping with frequent changes of active window, hard to reproduce
-   if (!mOwningProject)
+   if (mOwningProject.expired())
       return false;
 
    mInputMeter.reset();
@@ -590,7 +589,7 @@ bool AudioIO::StartPortAudioStream(const AudioIOStartStreamOptions &options,
       else
          captureParameters.suggestedLatency = latencyDuration/1000.0;
 
-      SetCaptureMeter( mOwningProject, options.captureMeter );
+      SetCaptureMeter( mOwningProject.lock(), options.captureMeter );
    }
 
    SetMeters();
@@ -694,18 +693,19 @@ void AudioIO::StartMonitoring( const AudioIOStartStreamOptions &options )
                                   (unsigned int)captureChannels,
                                   captureFormat);
 
+   auto pOwningProject = mOwningProject.lock();
    if (!success) {
       using namespace BasicUI;
       auto msg = XO("Error opening recording device.\nError code: %s")
          .Format( Get()->LastPaErrorString() );
-      ShowErrorDialog( *ProjectFramePlacement( mOwningProject ),
+      ShowErrorDialog( *ProjectFramePlacement( pOwningProject.get() ),
          XO("Error"), msg, wxT("Error_opening_sound_device"),
          ErrorDialogOptions{ ErrorDialogType::ModalErrorReport } );
       return;
    }
 
    wxCommandEvent e(EVT_AUDIOIO_MONITOR);
-   e.SetEventObject(mOwningProject);
+   e.SetEventObject( pOwningProject.get() );
    e.SetInt(true);
    wxTheApp->ProcessEvent(e);
 
@@ -1012,10 +1012,11 @@ int AudioIO::StartStream(const TransportTracks &tracks,
       pListener->OnAudioIORate((int)mRate);
    }
 
+   auto pOwningProject = mOwningProject.lock();
    if (mNumPlaybackChannels > 0)
    {
       wxCommandEvent e(EVT_AUDIOIO_PLAYBACK);
-      e.SetEventObject(mOwningProject);
+      e.SetEventObject( pOwningProject.get() );
       e.SetInt(true);
       wxTheApp->ProcessEvent(e);
    }
@@ -1023,7 +1024,7 @@ int AudioIO::StartStream(const TransportTracks &tracks,
    if (mNumCaptureChannels > 0)
    {
       wxCommandEvent e(EVT_AUDIOIO_CAPTURE);
-      e.SetEventObject(mOwningProject);
+      e.SetEventObject( pOwningProject.get() );
       e.SetInt(true);
       wxTheApp->ProcessEvent(e);
    }
@@ -1273,9 +1274,10 @@ void AudioIO::StartStreamCleanup(bool bOnlyBuffers)
    mPlaybackSchedule.GetPolicy().Finalize( mPlaybackSchedule );
 }
 
-bool AudioIO::IsAvailable(AudacityProject *project) const
+bool AudioIO::IsAvailable(AudacityProject &project) const
 {
-   return mOwningProject == NULL || mOwningProject == project;
+   auto pOwningProject = mOwningProject.lock();
+   return !pOwningProject || pOwningProject.get() == &project;
 }
 
 void AudioIO::SetMeters()
@@ -1480,8 +1482,9 @@ void AudioIO::StopStream()
             // This scope may combine many splittings of wave tracks
             // into one transaction, lessening the number of checkpoints
             Optional<TransactionScope> pScope;
-            if (mOwningProject) {
-               auto &pIO = ProjectFileIO::Get(*mOwningProject);
+            auto pOwningProject = mOwningProject.lock();
+            if (pOwningProject) {
+               auto &pIO = ProjectFileIO::Get(*pOwningProject);
                pScope.emplace(pIO.GetConnection(), "Dropouts");
             }
             for (auto &interval : mLostCaptureIntervals) {
@@ -1510,7 +1513,7 @@ void AudioIO::StopStream()
 
    mInputMeter.reset();
    mOutputMeter.reset();
-   mOwningProject = nullptr;
+   mOwningProject.reset();
 
    if (pListener && mNumCaptureChannels > 0)
       pListener->OnAudioIOStopRecording();
@@ -1539,7 +1542,8 @@ void AudioIO::StopStream()
    if (mNumPlaybackChannels > 0)
    {
       wxCommandEvent e(EVT_AUDIOIO_PLAYBACK);
-      e.SetEventObject(mOwningProject);
+      auto pOwningProject = mOwningProject.lock();
+      e.SetEventObject(pOwningProject.get());
       e.SetInt(false);
       wxTheApp->ProcessEvent(e);
    }
@@ -1547,7 +1551,8 @@ void AudioIO::StopStream()
    if (mNumCaptureChannels > 0)
    {
       wxCommandEvent e(wasMonitoring ? EVT_AUDIOIO_MONITOR : EVT_AUDIOIO_CAPTURE);
-      e.SetEventObject(mOwningProject);
+      auto pOwningProject = mOwningProject.lock();
+      e.SetEventObject(pOwningProject.get());
       e.SetInt(false);
       wxTheApp->ProcessEvent(e);
    }
@@ -1877,8 +1882,9 @@ void AudioIO::DrainRecordBuffers()
          // and also an autosave, into one transaction,
          // lessening the number of checkpoints
          Optional<TransactionScope> pScope;
-         if (mOwningProject) {
-            auto &pIO = ProjectFileIO::Get(*mOwningProject);
+         auto pOwningProject = mOwningProject.lock();
+         if (pOwningProject) {
+            auto &pIO = ProjectFileIO::Get( *pOwningProject );
             pScope.emplace(pIO.GetConnection(), "Recording");
          }
 
@@ -2088,7 +2094,7 @@ double AudioIO::AILAGetLastDecisionTime() {
 }
 
 void AudioIO::AILAProcess(double maxPeak) {
-   AudacityProject *const proj = mOwningProject;
+   const auto proj = mOwningProject.lock();
    if (proj && mAILAActive) {
       if (mInputMeter && mInputMeter->IsClipping()) {
          mAILAClipped = true;

src/AudioIO.h
@@ -400,7 +400,8 @@ public:
    wxString LastPaErrorString();
 
    wxLongLong GetLastPlaybackTime() const { return mLastPlaybackTimeMillis; }
-   AudacityProject *GetOwningProject() const { return mOwningProject; }
+   std::shared_ptr<AudacityProject> GetOwningProject() const
+   { return mOwningProject.lock(); }
 
    /** \brief Pause and un-pause playback and recording */
    void SetPaused(bool state);
@@ -461,7 +462,7 @@ public:
       double AILAGetLastDecisionTime();
    #endif
 
-   bool IsAvailable(AudacityProject *projecT) const;
+   bool IsAvailable(AudacityProject &project) const;
 
    /** \brief Return a valid sample rate that is supported by the current I/O
    * device(s).

tenacityteam/tenacity

(Modified for Tenacity)
(cherry picked from commit 4c339e6f0923fc5c755f08a2f043d6f2d50ca80a)
Signed-off-by: Avery King <avery98@pm.me>

libraries/lib-audio-devices/AudioIOBase.cpp
@@ -120,9 +120,10 @@ void AudioIOBase::HandleDeviceChange()
 }
 
 void AudioIOBase::SetCaptureMeter(
-   TenacityProject *project, const std::weak_ptr<Meter> &wMeter)
+   const std::shared_ptr<TenacityProject> &project, const std::weak_ptr<Meter> &wMeter)
 {
-   if (( mOwningProject ) && ( mOwningProject != project))
+   if (auto pOwningProject = mOwningProject.lock();
+       ( pOwningProject ) && ( pOwningProject != project))
       return;
 
    auto meter = wMeter.lock();
@@ -136,9 +137,10 @@ void AudioIOBase::SetCaptureMeter(
 }
 
 void AudioIOBase::SetPlaybackMeter(
-   TenacityProject *project, const std::weak_ptr<Meter> &wMeter)
+   const std::shared_ptr<TenacityProject> &project, const std::weak_ptr<Meter> &wMeter)
 {
-   if (( mOwningProject ) && ( mOwningProject != project))
+   if (auto pOwningProject = mOwningProject.lock();
+       ( pOwningProject ) && ( pOwningProject != project))
       return;
 
    auto meter = wMeter.lock();

libraries/lib-audio-devices/AudioIOBase.h
@@ -72,8 +72,9 @@ struct ScrubbingOptions {
 struct AudioIOStartStreamOptions
 {
    explicit
-   AudioIOStartStreamOptions(TenacityProject *pProject_, double rate_)
-      : pProject{ pProject_ }
+   AudioIOStartStreamOptions(
+      const std::shared_ptr<TenacityProject> &pProject, double rate_)
+      : pProject{ pProject }
       , envelope(nullptr)
       , rate(rate_)
       , playLooped(false)
@@ -83,7 +84,7 @@ struct AudioIOStartStreamOptions
       , preRoll(0.0)
    {}
 
-   TenacityProject *pProject{};
+   std::shared_ptr<TenacityProject> pProject;
    std::weak_ptr<Meter> captureMeter, playbackMeter;
    const BoundedEnvelope *envelope; // for time warping
    std::shared_ptr< AudioIOListener > listener;
@@ -121,9 +122,9 @@ public:
    virtual ~AudioIOBase();
 
    void SetCaptureMeter(
-      TenacityProject *project, const std::weak_ptr<Meter> &meter);
+      const std::shared_ptr<TenacityProject> &project, const std::weak_ptr<Meter> &meter);
    void SetPlaybackMeter(
-      TenacityProject *project, const std::weak_ptr<Meter> &meter);
+      const std::shared_ptr<TenacityProject> &project, const std::weak_ptr<Meter> &meter);
 
    /** \brief update state after changing what audio devices are selected
     *
@@ -247,7 +248,7 @@ protected:
    static std::string DeviceName(const PaDeviceInfo* info);
    static std::string HostName(const PaDeviceInfo* info);
 
-   TenacityProject    *mOwningProject;
+   std::weak_ptr<TenacityProject> mOwningProject;
 
    /// True if audio playback is paused
    bool                mPaused;

src/AudioIO.cpp
@@ -1089,7 +1089,6 @@ AudioIO::AudioIO()
    mUpdateMeters = false;
    mUpdatingMeters = false;
 
-   mOwningProject = NULL;
    mOutputMeter.reset();
 
    mBuffersPrepared = false;
@@ -1219,7 +1218,7 @@ bool AudioIO::StartPortAudioStream(const AudioIOStartStreamOptions &options,
 
    // PRL:  Protection from crash reported by David Bailes, involving starting
    // and stopping with frequent changes of active window, hard to reproduce
-   if (!mOwningProject)
+   if (mOwningProject.expired())
       return false;
 
    mInputMeter.reset();
@@ -1303,7 +1302,7 @@ bool AudioIO::StartPortAudioStream(const AudioIOStartStreamOptions &options,
          captureDeviceInfo->defaultHighInputLatency :
          0.0;
 
-      SetCaptureMeter( mOwningProject, options.captureMeter );
+      SetCaptureMeter( mOwningProject.lock(), options.captureMeter );
    }
 
    SetMeters();
@@ -1389,6 +1388,7 @@ void AudioIO::StartMonitoring( const AudioIOStartStreamOptions &options )
                                   (unsigned int)captureChannels,
                                   captureFormat);
 
+   auto pOwningProject = mOwningProject.lock();
    if (!success) {
       auto msg = XO("Error opening recording device.\nError code: %s")
          .Format( Get()->LastPaErrorString() );
@@ -1397,7 +1397,7 @@ void AudioIO::StartMonitoring( const AudioIOStartStreamOptions &options )
    }
 
    wxCommandEvent e(EVT_AUDIOIO_MONITOR);
-   e.SetEventObject(mOwningProject);
+   e.SetEventObject( pOwningProject.get() );
    e.SetInt(true);
    wxTheApp->ProcessEvent(e);
 
@@ -1726,10 +1726,11 @@ int AudioIO::StartStream(const TransportTracks &tracks,
       pListener->OnAudioIORate((int)mRate);
    }
 
+   auto pOwningProject = mOwningProject.lock();
    if (mNumPlaybackChannels > 0)
    {
       wxCommandEvent e(EVT_AUDIOIO_PLAYBACK);
-      e.SetEventObject(mOwningProject);
+      e.SetEventObject( pOwningProject.get() );
       e.SetInt(true);
       wxTheApp->ProcessEvent(e);
    }
@@ -1737,7 +1738,7 @@ int AudioIO::StartStream(const TransportTracks &tracks,
    if (mNumCaptureChannels > 0)
    {
       wxCommandEvent e(EVT_AUDIOIO_CAPTURE);
-      e.SetEventObject(mOwningProject);
+      e.SetEventObject( pOwningProject.get() );
       e.SetInt(true);
       wxTheApp->ProcessEvent(e);
    }
@@ -2100,9 +2101,10 @@ bool AudioIoCallback::StartPortMidiStream()
 }
 #endif
 
-bool AudioIO::IsAvailable(TenacityProject *project) const
+bool AudioIO::IsAvailable(TenacityProject &project) const
 {
-   return mOwningProject == NULL || mOwningProject == project;
+   auto pOwningProject = mOwningProject.lock();
+   return !pOwningProject || pOwningProject.get() == &project;
 }
 
 void AudioIO::SetMeters()
@@ -2329,8 +2331,9 @@ void AudioIO::StopStream()
             // This scope may combine many splittings of wave tracks
             // into one transaction, lessening the number of checkpoints
             std::optional<TransactionScope> pScope;
-            if (mOwningProject) {
-               auto &pIO = ProjectFileIO::Get(*mOwningProject);
+            auto pOwningProject = mOwningProject.lock();
+            if (pOwningProject) {
+               auto &pIO = ProjectFileIO::Get(*pOwningProject);
                pScope.emplace(pIO.GetConnection(), "Dropouts");
             }
             for (auto &interval : mLostCaptureIntervals) {
@@ -2359,7 +2362,7 @@ void AudioIO::StopStream()
 
    mInputMeter.reset();
    mOutputMeter.reset();
-   mOwningProject = nullptr;
+   mOwningProject.reset();
 
    if (pListener && mNumCaptureChannels > 0)
       pListener->OnAudioIOStopRecording();
@@ -2388,7 +2391,8 @@ void AudioIO::StopStream()
    if (mNumPlaybackChannels > 0)
    {
       wxCommandEvent e(EVT_AUDIOIO_PLAYBACK);
-      e.SetEventObject(mOwningProject);
+      auto pOwningProject = mOwningProject.lock();
+      e.SetEventObject(pOwningProject.get());
       e.SetInt(false);
       wxTheApp->ProcessEvent(e);
    }
@@ -2396,7 +2400,8 @@ void AudioIO::StopStream()
    if (mNumCaptureChannels > 0)
    {
       wxCommandEvent e(wasMonitoring ? EVT_AUDIOIO_MONITOR : EVT_AUDIOIO_CAPTURE);
-      e.SetEventObject(mOwningProject);
+      auto pOwningProject = mOwningProject.lock();
+      e.SetEventObject(pOwningProject.get());
       e.SetInt(false);
       wxTheApp->ProcessEvent(e);
    }
@@ -2883,8 +2888,9 @@ void AudioIO::FillBuffers()
             // and also an autosave, into one transaction,
             // lessening the number of checkpoints
             std::optional<TransactionScope> pScope;
-            if (mOwningProject) {
-               auto &pIO = ProjectFileIO::Get(*mOwningProject);
+            auto pOwningProject = mOwningProject.lock();
+            if (pOwningProject) {
+               auto &pIO = ProjectFileIO::Get(*pOwningProject);
                pScope.emplace(pIO.GetConnection(), "Recording");
             }
 

src/AudioIO.h
@@ -552,7 +552,8 @@ public:
    std::string LastPaErrorString();
 
    wxLongLong GetLastPlaybackTime() const { return mLastPlaybackTimeMillis; }
-   TenacityProject *GetOwningProject() const { return mOwningProject; }
+   std::shared_ptr<TenacityProject> GetOwningProject() const
+   { return mOwningProject.lock(); }
 
    /** \brief Pause and un-pause playback and recording */
    void SetPaused(bool state);
@@ -573,7 +574,7 @@ public:
     *
     */
 
-   bool IsAvailable(TenacityProject *projecT) const;
+   bool IsAvailable(TenacityProject &project) const;
 
    /** \brief Return a valid sample rate that is supported by the current I/O
    * device(s).