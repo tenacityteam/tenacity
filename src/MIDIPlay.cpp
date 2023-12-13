Paul-Licameli/audacity

MIDIPlay does not use AudacityMessageBox

modules/mod-note-track/MIDIPlay.cpp
@@ -357,11 +357,11 @@ Time (in seconds, = total_sample_count / sample_rate)
 #include "AudioIO.h"
 
 #include "AudioIOExt.h"
+#include "BasicUI.h"
 #include "Prefs.h"
 #include "../lib-src/portmidi/pm_common/portmidi.h"
 #include "../lib-src/portmidi/porttime/porttime.h"
 #include "../lib-src/header-substitutes/allegro.h"
-#include "AudacityMessageBox.h"
 #include <wx/log.h>
 #include <wx/sstream.h>
 #include <wx/txtstrm.h>
@@ -570,10 +570,13 @@ MIDIPlay::MIDIPlay(const PlaybackSchedule &schedule)
          errStr += XO("Error: %s").Format( pmErrStr );
       // XXX: we are in libaudacity, popping up dialogs not allowed!  A
       // long-term solution will probably involve exceptions
-      AudacityMessageBox(
+      using namespace BasicUI;
+      ShowMessageBox(
          errStr,
-         XO("Error Initializing Midi"),
-         wxICON_ERROR|wxOK);
+         MessageBoxOptions{}
+            .Caption(XO("Error Initializing Midi"))
+            .ButtonStyle(Button::Ok)
+            .IconStyle(Icon::Error));
 
       // Same logic for PortMidi as described above for PortAudio
    }

tenacityteam/tenacity

(Modified for Tenacity)
(cherry picked from commit 390340f6a1b2bc286375923de7de41efa24df46e)
Signed-off-by: Avery King <avery98@pm.me>

src/MIDIPlay.cpp
@@ -357,11 +357,11 @@ Time (in seconds, = total_sample_count / sample_rate)
 #include "MIDIPlay.h"
 #include "AudioIO.h"
 
+#include "BasicUI.h"
 #include "Prefs.h"
 #include "portaudio.h"
 #include <portmidi.h>
 #include <porttime.h>
-#include "widgets/AudacityMessageBox.h"
 
 #define ROUND(x) (int) ((x)+0.5)
 
@@ -531,10 +531,13 @@ MIDIPlay::MIDIPlay(const PlaybackSchedule &schedule)
          errStr += XO("Error: %s").Format( pmErrStr );
       // XXX: we are in libaudacity, popping up dialogs not allowed!  A
       // long-term solution will probably involve exceptions
-      AudacityMessageBox(
+      using namespace GenericUI;
+      ShowMessageBox(
          errStr,
-         XO("Error Initializing Midi"),
-         wxICON_ERROR|wxOK);
+         MessageBoxOptions{}
+            .Caption(XO("Error Initializing Midi"))
+            .ButtonStyle(Button::Ok)
+            .IconStyle(Icon::Error));
 
       // Same logic for PortMidi as described above for PortAudio
    }