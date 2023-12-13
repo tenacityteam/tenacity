alex74747/audacity

CommonCommandFlags does not depend on NoteTrack

modules/mod-track-menus/TrackMenus.cpp
@@ -331,6 +331,15 @@ void DoAlign
 
 #ifdef EXPERIMENTAL_SCOREALIGN
 
+#ifdef USE_MIDI
+static const ReservedCommandFlag&
+   NoteTracksSelectedFlag() { static ReservedCommandFlag flag{
+      [](const AudacityProject &project){
+         return !TrackList::Get( project ).Selected<const NoteTrack>().empty();
+      }
+   }; return flag; }  //gsw
+#endif
+
 // rough relative amount of time to compute one
 //    frame of audio or midi, or one cell of matrix, or one iteration
 //    of smoothing, measured on a 1.9GHz Core 2 Duo in 32-bit mode

tenacityteam/tenacity

(Modified for Tenacity)
(cherry picked from commit 2abf754bb54be081a31d9292ece46e94343e9a21)
Signed-off-by: Avery King <avery98@pm.me>

src/menus/TrackMenus.cpp
@@ -347,6 +347,15 @@ void DoAlign
 
 #ifdef EXPERIMENTAL_SCOREALIGN
 
+#ifdef USE_MIDI
+static const ReservedCommandFlag&
+   NoteTracksSelectedFlag() { static ReservedCommandFlag flag{
+      [](const TenacityProject &project){
+         return !TrackList::Get( project ).Selected<const NoteTrack>().empty();
+      }
+   }; return flag; }  //gsw
+#endif
+
 // rough relative amount of time to compute one
 //    frame of audio or midi, or one cell of matrix, or one iteration
 //    of smoothing, measured on a 1.9GHz Core 2 Duo in 32-bit mode