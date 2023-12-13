audacity/audacity

Some Settings objects for Midi, eliminate duplicate constant

src/NoteTrack.h
@@ -16,6 +16,7 @@
 
 
 #include <utility>
+#include "Prefs.h"
 #include "Track.h"
 
 #if defined(USE_MIDI)
@@ -269,6 +270,11 @@ public:
    // of the line separating B and C
    int GetWhitePos(int i) const { return 1 + (i * GetOctaveHeight()) / 7; }
 };
+
+extern AUDACITY_DLL_API StringSetting MIDIPlaybackDevice;
+extern AUDACITY_DLL_API StringSetting MIDIRecordingDevice;
+extern AUDACITY_DLL_API IntSetting MIDISynthLatency_ms;
+
 #endif // USE_MIDI
 
 #ifndef SONIFY

tenacityteam/tenacity

(Modified for Tenacity)
(cherry picked from commit f8916768430cc74e186a69e31482ff3644119025)
Signed-off-by: Avery King <avery98@pm.me>

src/NoteTrack.h
@@ -13,6 +13,7 @@
 
 
 #include <utility>
+#include "Prefs.h"
 #include "Track.h"
 
 #if defined(USE_MIDI)
@@ -272,6 +273,10 @@ public:
 };
 
 ENUMERATE_TRACK_TYPE(NoteTrack);
+
+extern TENACITY_DLL_API StringSetting MIDIPlaybackDevice;
+extern TENACITY_DLL_API StringSetting MIDIRecordingDevice;
+extern TENACITY_DLL_API IntSetting MIDISynthLatency_ms;
 
 #endif // USE_MIDI
 