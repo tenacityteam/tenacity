0c715f14cfd284a466b277c78bf88312617a0d8a
AnErrupTion <anerruption@disroot.org>
2024-01-31 22:01:53 +0100

    Add .clang-format, Qt MainWindow
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

---
+++ qt-src/Forms/MainWindow/MainWindow.h
@@ -0,0 +1,27 @@
+//
+// Created by anerruption on 31/01/24.
+//
+
+#ifndef TENACITY_MAINWINDOW_H
+#define TENACITY_MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif//TENACITY_MAINWINDOW_H
