dbf1a2f3eb568b38da8fc9afcf189bc4ce66b7a8
AnErrupTion <anerruption@disroot.org>
2024-01-31 22:01:53 +0100

    Add .clang-format, Qt MainWindow
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

---
+++ qt-src/Forms/MainWindow/MainWindow.cpp
@@ -0,0 +1,16 @@
+//
+// Created by anerruption on 31/01/24.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
+
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
