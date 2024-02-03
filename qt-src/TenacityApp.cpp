c43a572b500625078b6dcb82dd95dfacbbf87fba
AnErrupTion <anerruption@disroot.org>
2024-02-03 02:04:52 +0100

    Force Qt 6, start work on MainWindow
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

--- qt-src/TenacityApp.cpp
+++ qt-src/TenacityApp.cpp
@@ -11,16 +11,7 @@
 *******************************************************************************/
 
 #include "TenacityApp.h"
-#include "Forms/MainWindow/MainWindow.h"
 
-int main(int argc, char** argv)
-{
-    QCoreApplication::setApplicationName("Tenacity");
-
-    QApplication a(argc, argv);
-
-    MainWindow mainWindow {};
-    mainWindow.show();
-
-    return QApplication::exec();
+void TenacityApp::Start() {
+    // Startup initialization code will be placed here
 }
---
+++ qt-src/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by anerruption on 03/02/24.
+//
+
+#include "Forms/MainWindow/MainWindow.h"
+#include "TenacityApp.h"
+
+#include <QApplication>
+
+int main(int argc, char** argv)
+{
+    QCoreApplication::setApplicationName("Tenacity");
+
+    TenacityApp::Start();
+
+    QApplication a { argc, argv };
+
+    MainWindow mainWindow {};
+    mainWindow.show();
+
+    return QApplication::exec();
+}
