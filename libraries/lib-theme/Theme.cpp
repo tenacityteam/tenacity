21c19c6baffc9b6f9f31415ac61f7227b2f46dab
Avery King <avery98@pm.me>
2024-03-26 17:41:20 -0700

    Theme: Implement ThemePackage backing
    - Update comments
    - Handle attributes (only name for now)
    - Implement package-backed themes
    - Implement resource loading from theme packages
    
    Signed-off-by: Avery King <avery98@pm.me>

--- libraries/lib-theme/Theme.cpp
+++ libraries/lib-theme/Theme.cpp
@@ -11,6 +11,16 @@
 **********************************************************************/
 
 #include "Theme.h"
+#include "exceptions/ArchiveError.h"
+#include "exceptions/InvalidState.h"
+using namespace ThemeExceptions;
+
+#include <utility>
+
+Theme::Theme(ThemePackage&& package)
+{
+    mPackage = std::forward<ThemePackage>(package);
+}
 
 Theme& Theme::operator=(const Theme& other)
 {
@@ -21,6 +31,7 @@ Theme& Theme::operator=(const Theme& other)
 Theme& Theme::operator=(Theme&& other)
 {
     mResources = std::move(other.mResources);
+    mPackage = std::move(other.mPackage);
     return *this;
 }
 
@@ -29,7 +40,80 @@ const ThemeResourceMap& Theme::GetResourceMap()
     return mResources;
 }
 
+void Theme::SetName(const std::string& name)
+{
+    mName = name;
+}
+
+std::string Theme::GetName()
+{
+    return mName;
+}
+
+void Theme::SetPackage(ThemePackage&& package)
+{
+    mPackage = std::forward<ThemePackage>(package);
+}
+
+const ThemePackage& Theme::GetPackage()
+{
+    if (!mPackage)
+    {
+        throw InvalidState();
+    }
+
+    return mPackage.value();
+}
+
+void Theme::LoadAttributesFromPackage()
+{
+    if (!mPackage)
+    {
+        throw InvalidState();
+    }
+
+    // Load theme name
+    auto value = mPackage->GetAttribute("name");
+    mName = value.asString();
+}
+
+ThemePackage&& Theme::ReleasePackage()
+{
+    return std::move(mPackage.value());
+}
+
 const std::any& Theme::GetResource(const std::string& name)
 {
+    // First, search our map.
+    auto exists = mResources.find(name);
+    if (exists == mResources.end())
+    {
+        // The resource doesn't exist in our resource map, so load it from the
+        // backing package if it exists.
+        if (mPackage)
+        {
+            ThemePackage& package = mPackage.value();
+            try
+            {
+                auto resource = package.LoadResource(name);
+                mResources[name] = resource;
+            } catch(ArchiveError& ae)
+            {
+                // Rethrow the exception.
+                throw;
+            }
+        } else
+        {
+            throw ArchiveError(ArchiveError::Type::ResourceNotFound);
+        }
+    }
+
+    // At this point, we should've added the resource to our resource map, so
+    // go ahead and retrieve it from the map.
     return mResources.at(name);
+}
+
+void Theme::AddResource(const std::string& name, const std::any& data)
+{
+    mResources[name] = data;
 }
--- libraries/lib-theme/Theme.h
+++ libraries/lib-theme/Theme.h
@@ -13,27 +13,105 @@
 
 #include <any>
 #include <list>
+#include <optional>
 #include <unordered_map>
 #include <string>
 
+#include "ThemePackage.h"
 #include "Types.h"
 
+/** @brief Represents a theme in Tenacity.
+ * 
+ * This class is completely different from Audacity's Theme class (which
+ * Tenacity inherited). It is rewritten to contain its own set of theme
+ * resources rather than act as a singleton for managing themes. See @ref
+ * ThemeResources to see that functionality.
+ * 
+ * Themes can be constructed either from a ThemePackage or in-memory. If a
+ * theme is backed by a ThemePackage, it can commit any changes to the package.
+ * (Note that it's currently not possible to create an in-memory ThemePackage.
+ * See TODO for more details).
+ * 
+ * @todo Implementing the following:
+ * 
+ * 1. Support for multi-theme packages.
+ * 2. Support for committing in-memory changes.
+ * 
+*/
 class THEME_API Theme final
 {
+    private:
+        std::optional<ThemePackage> mPackage;
+        std::string mName;
+        ThemeResourceMap mResources;
+
     public:
         Theme() = default;
+
+        /// Copy constructor. Any backing theme package is **not** copied.
         Theme(const Theme& other) : mResources{other.mResources} {}
         Theme(Theme&& other) : mResources{std::move(other.mResources)} {}
 
+        /// Constructs a theme from a ThemePackage
+        Theme(ThemePackage&& package);
+
+        /// Copy assignment operator. Any backing theme package is **not**
+        /// copied.
         Theme& operator=(const Theme& other);
         Theme& operator=(Theme&& other);
 
-        /// @brief Returns the theme package's resurce map if available.
+        /// Returns the theme package's resurce map if available.
         const ThemeResourceMap& GetResourceMap();
+
+        void SetName(const std::string& name);
+        std::string GetName();
+
+        /** @brief Sets the backing theme package.
+         * 
+         * Any current backing package is closed after a call to this function.
+         * To unset the backing theme package, call @ref ReleasePackage()
+         * ignoring the return value.
+         * 
+         * Because ThemePackage cannot be copied, it must be moved, and thus
+         * Theme takes ownership over the package. You can still access the
+         * package via @ref GetPackage().
+         * 
+         * @param package The package to set.
+         * 
+        */
+        void SetPackage(ThemePackage&& package);
+
+        /// Returns the backing theme package.
+        const ThemePackage& GetPackage();
+
+        /** @brief Loads all attributes from a backing theme package.
+         * 
+         * @exception InvalidState Thrown if there is no backing package set.
+         * 
+        */
+        void LoadAttributesFromPackage();
+
+        /** @brief Releases the current theme package if any.
+         * 
+         * This member functions releases ownership of the current theme
+         * package to the current owner. Simply ignore the return value if you
+         * only want to reset the backing package.
+         * 
+        */
+        ThemePackage&& ReleasePackage();
 
         /** @brief Returns data associated with an individual resource.
          * 
+         * If the resource doesn't exist in the map, it is automatically loaded
+         * from the backing theme package if available. After it is loaded, it
+         * is added to its resource map.
+         * 
          * @param name The name of the resource to get.
+         * 
+         * @exception InvalidState Thrown if there is a valid backing theme
+         * package but the package is invalid.
+         * 
+         * @exception ArchiveError Thrown if the resource wasn't found.
          * 
          * @return Returns data with the associated resource.
          * 
@@ -46,8 +124,5 @@ class THEME_API Theme final
          * @param data The data to associate with the new resource. Optional.
          * 
         */
-       void AddResource(const std::string& name, const std::any data = {});
-
-    private:
-        ThemeResourceMap mResources;
+       void AddResource(const std::string& name, const std::any& data = {});
 };
