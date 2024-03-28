Paul-Licameli/audacity

WaveTrack inherits new WritableSampleTrack which inherits SampleTrack

src/WaveTrack.h
@@ -62,7 +62,7 @@ using Regions = std::vector < Region >;
 
 class Envelope;
 
-class AUDACITY_DLL_API WaveTrack final : public SampleTrack
+class AUDACITY_DLL_API WaveTrack final : public WritableSampleTrack
 {
 public:
    static wxString GetDefaultAudioTrackNamePreference();

tenacityteam/tenacity

(cherry picked from commit 7d4c12d6b2a09e34e8054e5980946b9e5cd36c38;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/WaveTrack.h
@@ -71,7 +71,7 @@ using Regions = std::vector < Region >;
 
 class Envelope;
 
-class TENACITY_DLL_API WaveTrack final : public SampleTrack
+class TENACITY_DLL_API WaveTrack final : public WritableSampleTrack
 {
 public:
 