alex74747/audacity

New base class SampleTrack, of WaveTrack

src/WaveTrack.h
@@ -12,7 +12,7 @@
 #define __AUDACITY_WAVETRACK__
 
 #include "Audacity.h"
-#include "Track.h"
+#include "SampleTrack.h"
 
 #include <vector>
 #include <functional>
@@ -70,7 +70,7 @@ using WaveTrackCaches = ClientData::Site<
    ClientData::DeepCopying
 >;
 
-class AUDACITY_DLL_API WaveTrack final : public PlayableTrack
+class AUDACITY_DLL_API WaveTrack final : public SampleTrack
    , public WaveTrackCaches
 {
 public:

tenacityteam/tenacity

(cherry picked from commit aa453cdc912886ac45e64ed8a62a7e3929d6f322;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/WaveTrack.h
@@ -17,6 +17,8 @@
 #include <lib-math/SampleCount.h>
 #include <lib-math/SampleFormat.h>
 
+#include "SampleTrack.h"
+
 #include <vector>
 #include <functional>
 #include <wx/longlong.h>
@@ -69,7 +71,8 @@ using Regions = std::vector < Region >;
 
 class Envelope;
 
-class TENACITY_DLL_API WaveTrack final : public PlayableTrack {
+class TENACITY_DLL_API WaveTrack final : public SampleTrack
+{
 public:
 
    //