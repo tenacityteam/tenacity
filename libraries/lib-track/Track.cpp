audacity/audacity

Issue =2424: Fix crash when recovering projects at startup...
... Bug was in 69562f1657c63ac467e023b0e143fc201c37cd3a

libraries/lib-track/Track.cpp
@@ -456,7 +456,10 @@ void TrackList::RecalcPositions(TrackNodePointer node)
 
 void TrackList::QueueEvent(TrackListEvent event)
 {
-   BasicUI::CallAfter( [this, event = std::move(event)]{ Publish(event); } );
+   BasicUI::CallAfter( [wThis = weak_from_this(), event = std::move(event)]{
+      if (auto pThis = wThis.lock())
+         pThis->Publish(event);
+   } );
 }
 
 void TrackList::SelectionEvent( const std::shared_ptr<Track> &pTrack )

tenacityteam/tenacity

(cherry-picked from commit 0b58571fe494728472cff46cb3201e804cbfc864;
modified for Tenacity).
Signed-off-by: Avery King <avery98@pm.me>

libraries/lib-track/Track.cpp
@@ -457,7 +457,10 @@ void TrackList::RecalcPositions(TrackNodePointer node)
 
 void TrackList::QueueEvent(TrackListEvent event)
 {
-   GenericUI::CallAfter( [this, event = std::move(event)]{ Publish(event); } );
+   GenericUI::CallAfter( [wThis = weak_from_this(), event = std::move(event)]{
+      if (auto pThis = wThis.lock())
+         pThis->Publish(event);
+   } );
 }
 
 void TrackList::SelectionEvent( const std::shared_ptr<Track> &pTrack )