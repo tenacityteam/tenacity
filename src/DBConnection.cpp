alex74747/audacity

Simplify TransactionScope's hook for implementation policy

src/DBConnection.cpp
@@ -601,16 +601,15 @@ struct DBConnectionTransactionScopeImpl final : TransactionScopeImpl {
    DBConnection &mConnection;
 };
 
-static struct Installer{ Installer() { TransactionScope::InstallImplementation(
-   [](AudacityProject &project) -> std::unique_ptr<TransactionScopeImpl> {
-      auto &connectionPtr = ConnectionPtr::Get(project);
-      if (auto pConnection = connectionPtr.mpConnection.get())
-         return
-            std::make_unique<DBConnectionTransactionScopeImpl>(*pConnection);
-      else
-         return nullptr;
-   }
-); } } installer;
+static TransactionScope::Factory::Scope scope {
+[](AudacityProject &project) -> std::unique_ptr<TransactionScopeImpl> {
+   auto &connectionPtr = ConnectionPtr::Get(project);
+   if (auto pConnection = connectionPtr.mpConnection.get())
+      return
+         std::make_unique<DBConnectionTransactionScopeImpl>(*pConnection);
+   else
+      return nullptr;
+} };
 
 DBConnectionTransactionScopeImpl::~DBConnectionTransactionScopeImpl() = default;
 

tenacityteam/tenacity

(cherry picked from commit 462aca7154f3349b03b9b5b566ff39fd24911df2;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/DBConnection.cpp
@@ -543,16 +543,15 @@ struct DBConnectionTransactionScopeImpl final : TransactionScopeImpl {
    DBConnection &mConnection;
 };
 
-static struct Installer{ Installer() { TransactionScope::InstallImplementation(
-   [](TenacityProject &project) -> std::unique_ptr<TransactionScopeImpl> {
-      auto &connectionPtr = ConnectionPtr::Get(project);
-      if (auto pConnection = connectionPtr.mpConnection.get())
-         return
-            std::make_unique<DBConnectionTransactionScopeImpl>(*pConnection);
-      else
-         return nullptr;
-   }
-); } } installer;
+static TransactionScope::Factory::Scope scope {
+[](TenacityProject &project) -> std::unique_ptr<TransactionScopeImpl> {
+   auto &connectionPtr = ConnectionPtr::Get(project);
+   if (auto pConnection = connectionPtr.mpConnection.get())
+      return
+         std::make_unique<DBConnectionTransactionScopeImpl>(*pConnection);
+   else
+      return nullptr;
+} };
 
 DBConnectionTransactionScopeImpl::~DBConnectionTransactionScopeImpl() = default;
 