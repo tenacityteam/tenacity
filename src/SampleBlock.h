alex74747/audacity

Simplify sample block factory hook

src/SampleBlock.h
@@ -9,6 +9,7 @@ SampleBlock.h
 #ifndef __AUDACITY_SAMPLE_BLOCK__
 #define __AUDACITY_SAMPLE_BLOCK__
 
+#include "GlobalVariable.h"
 #include "SampleFormat.h"
 
 #include <functional>
@@ -25,8 +26,6 @@ class SampleBlock;
 using SampleBlockPtr = std::shared_ptr<SampleBlock>;
 class SampleBlockFactory;
 using SampleBlockFactoryPtr = std::shared_ptr<SampleBlockFactory>;
-using SampleBlockFactoryFactory =
-   std::function< SampleBlockFactoryPtr( AudacityProject& ) >;
 
 using SampleBlockID = long long;
 
@@ -37,8 +36,6 @@ public:
    float max = 0;
    float RMS = 0;
 };
-
-class SqliteSampleBlockFactory;
 
 ///\brief Abstract class allows access to contents of a block of sound samples,
 /// serialization as XML, and reference count management that can suppress
@@ -107,11 +104,9 @@ BlockSpaceUsageAccumulator (unsigned long long &total)
 class SampleBlockFactory
 {
 public:
-   // Install global function that produces a sample block factory object for
-   // a given project; the factory has methods that later make sample blocks.
-   // Return the previously installed factory.
-   static SampleBlockFactoryFactory RegisterFactoryFactory(
-      SampleBlockFactoryFactory newFactory );
+   //! Global factory of per-project factories of sample blocks
+   using Factory = GlobalHook<SampleBlockFactory,
+      SampleBlockFactoryPtr( AudacityProject& )>;
 
    // Invoke the installed factory (throw an exception if none was installed)
    static SampleBlockFactoryPtr New( AudacityProject &project );

src/SqliteSampleBlock.cpp
@@ -1009,17 +1009,7 @@ void SqliteSampleBlock::CalcSummary(Sizes sizes)
 }
 
 // Inject our database implementation at startup
-static struct Injector
+static SampleBlockFactory::Factory::Scope scope{ []( AudacityProject &project )
 {
-   Injector()
-   {
-      // Do this some time before the first project is created
-      (void) SampleBlockFactory::RegisterFactoryFactory(
-         []( AudacityProject &project )
-         {
-            return std::make_shared<SqliteSampleBlockFactory>( project );
-         }
-      );
-   }
-} injector;
- 
+   return std::make_shared<SqliteSampleBlockFactory>( project );
+} };

tenacityteam/tenacity

(cherry picked from commit 170bcc35572e67319dc93acd0af47dd4fff65147;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/SampleBlock.h
@@ -11,6 +11,7 @@ SampleBlock.h
 
 // Tenacity libraries
 #include <lib-math/SampleFormat.h>
+#include <lib-utility/GlobalVariable.h>
 
 #include <functional>
 #include <memory>
@@ -26,8 +27,6 @@ class SampleBlock;
 using SampleBlockPtr = std::shared_ptr<SampleBlock>;
 class SampleBlockFactory;
 using SampleBlockFactoryPtr = std::shared_ptr<SampleBlockFactory>;
-using SampleBlockFactoryFactory =
-   std::function< SampleBlockFactoryPtr( TenacityProject& ) >;
 
 using SampleBlockID = long long;
 
@@ -38,8 +37,6 @@ public:
    float max = 0;
    float RMS = 0;
 };
-
-class SqliteSampleBlockFactory;
 
 ///\brief Abstract class allows access to contents of a block of sound samples,
 /// serialization as XML, and reference count management that can suppress
@@ -108,11 +105,9 @@ BlockSpaceUsageAccumulator (unsigned long long &total)
 class SampleBlockFactory
 {
 public:
-   // Install global function that produces a sample block factory object for
-   // a given project; the factory has methods that later make sample blocks.
-   // Return the previously installed factory.
-   static SampleBlockFactoryFactory RegisterFactoryFactory(
-      SampleBlockFactoryFactory newFactory );
+   //! Global factory of per-project factories of sample blocks
+   using Factory = GlobalHook<SampleBlockFactory,
+      SampleBlockFactoryPtr( TenacityProject& )>;
 
    // Invoke the installed factory (throw an exception if none was installed)
    static SampleBlockFactoryPtr New( TenacityProject &project );

src/SqliteSampleBlock.cpp
@@ -978,17 +978,7 @@ void SqliteSampleBlock::CalcSummary(Sizes sizes)
 }
 
 // Inject our database implementation at startup
-static struct Injector
+static SampleBlockFactory::Factory::Scope scope{ []( TenacityProject &project )
 {
-   Injector()
-   {
-      // Do this some time before the first project is created
-      (void) SampleBlockFactory::RegisterFactoryFactory(
-         []( TenacityProject &project )
-         {
-            return std::make_shared<SqliteSampleBlockFactory>( project );
-         }
-      );
-   }
-} injector;
- 
+   return std::make_shared<SqliteSampleBlockFactory>( project );
+} };