audacity/audacity

Reimplement TransactionScope...
... This abstracts database transactions, simplifies usage, and breaks some
dependencies on DBConnection and ProjectFileIO; importantly, from AudioIO,
which will lead to extracting that into another library.

DBConnection.cpp injects the concrete sqlite-based implementation of the
abstraction at startup time.

src/DBConnection.cpp
@@ -586,8 +586,34 @@ int DBConnection::CheckpointHook(void *data, sqlite3 *db, const char *schema, in
    return SQLITE_OK;
 }
 
-#if 0
-bool TransactionScope::TransactionStart(const wxString &name)
+// Install an implementation of TransactionScope
+#include "TransactionScope.h"
+
+struct DBConnectionTransactionScopeImpl final : TransactionScopeImpl {
+   explicit DBConnectionTransactionScopeImpl(DBConnection &connection)
+      : mConnection{ connection } {}
+   ~DBConnectionTransactionScopeImpl() override;
+   bool TransactionStart(const wxString &name) override;
+   bool TransactionCommit(const wxString &name) override;
+   bool TransactionRollback(const wxString &name) override;
+
+   DBConnection &mConnection;
+};
+
+static struct Installer{ Installer() { TransactionScope::InstallImplementation(
+   [](AudacityProject &project) -> std::unique_ptr<TransactionScopeImpl> {
+      auto &connectionPtr = ConnectionPtr::Get(project);
+      if (auto pConnection = connectionPtr.mpConnection.get())
+         return
+            std::make_unique<DBConnectionTransactionScopeImpl>(*pConnection);
+      else
+         return nullptr;
+   }
+); } } installer;
+
+DBConnectionTransactionScopeImpl::~DBConnectionTransactionScopeImpl() = default;
+
+bool DBConnectionTransactionScopeImpl::TransactionStart(const wxString &name)
 {
    char *errmsg = nullptr;
 
@@ -611,7 +637,7 @@ bool TransactionScope::TransactionStart(const wxString &name)
    return rc == SQLITE_OK;
 }
 
-bool TransactionScope::TransactionCommit(const wxString &name)
+bool DBConnectionTransactionScopeImpl::TransactionCommit(const wxString &name)
 {
    char *errmsg = nullptr;
 
@@ -635,7 +661,7 @@ bool TransactionScope::TransactionCommit(const wxString &name)
    return rc == SQLITE_OK;
 }
 
-bool TransactionScope::TransactionRollback(const wxString &name)
+bool DBConnectionTransactionScopeImpl::TransactionRollback(const wxString &name)
 {
    char *errmsg = nullptr;
 
@@ -649,63 +675,21 @@ bool TransactionScope::TransactionRollback(const wxString &name)
    {
       ADD_EXCEPTION_CONTEXT("sqlite3.rc", std::to_string(rc));
       ADD_EXCEPTION_CONTEXT("sqlite3.context", "TransactionScope::TransactionRollback");
-
       mConnection.SetDBError(
          XO("Failed to release savepoint:\n\n%s").Format(name)
       );
       sqlite3_free(errmsg);
    }
 
-   return rc == SQLITE_OK;
+   if (rc != SQLITE_OK)
+      return false;
+
+   // Rollback AND REMOVE the transaction
+   // -- must do both; rolling back a savepoint only rewinds it
+   // without removing it, unlike the ROLLBACK command
+
+   return TransactionCommit(name);
 }
-
-TransactionScope::TransactionScope(
-   DBConnection &connection, const char *name)
-:  mConnection(connection),
-   mName(name)
-{
-   mInTrans = TransactionStart(mName);
-   if ( !mInTrans )
-      // To do, improve the message
-      throw SimpleMessageBoxException( ExceptionType::Internal,
-         XO("Database error.  Sorry, but we don't have more details."), 
-         XO("Warning"), 
-         "Error:_Disk_full_or_not_writable"
-      );
-}
-
-TransactionScope::~TransactionScope()
-{
-   if (mInTrans)
-   {
-      // Rollback AND REMOVE the transaction
-      // -- must do both; rolling back a savepoint only rewinds it
-      // without removing it, unlike the ROLLBACK command
-      if (!(TransactionRollback(mName) &&
-            TransactionCommit(mName) ) )
-      {
-         // Do not throw from a destructor!
-         // This has to be a no-fail cleanup that does the best that it can.
-         wxLogMessage("Transaction active at scope destruction");
-      }
-   }
-}
-
-bool TransactionScope::Commit()
-{
-   if ( !mInTrans )
-   {
-      wxLogMessage("No active transaction to commit");
-
-      // Misuse of this class
-      THROW_INCONSISTENCY_EXCEPTION;
-   }
-
-   mInTrans = !TransactionCommit(mName);
-
-   return !mInTrans;
-}
-#endif
 
 ConnectionPtr::~ConnectionPtr()
 {

src/TransactionScope.cpp
@@ -1,99 +1,48 @@
 /*!********************************************************************
 
-Audacity: A Digital Audio Editor
+  Audacity: A Digital Audio Editor
 
-@file TransactionScope.cpp
+  @file TransactionScope.cpp
 
-Paul Licameli -- split from DBConnection.cpp
+  Paul Licameli split from DBConnection.cpp
 
 **********************************************************************/
 
 #include "TransactionScope.h"
-
-#include "DBConnection.h"
-
-#include "sqlite3.h"
-#include "SentryHelper.h"
+#include "InconsistencyException.h"
 #include <wx/log.h>
 
-bool TransactionScope::TransactionStart(const wxString &name)
+namespace {
+TransactionScope::TransactionScopeImplFactory &GetFactory()
 {
-   char *errmsg = nullptr;
-
-   int rc = sqlite3_exec(mConnection.DB(),
-                         wxT("SAVEPOINT ") + name + wxT(";"),
-                         nullptr,
-                         nullptr,
-                         &errmsg);
-
-   if (errmsg)
-   {
-      ADD_EXCEPTION_CONTEXT("sqlite3.rc", std::to_string(rc));
-      ADD_EXCEPTION_CONTEXT("sqlite3.context", "TransactionScope::TransactionStart");
-
-      mConnection.SetDBError(
-         XO("Failed to create savepoint:\n\n%s").Format(name)
-      );
-      sqlite3_free(errmsg);
-   }
-
-   return rc == SQLITE_OK;
+   static TransactionScope::TransactionScopeImplFactory theFactory;
+   return theFactory;
+}
 }
 
-bool TransactionScope::TransactionCommit(const wxString &name)
+auto TransactionScope::InstallImplementation(
+   TransactionScopeImplFactory factory) -> TransactionScopeImplFactory
 {
-   char *errmsg = nullptr;
-
-   int rc = sqlite3_exec(mConnection.DB(),
-                         wxT("RELEASE ") + name + wxT(";"),
-                         nullptr,
-                         nullptr,
-                         &errmsg);
-
-   if (errmsg)
-   {
-      ADD_EXCEPTION_CONTEXT("sqlite3.rc", std::to_string(rc));
-      ADD_EXCEPTION_CONTEXT("sqlite3.context", "TransactionScope::TransactionCommit");
-
-      mConnection.SetDBError(
-         XO("Failed to release savepoint:\n\n%s").Format(name)
-      );
-      sqlite3_free(errmsg);
-   }
-
-   return rc == SQLITE_OK;
+   auto &theFactory = GetFactory();
+   auto result = theFactory;
+   theFactory = move(factory);
+   return result;
 }
 
-bool TransactionScope::TransactionRollback(const wxString &name)
-{
-   char *errmsg = nullptr;
-
-   int rc = sqlite3_exec(mConnection.DB(),
-                         wxT("ROLLBACK TO ") + name + wxT(";"),
-                         nullptr,
-                         nullptr,
-                         &errmsg);
-
-   if (errmsg)
-   {
-      ADD_EXCEPTION_CONTEXT("sqlite3.rc", std::to_string(rc));
-      ADD_EXCEPTION_CONTEXT("sqlite3.context", "TransactionScope::TransactionRollback");
-
-      mConnection.SetDBError(
-         XO("Failed to release savepoint:\n\n%s").Format(name)
-      );
-      sqlite3_free(errmsg);
-   }
-
-   return rc == SQLITE_OK;
-}
+TransactionScopeImpl::~TransactionScopeImpl() = default;
 
 TransactionScope::TransactionScope(
-   DBConnection &connection, const char *name)
-:  mConnection(connection),
-   mName(name)
+   AudacityProject &project, const char *name)
+:  mName(name)
 {
-   mInTrans = TransactionStart(mName);
+   auto &factory = GetFactory();
+   if (!factory)
+      return;
+   mpImpl = factory(project);
+   if (!mpImpl)
+      return;
+
+   mInTrans = mpImpl->TransactionStart(mName);
    if ( !mInTrans )
       // To do, improve the message
       throw SimpleMessageBoxException( ExceptionType::Internal,
@@ -105,13 +54,9 @@ TransactionScope::TransactionScope(
 
 TransactionScope::~TransactionScope()
 {
-   if (mInTrans)
+   if (mpImpl && mInTrans)
    {
-      // Rollback AND REMOVE the transaction
-      // -- must do both; rolling back a savepoint only rewinds it
-      // without removing it, unlike the ROLLBACK command
-      if (!(TransactionRollback(mName) &&
-            TransactionCommit(mName) ) )
+      if (!mpImpl->TransactionRollback(mName))
       {
          // Do not throw from a destructor!
          // This has to be a no-fail cleanup that does the best that it can.
@@ -122,15 +67,15 @@ TransactionScope::~TransactionScope()
 
 bool TransactionScope::Commit()
 {
-   if ( !mInTrans )
-   {
+   if (mpImpl && !mInTrans) {
       wxLogMessage("No active transaction to commit");
-
       // Misuse of this class
       THROW_INCONSISTENCY_EXCEPTION;
    }
 
-   mInTrans = !TransactionCommit(mName);
+   // If commit is unsuccessful, consider us still in a commit, for the dtor
+   // later
+   mInTrans = !mpImpl->TransactionCommit(mName);
 
-   return !mInTrans;
+   return mInTrans;
 }

src/TransactionScope.h
@@ -7,12 +7,15 @@
   Paul Licameli split from DBConnection.h
 
 **********************************************************************/
-
 #ifndef __AUDACITY_TRANSACTION_SCOPE__
 #define __AUDACITY_TRANSACTION_SCOPE__
 
+class AudacityProject;
+#include <functional>
+#include <memory>
 #include <wx/string.h>
-class DBConnection;
+
+class TransactionScopeImpl;
 
 //! RAII for a database transaction, possibly nested
 /*! Make a savepoint (a transaction, possibly nested) with the given name;
@@ -23,19 +26,42 @@ class DBConnection;
 class AUDACITY_DLL_API TransactionScope
 {
 public:
-   TransactionScope(DBConnection &connection, const char *name);
+   //! Type of function supplying implementation of steps
+   using TransactionScopeImplFactory = std::function<
+      std::unique_ptr<TransactionScopeImpl>(AudacityProject &) >;
+
+   //! Installs global factory; returns previously installed factory
+   static TransactionScopeImplFactory
+   InstallImplementation(TransactionScopeImplFactory factory);
+
+   //! Construct from a project
+   /*!
+    If no implementation factory is installed, or the factory returns null,
+    then this object does nothing */
+   TransactionScope(AudacityProject &project, const char *name);
+
+   //! Rollback transaction if it was not yet committed
    ~TransactionScope();
 
+   //! Commit the transaction
    bool Commit();
 
 private:
-   bool TransactionStart(const wxString &name);
-   bool TransactionCommit(const wxString &name);
-   bool TransactionRollback(const wxString &name);
-
-   DBConnection &mConnection;
+   std::unique_ptr<TransactionScopeImpl> mpImpl;
    bool mInTrans;
    wxString mName;
+};
+
+//! Abstract base class for implementation of steps of TransactionScope
+class AUDACITY_DLL_API TransactionScopeImpl {
+public:
+   virtual ~TransactionScopeImpl();
+   //! @return success; if false, TransactionScope ctor throws
+   virtual bool TransactionStart(const wxString &name) = 0;
+   //! @return success
+   virtual bool TransactionCommit(const wxString &name) = 0;
+   //! @return success
+   virtual bool TransactionRollback(const wxString &name) = 0;
 };
 
 #endif

tenacityteam/tenacity

(cherry picked from commit 339eb5fa3df6a2b4f0762eaf254163ce0a7b4feb;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/DBConnection.cpp
@@ -529,8 +529,34 @@ int DBConnection::CheckpointHook(void *data, sqlite3 *db, const char *schema, in
    return SQLITE_OK;
 }
 
-#if 0
-bool TransactionScope::TransactionStart(const wxString &name)
+// Install an implementation of TransactionScope
+#include "TransactionScope.h"
+
+struct DBConnectionTransactionScopeImpl final : TransactionScopeImpl {
+   explicit DBConnectionTransactionScopeImpl(DBConnection &connection)
+      : mConnection{ connection } {}
+   ~DBConnectionTransactionScopeImpl() override;
+   bool TransactionStart(const wxString &name) override;
+   bool TransactionCommit(const wxString &name) override;
+   bool TransactionRollback(const wxString &name) override;
+
+   DBConnection &mConnection;
+};
+
+static struct Installer{ Installer() { TransactionScope::InstallImplementation(
+   [](TenacityProject &project) -> std::unique_ptr<TransactionScopeImpl> {
+      auto &connectionPtr = ConnectionPtr::Get(project);
+      if (auto pConnection = connectionPtr.mpConnection.get())
+         return
+            std::make_unique<DBConnectionTransactionScopeImpl>(*pConnection);
+      else
+         return nullptr;
+   }
+); } } installer;
+
+DBConnectionTransactionScopeImpl::~DBConnectionTransactionScopeImpl() = default;
+
+bool DBConnectionTransactionScopeImpl::TransactionStart(const wxString &name)
 {
    char *errmsg = nullptr;
 
@@ -551,7 +577,7 @@ bool TransactionScope::TransactionStart(const wxString &name)
    return rc == SQLITE_OK;
 }
 
-bool TransactionScope::TransactionCommit(const wxString &name)
+bool DBConnectionTransactionScopeImpl::TransactionCommit(const wxString &name)
 {
    char *errmsg = nullptr;
 
@@ -572,7 +598,7 @@ bool TransactionScope::TransactionCommit(const wxString &name)
    return rc == SQLITE_OK;
 }
 
-bool TransactionScope::TransactionRollback(const wxString &name)
+bool DBConnectionTransactionScopeImpl::TransactionRollback(const wxString &name)
 {
    char *errmsg = nullptr;
 
@@ -590,56 +616,15 @@ bool TransactionScope::TransactionRollback(const wxString &name)
       sqlite3_free(errmsg);
    }
 
-   return rc == SQLITE_OK;
+   if (rc != SQLITE_OK)
+      return false;
+
+   // Rollback AND REMOVE the transaction
+   // -- must do both; rolling back a savepoint only rewinds it
+   // without removing it, unlike the ROLLBACK command
+
+   return TransactionCommit(name);
 }
-
-TransactionScope::TransactionScope(
-   DBConnection &connection, const char *name)
-:  mConnection(connection),
-   mName(name)
-{
-   mInTrans = TransactionStart(mName);
-   if ( !mInTrans )
-      // To do, improve the message
-      throw SimpleMessageBoxException( ExceptionType::Internal,
-         XO("Database error.  Sorry, but we don't have more details."), 
-         XO("Warning"), 
-         "Error:_Disk_full_or_not_writable"
-      );
-}
-
-TransactionScope::~TransactionScope()
-{
-   if (mInTrans)
-   {
-      // Rollback AND REMOVE the transaction
-      // -- must do both; rolling back a savepoint only rewinds it
-      // without removing it, unlike the ROLLBACK command
-      if (!(TransactionRollback(mName) &&
-            TransactionCommit(mName) ) )
-      {
-         // Do not throw from a destructor!
-         // This has to be a no-fail cleanup that does the best that it can.
-         wxLogMessage("Transaction active at scope destruction");
-      }
-   }
-}
-
-bool TransactionScope::Commit()
-{
-   if ( !mInTrans )
-   {
-      wxLogMessage("No active transaction to commit");
-
-      // Misuse of this class
-      THROW_INCONSISTENCY_EXCEPTION;
-   }
-
-   mInTrans = !TransactionCommit(mName);
-
-   return !mInTrans;
-}
-#endif
 
 ConnectionPtr::~ConnectionPtr()
 {

src/TransactionScope.cpp
@@ -1,89 +1,48 @@
 /*!********************************************************************
 
-Audacity: A Digital Audio Editor
+  Audacity: A Digital Audio Editor
 
-@file TransactionScope.cpp
+  @file TransactionScope.cpp
 
-Paul Licameli -- split from DBConnection.cpp
+  Paul Licameli split from DBConnection.cpp
 
 **********************************************************************/
 
 #include "TransactionScope.h"
-
-#include "DBConnection.h"
-
-#include "sqlite3.h"
+#include "InconsistencyException.h"
 #include <wx/log.h>
 
-bool TransactionScope::TransactionStart(const wxString &name)
+namespace {
+TransactionScope::TransactionScopeImplFactory &GetFactory()
 {
-   char *errmsg = nullptr;
-
-   int rc = sqlite3_exec(mConnection.DB(),
-                         wxT("SAVEPOINT ") + name + wxT(";"),
-                         nullptr,
-                         nullptr,
-                         &errmsg);
-
-   if (errmsg)
-   {
-      mConnection.SetDBError(
-         XO("Failed to create savepoint:\n\n%s").Format(name)
-      );
-      sqlite3_free(errmsg);
-   }
-
-   return rc == SQLITE_OK;
+   static TransactionScope::TransactionScopeImplFactory theFactory;
+   return theFactory;
+}
 }
 
-bool TransactionScope::TransactionCommit(const wxString &name)
+auto TransactionScope::InstallImplementation(
+   TransactionScopeImplFactory factory) -> TransactionScopeImplFactory
 {
-   char *errmsg = nullptr;
-
-   int rc = sqlite3_exec(mConnection.DB(),
-                         wxT("RELEASE ") + name + wxT(";"),
-                         nullptr,
-                         nullptr,
-                         &errmsg);
-
-   if (errmsg)
-   {
-      mConnection.SetDBError(
-         XO("Failed to release savepoint:\n\n%s").Format(name)
-      );
-      sqlite3_free(errmsg);
-   }
-
-   return rc == SQLITE_OK;
+   auto &theFactory = GetFactory();
+   auto result = theFactory;
+   theFactory = move(factory);
+   return result;
 }
 
-bool TransactionScope::TransactionRollback(const wxString &name)
-{
-   char *errmsg = nullptr;
-
-   int rc = sqlite3_exec(mConnection.DB(),
-                         wxT("ROLLBACK TO ") + name + wxT(";"),
-                         nullptr,
-                         nullptr,
-                         &errmsg);
-
-   if (errmsg)
-   {
-      mConnection.SetDBError(
-         XO("Failed to release savepoint:\n\n%s").Format(name)
-      );
-      sqlite3_free(errmsg);
-   }
-
-   return rc == SQLITE_OK;
-}
+TransactionScopeImpl::~TransactionScopeImpl() = default;
 
 TransactionScope::TransactionScope(
-   DBConnection &connection, const char *name)
-:  mConnection(connection),
-   mName(name)
+   TenacityProject &project, const char *name)
+:  mName(name)
 {
-   mInTrans = TransactionStart(mName);
+   auto &factory = GetFactory();
+   if (!factory)
+      return;
+   mpImpl = factory(project);
+   if (!mpImpl)
+      return;
+
+   mInTrans = mpImpl->TransactionStart(mName);
    if ( !mInTrans )
       // To do, improve the message
       throw SimpleMessageBoxException( ExceptionType::Internal,
@@ -95,13 +54,9 @@ TransactionScope::TransactionScope(
 
 TransactionScope::~TransactionScope()
 {
-   if (mInTrans)
+   if (mpImpl && mInTrans)
    {
-      // Rollback AND REMOVE the transaction
-      // -- must do both; rolling back a savepoint only rewinds it
-      // without removing it, unlike the ROLLBACK command
-      if (!(TransactionRollback(mName) &&
-            TransactionCommit(mName) ) )
+      if (!mpImpl->TransactionRollback(mName))
       {
          // Do not throw from a destructor!
          // This has to be a no-fail cleanup that does the best that it can.
@@ -112,15 +67,15 @@ TransactionScope::~TransactionScope()
 
 bool TransactionScope::Commit()
 {
-   if ( !mInTrans )
-   {
+   if (mpImpl && !mInTrans) {
       wxLogMessage("No active transaction to commit");
-
       // Misuse of this class
       THROW_INCONSISTENCY_EXCEPTION;
    }
 
-   mInTrans = !TransactionCommit(mName);
+   // If commit is unsuccessful, consider us still in a commit, for the dtor
+   // later
+   mInTrans = !mpImpl->TransactionCommit(mName);
 
-   return !mInTrans;
+   return mInTrans;
 }

src/TransactionScope.h
@@ -7,12 +7,15 @@
   Paul Licameli split from DBConnection.h
 
 **********************************************************************/
-
 #ifndef __AUDACITY_TRANSACTION_SCOPE__
 #define __AUDACITY_TRANSACTION_SCOPE__
 
+class TenacityProject;
+#include <functional>
+#include <memory>
 #include <wx/string.h>
-class DBConnection;
+
+class TransactionScopeImpl;
 
 //! RAII for a database transaction, possibly nested
 /*! Make a savepoint (a transaction, possibly nested) with the given name;
@@ -23,19 +26,42 @@ class DBConnection;
 class TENACITY_DLL_API TransactionScope
 {
 public:
-   TransactionScope(DBConnection &connection, const char *name);
+   //! Type of function supplying implementation of steps
+   using TransactionScopeImplFactory = std::function<
+      std::unique_ptr<TransactionScopeImpl>(TenacityProject &) >;
+
+   //! Installs global factory; returns previously installed factory
+   static TransactionScopeImplFactory
+   InstallImplementation(TransactionScopeImplFactory factory);
+
+   //! Construct from a project
+   /*!
+    If no implementation factory is installed, or the factory returns null,
+    then this object does nothing */
+   TransactionScope(TenacityProject &project, const char *name);
+
+   //! Rollback transaction if it was not yet committed
    ~TransactionScope();
 
+   //! Commit the transaction
    bool Commit();
 
 private:
-   bool TransactionStart(const wxString &name);
-   bool TransactionCommit(const wxString &name);
-   bool TransactionRollback(const wxString &name);
-
-   DBConnection &mConnection;
+   std::unique_ptr<TransactionScopeImpl> mpImpl;
    bool mInTrans;
    wxString mName;
+};
+
+//! Abstract base class for implementation of steps of TransactionScope
+class TENACITY_DLL_API TransactionScopeImpl {
+public:
+   virtual ~TransactionScopeImpl();
+   //! @return success; if false, TransactionScope ctor throws
+   virtual bool TransactionStart(const wxString &name) = 0;
+   //! @return success
+   virtual bool TransactionCommit(const wxString &name) = 0;
+   //! @return success
+   virtual bool TransactionRollback(const wxString &name) = 0;
 };
 
 #endif