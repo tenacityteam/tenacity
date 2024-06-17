alex74747/audacity

Invert dependency of WaveTrack on TracksPrefs...
... and eliminate some duplicate string literals

src/WaveTrack.h
@@ -65,6 +65,8 @@ class Envelope;
 class AUDACITY_DLL_API WaveTrack final : public SampleTrack
 {
 public:
+   static wxString GetDefaultAudioTrackNamePreference();
+
    //
    // Constructor / Destructor / Duplicator
    //
@@ -627,5 +629,7 @@ class AUDACITY_DLL_API WaveTrackFactory final
       sampleFormat format = (sampleFormat)0,
       double rate = 0);
 };
+
+extern AUDACITY_DLL_API StringSetting AudioTrackNameSetting;
 
 #endif // __AUDACITY_WAVETRACK__

tenacityteam/tenacity

... and eliminate some duplicate string literals with a Setting object
(cherry picked from commit 90eeeb8c304050397553c003e33886e197382ee2;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/WaveTrack.h
@@ -16,6 +16,7 @@
 // Tenacity libraries
 #include <lib-math/SampleCount.h>
 #include <lib-math/SampleFormat.h>
+#include <lib-preferences/Prefs.h>
 
 #include "SampleTrack.h"
 
@@ -77,6 +78,7 @@ class Envelope;
 class TENACITY_DLL_API WaveTrack final : public WritableSampleTrack
 {
 public:
+   static wxString GetDefaultAudioTrackNamePreference();
 
    //
    // Constructor / Destructor / Duplicator
@@ -638,5 +640,7 @@ class TENACITY_DLL_API WaveTrackFactory final
       sampleFormat format = (sampleFormat)0,
       double rate = 0);
 };
+
+extern TENACITY_DLL_API StringSetting AudioTrackNameSetting;
 
 #endif // __AUDACITY_WAVETRACK__