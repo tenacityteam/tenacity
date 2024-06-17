audacity/audacity

Move function, so WaveTrack does not depend on TracksBehaviorsPrefs

src/WaveTrack.h
@@ -637,4 +637,6 @@ class AUDACITY_DLL_API WaveTrackFactory final
 
 extern AUDACITY_DLL_API StringSetting AudioTrackNameSetting;
 
+AUDACITY_DLL_API bool GetEditClipsCanMove();
+
 #endif // __AUDACITY_WAVETRACK__

tenacityteam/tenacity

(cherry picked from commit 7ecb64d57a676ea73f4f89f5566cffe3e6cc67bd;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/WaveTrack.h
@@ -643,4 +643,6 @@ class TENACITY_DLL_API WaveTrackFactory final
 
 extern TENACITY_DLL_API StringSetting AudioTrackNameSetting;
 
+TENACITY_DLL_API bool GetEditClipsCanMove();
+
 #endif // __AUDACITY_WAVETRACK__