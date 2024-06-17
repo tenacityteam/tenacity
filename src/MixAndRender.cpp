audacity/audacity

MixAndRender does not depend on ProgressDialog or wxSafeYield...
... The yield before putting up the progress dialog is just eliminated.  It
gives no obvious added benefit.

src/MixAndRender.cpp
@@ -10,9 +10,9 @@ Paul Licameli split from Mix.cpp
 
 #include "MixAndRender.h"
 
+#include "BasicUI.h"
 #include "Mix.h"
 #include "WaveTrack.h"
-#include "widgets/ProgressDialog.h"
 
 using WaveTrackConstArray = std::vector < std::shared_ptr < const WaveTrack > >;
 
@@ -128,11 +128,10 @@ void MixAndRender(TrackList *tracks, WaveTrackFactory *trackFactory,
       startTime, endTime, mono ? 1 : 2, maxBlockLen, false,
       rate, format);
 
-   ::wxSafeYield();
-
+   using namespace BasicUI;
    auto updateResult = ProgressResult::Success;
    {
-      ProgressDialog progress(XO("Mix and Render"),
+      auto pProgress = MakeProgress(XO("Mix and Render"),
          XO("Mixing and rendering tracks"));
 
       while (updateResult == ProgressResult::Success) {
@@ -152,7 +151,8 @@ void MixAndRender(TrackList *tracks, WaveTrackFactory *trackFactory,
             mixRight->Append(buffer, format, blockLen);
          }
 
-         updateResult = progress.Update(mixer.MixGetCurrentTime() - startTime, endTime - startTime);
+         updateResult = pProgress->Poll(
+            mixer.MixGetCurrentTime() - startTime, endTime - startTime);
       }
    }
 

tenacityteam/tenacity

(cherry picked from commit 5024a0413f6e1361df6f41da51c1028e61c8fdb4;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/MixAndRender.cpp
@@ -10,9 +10,9 @@ Paul Licameli split from Mix.cpp
 
 #include "MixAndRender.h"
 
+#include "BasicUI.h"
 #include "Mix.h"
 #include "WaveTrack.h"
-#include "widgets/ProgressDialog.h"
 
 using WaveTrackConstArray = std::vector < std::shared_ptr < const WaveTrack > >;
 
@@ -128,11 +128,10 @@ void MixAndRender(TrackList *tracks, WaveTrackFactory *trackFactory,
       startTime, endTime, mono ? 1 : 2, maxBlockLen, false,
       rate, format);
 
-   ::wxSafeYield();
-
+   using namespace GenericUI;
    auto updateResult = ProgressResult::Success;
    {
-      ProgressDialog progress(XO("Mix and Render"),
+      auto pProgress = MakeProgress(XO("Mix and Render"),
          XO("Mixing and rendering tracks"));
 
       while (updateResult == ProgressResult::Success) {
@@ -152,7 +151,8 @@ void MixAndRender(TrackList *tracks, WaveTrackFactory *trackFactory,
             mixRight->Append(buffer, format, blockLen);
          }
 
-         updateResult = progress.Update(mixer.MixGetCurrentTime() - startTime, endTime - startTime);
+         updateResult = pProgress->Poll(
+            mixer.MixGetCurrentTime() - startTime, endTime - startTime);
       }
    }
 