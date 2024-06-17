audacity/audacity

Sequence does not use AudacityMessageBox...
... and the one use of message boxes is for an odd case of bad data in files,
which might never happen now

src/Sequence.cpp
@@ -41,9 +41,9 @@
 #include <wx/ffile.h>
 #include <wx/log.h>
 
+#include "BasicUI.h"
 #include "SampleBlock.h"
 #include "InconsistencyException.h"
-#include "widgets/AudacityMessageBox.h"
 
 size_t Sequence::sMaxDiskBlockSize = 1048576;
 
@@ -963,13 +963,16 @@ void Sequence::WriteXML(XMLWriter &xmlFile) const
          // editing operation that caused this, not fixing
          // the problem but moving the point of detection earlier if we
          // find a reproducible case.
+         using namespace BasicUI;
          auto sMsg =
             XO("Sequence has block file exceeding maximum %s samples per block.\nTruncating to this maximum length.")
                .Format( Internat::ToString(((wxLongLong)mMaxSamples).ToDouble(), 0) );
-         AudacityMessageBox(
+         ShowMessageBox(
             sMsg,
-            XO("Warning - Truncating Overlong Block File"),
-            wxICON_EXCLAMATION | wxOK);
+            MessageBoxOptions{}
+               .Caption(XO("Warning - Truncating Overlong Block File"))
+               .IconStyle(Icon::Warning)
+               .ButtonStyle(Button::Ok));
          wxLogWarning(sMsg.Translation()); //Debug?
 //         bb.sb->SetLength(mMaxSamples);
       }

tenacityteam/tenacity

(cherry picked from commit 990a5477cb1632a21842d22f82e46b2bc4a37ea5;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/Sequence.cpp
@@ -40,8 +40,8 @@
 #include <wx/ffile.h>
 #include <wx/log.h>
 
+#include "BasicUI.h"
 #include "SampleBlock.h"
-#include "widgets/AudacityMessageBox.h"
 
 // Tenacity libraries
 #include <lib-exceptions/InconsistencyException.h>
@@ -963,13 +963,16 @@ void Sequence::WriteXML(XMLWriter &xmlFile) const
          // editing operation that caused this, not fixing
          // the problem but moving the point of detection earlier if we
          // find a reproducible case.
+         using namespace GenericUI;
          auto sMsg =
             XO("Sequence has block file exceeding maximum %s samples per block.\nTruncating to this maximum length.")
                .Format( Internat::ToString(((wxLongLong)mMaxSamples).ToDouble(), 0) );
-         AudacityMessageBox(
+         ShowMessageBox(
             sMsg,
-            XO("Warning - Truncating Overlong Block File"),
-            wxICON_EXCLAMATION | wxOK);
+            MessageBoxOptions{}
+               .Caption(XO("Warning - Truncating Overlong Block File"))
+               .IconStyle(Icon::Warning)
+               .ButtonStyle(Button::Ok));
          wxLogWarning(sMsg.Translation()); //Debug?
 //         bb.sb->SetLength(mMaxSamples);
       }