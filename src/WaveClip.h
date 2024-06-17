audacity/audacity

WaveClip does not depend on ProgressDialog...
... And Effect needs a BasicUI::ProgressDialog to make this work (because
EffectStereoToMono must pass the changed type of progress dialog to
WaveClip::Resample)

Also remove unused Profiler.h

src/WaveClip.h
@@ -35,6 +35,7 @@ class SpectrogramSettings;
 class WaveCache;
 class SampleTrackCache;
 class wxFileNameWrapper;
+namespace BasicUI { class ProgressDialog; }
 
 class AUDACITY_DLL_API SpecCache {
 public:
@@ -216,7 +217,7 @@ public:
 
    // Resample clip. This also will set the rate, but without changing
    // the length of the clip
-   void Resample(int rate, ProgressDialog *progress = NULL);
+   void Resample(int rate, BasicUI::ProgressDialog *progress = NULL);
 
    void SetColourIndex( int index ){ mColourIndex = index;};
    int GetColourIndex( ) const { return mColourIndex;};

src/WaveTrack.cpp
@@ -2686,7 +2686,7 @@ void WaveTrack::MergeClips(int clipidx1, int clipidx2)
 
 /*! @excsafety{Weak} -- Partial completion may leave clips at differing sample rates!
 */
-void WaveTrack::Resample(int rate, ProgressDialog *progress)
+void WaveTrack::Resample(int rate, BasicUI::ProgressDialog *progress)
 {
    for (const auto &clip : mClips)
       clip->Resample(rate, progress);

src/WaveTrack.h
@@ -18,10 +18,9 @@
 #include <vector>
 #include <functional>
 #include <wx/longlong.h>
-
 #include "WaveTrackLocation.h"
 
-class ProgressDialog;
+namespace BasicUI{ class ProgressDialog; }
 
 class SampleBlockFactory;
 using SampleBlockFactoryPtr = std::shared_ptr<SampleBlockFactory>;
@@ -490,7 +489,7 @@ private:
    void Merge(const Track &orig) override;
 
    // Resample track (i.e. all clips in the track)
-   void Resample(int rate, ProgressDialog *progress = NULL);
+   void Resample(int rate, BasicUI::ProgressDialog *progress = NULL);
 
    const TypeInfo &GetTypeInfo() const override;
    static const TypeInfo &ClassTypeInfo();

src/effects/Effect.cpp
@@ -1088,13 +1088,14 @@ bool Effect::DoEffect(double projectRate,
    bool skipFlag = CheckWhetherSkipEffect();
    if (skipFlag == false)
    {
+      using namespace BasicUI;
       auto name = GetName();
-      ProgressDialog progress{
+      auto progress = MakeProgress(
          name,
          XO("Applying %s...").Format( name ),
-         pdlgHideStopButton
-      };
-      auto vr = valueRestorer( mProgress, &progress );
+         ProgressShowCancel
+      );
+      auto vr = valueRestorer( mProgress, progress.get() );
 
       {
          returnVal = Process();
@@ -1780,7 +1781,7 @@ void Effect::IncludeNotSelectedPreviewTracks(bool includeNotSelected)
 bool Effect::TotalProgress(double frac, const TranslatableString &msg)
 {
    auto updateResult = (mProgress ?
-      mProgress->Update(frac, msg) :
+      mProgress->Poll(frac * 1000, 1000, msg) :
       ProgressResult::Success);
    return (updateResult != ProgressResult::Success);
 }
@@ -1788,7 +1789,7 @@ bool Effect::TotalProgress(double frac, const TranslatableString &msg)
 bool Effect::TrackProgress(int whichTrack, double frac, const TranslatableString &msg)
 {
    auto updateResult = (mProgress ?
-      mProgress->Update(whichTrack + frac, (double) mNumTracks, msg) :
+      mProgress->Poll(whichTrack + frac, (double) mNumTracks, msg) :
       ProgressResult::Success);
    return (updateResult != ProgressResult::Success);
 }
@@ -1796,7 +1797,7 @@ bool Effect::TrackProgress(int whichTrack, double frac, const TranslatableString
 bool Effect::TrackGroupProgress(int whichGroup, double frac, const TranslatableString &msg)
 {
    auto updateResult = (mProgress ?
-      mProgress->Update(whichGroup + frac, (double) mNumGroups, msg) :
+      mProgress->Poll(whichGroup + frac, (double) mNumGroups, msg) :
       ProgressResult::Success);
    return (updateResult != ProgressResult::Success);
 }
@@ -2176,12 +2177,13 @@ void Effect::Preview(bool dryOnly)
 
    // Apply effect
    if (!dryOnly) {
-      ProgressDialog progress{
+      using namespace BasicUI;
+      auto progress = MakeProgress(
          GetName(),
          XO("Preparing preview"),
-         pdlgHideCancelButton
-      }; // Have only "Stop" button.
-      auto vr = valueRestorer( mProgress, &progress );
+         ProgressShowStop
+      ); // Have only "Stop" button.
+      auto vr = valueRestorer( mProgress, progress.get() );
 
       auto vr2 = valueRestorer( mIsPreview, true );
 

src/effects/Effect.h
@@ -44,10 +44,11 @@ class AudacityCommand;
 
 #define BUILTIN_EFFECT_PREFIX wxT("Built-in Effect: ")
 
+namespace BasicUI { class ProgressDialog; }
+
 class AudacityProject;
 class LabelTrack;
 class NotifyingSelectedRegion;
-class ProgressDialog;
 class SelectedRegion;
 class EffectUIHost;
 class Track;
@@ -411,7 +412,7 @@ protected:
 // may be needed by any particular subclass of Effect.
 //
 protected:
-   ProgressDialog *mProgress; // Temporary pointer, NOT deleted in destructor.
+   BasicUI::ProgressDialog *mProgress = nullptr; // Temporary pointer, NOT deleted in destructor.
    double         mProjectRate; // Sample rate of the project - NEW tracks should
                                // be created with this rate...
    double         mSampleRate;

src/menus/TrackMenus.cpp
@@ -711,14 +711,15 @@ void OnResample(const CommandContext &context)
    {
       auto msg = XO("Resampling track %d").Format( ++ndx );
 
-      ProgressDialog progress(XO("Resample"), msg);
+      using namespace BasicUI;
+      auto progress = MakeProgress(XO("Resample"), msg);
 
       // The resampling of a track may be stopped by the user.  This might
       // leave a track with multiple clips in a partially resampled state.
       // But the thrown exception will cause rollback in the application
       // level handler.
 
-       wt->Resample(newRate, &progress);
+       wt->Resample(newRate, progress.get());
 
       // Each time a track is successfully, completely resampled,
       // commit that to the undo stack.  The second and later times,

tenacityteam/tenacity

(cherry picked from commit 881ee943b44ed202c1ba8a4173dbbda23b6085b3;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/WaveClip.h
@@ -34,6 +34,7 @@ class SpectrogramSettings;
 class WaveCache;
 class SampleTrackCache;
 class wxFileNameWrapper;
+namespace GenericUI { class ProgressDialog; }
 
 class TENACITY_DLL_API SpecCache {
 public:
@@ -210,7 +211,7 @@ public:
 
    // Resample clip. This also will set the rate, but without changing
    // the length of the clip
-   void Resample(int rate, ProgressDialog *progress = NULL);
+   void Resample(int rate, GenericUI::ProgressDialog *progress = NULL);
 
    void SetColourIndex( int index ){ mColourIndex = index;};
    int GetColourIndex( ) const { return mColourIndex;};

src/WaveTrack.cpp
@@ -2688,7 +2688,7 @@ void WaveTrack::MergeClips(int clipidx1, int clipidx2)
 
 /*! @excsafety{Weak} -- Partial completion may leave clips at differing sample rates!
 */
-void WaveTrack::Resample(int rate, ProgressDialog *progress)
+void WaveTrack::Resample(int rate, GenericUI::ProgressDialog *progress)
 {
    for (const auto &clip : mClips)
       clip->Resample(rate, progress);

src/WaveTrack.h
@@ -28,7 +28,7 @@ class wxRect;
 
 #include "WaveTrackLocation.h"
 
-class ProgressDialog;
+namespace GenericUI{ class ProgressDialog; }
 
 class SampleBlockFactory;
 using SampleBlockFactoryPtr = std::shared_ptr<SampleBlockFactory>;
@@ -497,7 +497,7 @@ private:
    void Merge(const Track &orig) override;
 
    // Resample track (i.e. all clips in the track)
-   void Resample(int rate, ProgressDialog *progress = NULL);
+   void Resample(int rate, GenericUI::ProgressDialog *progress = NULL);
 
    const TypeInfo &GetTypeInfo() const override;
    static const TypeInfo &ClassTypeInfo();

src/effects/Effect.cpp
@@ -1107,13 +1107,14 @@ bool Effect::DoEffect(double projectRate,
    bool skipFlag = CheckWhetherSkipEffect();
    if (skipFlag == false)
    {
+      using namespace GenericUI;
       auto name = GetName();
-      ProgressDialog progress{
+      auto progress = MakeProgress(
          name,
          XO("Applying %s...").Format( name ),
-         pdlgHideStopButton
-      };
-      auto vr = valueRestorer( mProgress, &progress );
+         ProgressShowCancel
+      );
+      auto vr = valueRestorer( mProgress, progress.get() );
 
       {
          returnVal = Process();
@@ -1798,7 +1799,7 @@ void Effect::IncludeNotSelectedPreviewTracks(bool includeNotSelected)
 bool Effect::TotalProgress(double frac, const TranslatableString &msg)
 {
    auto updateResult = (mProgress ?
-      mProgress->Update(frac, msg) :
+      mProgress->Poll(frac * 1000, 1000, msg) :
       ProgressResult::Success);
    return (updateResult != ProgressResult::Success);
 }
@@ -1806,7 +1807,7 @@ bool Effect::TotalProgress(double frac, const TranslatableString &msg)
 bool Effect::TrackProgress(int whichTrack, double frac, const TranslatableString &msg)
 {
    auto updateResult = (mProgress ?
-      mProgress->Update(whichTrack + frac, (double) mNumTracks, msg) :
+      mProgress->Poll(whichTrack + frac, (double) mNumTracks, msg) :
       ProgressResult::Success);
    return (updateResult != ProgressResult::Success);
 }
@@ -1814,7 +1815,7 @@ bool Effect::TrackProgress(int whichTrack, double frac, const TranslatableString
 bool Effect::TrackGroupProgress(int whichGroup, double frac, const TranslatableString &msg)
 {
    auto updateResult = (mProgress ?
-      mProgress->Update(whichGroup + frac, (double) mNumGroups, msg) :
+      mProgress->Poll(whichGroup + frac, (double) mNumGroups, msg) :
       ProgressResult::Success);
    return (updateResult != ProgressResult::Success);
 }
@@ -2194,12 +2195,13 @@ void Effect::Preview(bool dryOnly)
 
    // Apply effect
    if (!dryOnly) {
-      ProgressDialog progress{
+      using namespace GenericUI;
+      auto progress = MakeProgress(
          GetName(),
          XO("Preparing preview"),
-         pdlgHideCancelButton
-      }; // Have only "Stop" button.
-      auto vr = valueRestorer( mProgress, &progress );
+         ProgressShowStop
+      ); // Have only "Stop" button.
+      auto vr = valueRestorer( mProgress, progress.get() );
 
       auto vr2 = valueRestorer( mIsPreview, true );
 

src/effects/Effect.h
@@ -45,10 +45,11 @@ class AudacityCommand;
 
 #define BUILTIN_EFFECT_PREFIX wxT("Built-in Effect: ")
 
+namespace GenericUI { class ProgressDialog; }
+
 class TenacityProject;
 class LabelTrack;
 class NotifyingSelectedRegion;
-class ProgressDialog;
 class SelectedRegion;
 class EffectUIHost;
 class Track;
@@ -417,7 +418,7 @@ protected:
 // may be needed by any particular subclass of Effect.
 //
 protected:
-   ProgressDialog *mProgress; // Temporary pointer, NOT deleted in destructor.
+   GenericUI::ProgressDialog *mProgress = nullptr; // Temporary pointer, NOT deleted in destructor.
    double         mProjectRate; // Sample rate of the project - NEW tracks should
                                // be created with this rate...
    double         mSampleRate;

src/menus/TrackMenus.cpp
@@ -655,14 +655,15 @@ void OnResample(const CommandContext &context)
    {
       auto msg = XO("Resampling track %d").Format( ++ndx );
 
-      ProgressDialog progress(XO("Resample"), msg);
+      using namespace GenericUI;
+      auto progress = MakeProgress(XO("Resample"), msg);
 
       // The resampling of a track may be stopped by the user.  This might
       // leave a track with multiple clips in a partially resampled state.
       // But the thrown exception will cause rollback in the application
       // level handler.
 
-       wt->Resample(newRate, &progress);
+       wt->Resample(newRate, progress.get());
 
       // Each time a track is successfully, completely resampled,
       // commit that to the undo stack.  The second and later times,