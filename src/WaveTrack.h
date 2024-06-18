audacity/audacity

Persistency of realtime effect settings...
... using registries to avoid bad dependencies

src/WaveTrack.h
@@ -640,4 +640,9 @@ extern AUDACITY_DLL_API StringSetting AudioTrackNameSetting;
 
 AUDACITY_DLL_API bool GetEditClipsCanMove();
 
+// Generate a registry for serialized data
+#include "XMLMethodRegistry.h"
+using WaveTrackIORegistry = XMLMethodRegistry<WaveTrack>;
+DECLARE_XML_METHOD_REGISTRY( AUDACITY_DLL_API, WaveTrackIORegistry );
+
 #endif // __AUDACITY_WAVETRACK__

src/effects/EffectUI.cpp
@@ -1463,3 +1463,32 @@ void EffectDialog::OnOk(wxCommandEvent & WXUNUSED(evt))
 static struct EffectFactoryInstaller { EffectFactoryInstaller() {
    RealtimeEffectState::InstallFactory(&EffectManager::NewEffect);
 } } installer;
+
+/* The following registration objects need a home at a higher level to avoid
+ dependency either way between WaveTrack or RealtimeEffectList, which need to
+ be in different libraries that do not depend either on the other.
+
+ WaveTrack, like AudacityProject, has a registry for attachment of serializable
+ data.  RealtimeEffectList exposes an interface for serialization.  This is
+ where we connect them.
+ */
+#include "RealtimeEffectList.h"
+static ProjectFileIORegistry::ObjectReaderEntry projectAccessor {
+   RealtimeEffectList::XMLTag(),
+   [](AudacityProject &project) { return &RealtimeEffectList::Get(project); }
+};
+
+static ProjectFileIORegistry::ObjectWriterEntry projectWriter {
+[](const AudacityProject &project, XMLWriter &xmlFile){
+   RealtimeEffectList::Get(project).WriteXML(xmlFile);
+} };
+
+static WaveTrackIORegistry::ObjectReaderEntry waveTrackAccessor {
+   RealtimeEffectList::XMLTag(),
+   [](WaveTrack &track) { return &RealtimeEffectList::Get(track); }
+};
+
+static WaveTrackIORegistry::ObjectWriterEntry waveTrackWriter {
+[](const WaveTrack &track, auto &xmlFile) {
+   RealtimeEffectList::Get(track).WriteXML(xmlFile);
+} };

tenacityteam/tenacity

(cherry picked from commit 0ec99c025ac58be63f9f42883dc2b122f786b0be;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/WaveTrack.h
@@ -642,4 +642,9 @@ extern TENACITY_DLL_API StringSetting AudioTrackNameSetting;
 
 TENACITY_DLL_API bool GetEditClipsCanMove();
 
+// Generate a registry for serialized data
+#include "XMLMethodRegistry.h"
+using WaveTrackIORegistry = XMLMethodRegistry<WaveTrack>;
+DECLARE_XML_METHOD_REGISTRY( TENACITY_DLL_API, WaveTrackIORegistry );
+
 #endif // __AUDACITY_WAVETRACK__

src/effects/EffectUI.cpp
@@ -1449,3 +1449,32 @@ void EffectDialog::OnOk(wxCommandEvent & /* evt */)
 #include "RealtimeEffectState.h"
 static
 RealtimeEffectState::EffectFactory::Scope scope{ &EffectManager::NewEffect };
+
+/* The following registration objects need a home at a higher level to avoid
+ dependency either way between WaveTrack or RealtimeEffectList, which need to
+ be in different libraries that do not depend either on the other.
+
+ WaveTrack, like TenacityProject, has a registry for attachment of serializable
+ data.  RealtimeEffectList exposes an interface for serialization.  This is
+ where we connect them.
+ */
+#include "RealtimeEffectList.h"
+static ProjectFileIORegistry::ObjectReaderEntry projectAccessor {
+   RealtimeEffectList::XMLTag(),
+   [](TenacityProject &project) { return &RealtimeEffectList::Get(project); }
+};
+
+static ProjectFileIORegistry::ObjectWriterEntry projectWriter {
+[](const TenacityProject &project, XMLWriter &xmlFile){
+   RealtimeEffectList::Get(project).WriteXML(xmlFile);
+} };
+
+static WaveTrackIORegistry::ObjectReaderEntry waveTrackAccessor {
+   RealtimeEffectList::XMLTag(),
+   [](WaveTrack &track) { return &RealtimeEffectList::Get(track); }
+};
+
+static WaveTrackIORegistry::ObjectWriterEntry waveTrackWriter {
+[](const WaveTrack &track, auto &xmlFile) {
+   RealtimeEffectList::Get(track).WriteXML(xmlFile);
+} };