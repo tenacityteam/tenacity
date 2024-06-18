audacity/audacity

EffectUIHost::mProject is a reference, not a pointer

src/effects/EffectUI.h
@@ -94,7 +94,7 @@ private:
 private:
    Observer::Subscription mSubscription;
 
-   AudacityProject *mProject;
+   AudacityProject &mProject;
    wxWindow *mParent;
    Effect &mEffect;
    EffectUIClientInterface &mClient;

tenacityteam/tenacity

(cherry picked from commit 97cb44d52e68537476746c7a8809389cc3ab1f7f;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/effects/EffectUI.h
@@ -93,7 +93,7 @@ private:
 private:
    Observer::Subscription mSubscription;
 
-   TenacityProject *mProject;
+   TenacityProject& mProject;
    wxWindow *mParent;
    Effect &mEffect;
    EffectUIClientInterface &mClient;