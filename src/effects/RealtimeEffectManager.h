audacity/audacity

Require InitializationScope to make SuspensionScope

src/effects/RealtimeEffectManager.h
@@ -150,7 +150,9 @@ private:
 class SuspensionScope {
 public:
    SuspensionScope() {}
-   explicit SuspensionScope(std::weak_ptr<AudacityProject> wProject)
+   //! Require a prior InitializationScope to ensure correct nesting
+   explicit SuspensionScope(InitializationScope &,
+      std::weak_ptr<AudacityProject> wProject)
       : mwProject{ move(wProject) }
    {
       if (auto pProject = mwProject.lock()) {

tenacityteam/tenacity

(cherry picked from commit 0d86bc6dccaf1073ad03b921052b1af00b49d173;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/effects/RealtimeEffectManager.h
@@ -156,7 +156,9 @@ private:
 class SuspensionScope {
 public:
    SuspensionScope() {}
-   explicit SuspensionScope(std::weak_ptr<TenacityProject> wProject)
+   //! Require a prior InitializationScope to ensure correct nesting
+   explicit SuspensionScope(InitializationScope &,
+      std::weak_ptr<TenacityProject> wProject)
       : mwProject{ move(wProject) }
    {
       if (auto pProject = mwProject.lock()) {