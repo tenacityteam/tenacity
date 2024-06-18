audacity/audacity

Effect "slave" processors are indexed by track...
... Also, it should not matter whether a track is selected, when applying the
realtime effects.

Also the portAudio callback  mixes the last batch of samples into the output
buffers, before checking completion.

src/AudioIO.cpp
@@ -919,25 +919,21 @@ int AudioIO::StartStream(const TransportTracks &tracks,
    {
       if (auto pOwningProject = mOwningProject.lock()) {
          auto & em = RealtimeEffectManager::Get(*pOwningProject);
+
          // Setup for realtime playback at the rate of the realtime
          // stream, not the rate of the track.
          em.Initialize(mRate);
 
-         // The following adds a NEW effect processor for each logical track and the
-         // group determination should mimic what is done in audacityAudioCallback()
-         // when calling RealtimeProcess().
-         int group = 0;
+         // The following adds a new effect processor for each logical track.
          for (size_t i = 0, cnt = mPlaybackTracks.size(); i < cnt;)
          {
-            const WaveTrack *vt = mPlaybackTracks[i].get();
-
-            // TODO: more-than-two-channels
+            WaveTrack *vt = mPlaybackTracks[i].get();
             unsigned chanCnt = TrackList::Channels(vt).size();
             i += chanCnt;
 
             // Setup for realtime playback at the rate of the realtime
             // stream, not the rate of the track.
-            em.AddTrack(group++, std::min(2u, chanCnt), mRate);
+            em.AddTrack(vt, std::min(mNumPlaybackChannels, chanCnt), mRate);
          }
       }
    }
@@ -2400,8 +2396,6 @@ bool AudioIoCallback::FillOutputBuffers(
    auto pProject = mOwningProject.lock();
    RealtimeEffectManager::ProcessScope scope{ pProject.get() };
 
-   bool selected = false;
-   int group = 0;
    int chanCnt = 0;
 
    // Choose a common size to take from all ring buffers
@@ -2441,7 +2435,6 @@ bool AudioIoCallback::FillOutputBuffers(
 
       if ( firstChannel )
       {
-         selected = vt->GetSelected();
          // IF mono THEN clear 'the other' channel.
          if ( lastChannel && (numPlaybackChannels>1)) {
             // TODO: more-than-two-channels
@@ -2495,38 +2488,45 @@ bool AudioIoCallback::FillOutputBuffers(
       // Last channel of a track seen now
       len = mMaxFramesOutput;
 
-      if( !dropQuickly && selected )
-         len = scope.Process(group, chanCnt, tempBufs, len);
-      group++;
+      // Do realtime effects
+      if( !dropQuickly && len > 0 ) {
+         scope.Process(chans[0], tempBufs, len);
+
+         // Mix the results with the existing output (software playthrough) and
+         // apply panning.  If post panning effects are desired, the panning would
+         // need to be be split out from the mixing and applied in a separate step.
+         for (auto c = 0; c < chanCnt; ++c)
+         {
+            // Our channels aren't silent.  We need to pass their data on.
+            //
+            // Note that there are two kinds of channel count.
+            // c and chanCnt are counting channels in the Tracks.
+            // chan (and numPlayBackChannels) is counting output channels on the device.
+            // chan = 0 is left channel
+            // chan = 1 is right channel.
+            //
+            // Each channel in the tracks can output to more than one channel on the device.
+            // For example mono channels output to both left and right output channels.
+            if (len > 0) for (int c = 0; c < chanCnt; c++)
+            {
+               vt = chans[c];
+
+               if (vt->GetChannelIgnoringPan() == Track::LeftChannel ||
+                     vt->GetChannelIgnoringPan() == Track::MonoChannel )
+                  AddToOutputChannel( 0, outputMeterFloats, outputFloats,
+                     tempBufs[c], drop, len, vt);
+
+               if (vt->GetChannelIgnoringPan() == Track::RightChannel ||
+                     vt->GetChannelIgnoringPan() == Track::MonoChannel  )
+                  AddToOutputChannel( 1, outputMeterFloats, outputFloats,
+                     tempBufs[c], drop, len, vt);
+            }
+         }
+      }
 
       CallbackCheckCompletion(mCallbackReturn, len);
       if (dropQuickly) // no samples to process, they've been discarded
          continue;
-
-      // Our channels aren't silent.  We need to pass their data on.
-      //
-      // Note that there are two kinds of channel count.
-      // c and chanCnt are counting channels in the Tracks.
-      // chan (and numPlayBackChannels) is counting output channels on the device.
-      // chan = 0 is left channel
-      // chan = 1 is right channel.
-      //
-      // Each channel in the tracks can output to more than one channel on the device.
-      // For example mono channels output to both left and right output channels.
-      if (len > 0) for (int c = 0; c < chanCnt; c++)
-      {
-         vt = chans[c];
-
-         if (vt->GetChannelIgnoringPan() == Track::LeftChannel ||
-               vt->GetChannelIgnoringPan() == Track::MonoChannel )
-            AddToOutputChannel( 0, outputMeterFloats, outputFloats,
-               tempBufs[c], drop, len, vt);
-
-         if (vt->GetChannelIgnoringPan() == Track::RightChannel ||
-               vt->GetChannelIgnoringPan() == Track::MonoChannel  )
-            AddToOutputChannel( 1, outputMeterFloats, outputFloats,
-               tempBufs[c], drop, len, vt);
-      }
 
       chanCnt = 0;
    }

src/effects/RealtimeEffectManager.cpp
@@ -16,6 +16,7 @@
 #include "EffectInterface.h"
 #include <memory>
 #include "Project.h"
+#include "Track.h"
 
 #include <atomic>
 #include <wx/time.h>
@@ -65,25 +66,31 @@ void RealtimeEffectManager::Initialize(double rate)
    // (Re)Set processor parameters
    mChans.clear();
    mRates.clear();
+   mGroupLeaders.clear();
 
    // RealtimeAdd/RemoveEffect() needs to know when we're active so it can
    // initialize newly added effects
    mActive = true;
 
-   // Tell each effect to get ready for action
+   // Tell each effect of the master list to get ready for action
    VisitGroup(nullptr, [rate](RealtimeEffectState &state, bool){
       state.Initialize(rate);
    });
 }
 
-void RealtimeEffectManager::AddTrack(int group, unsigned chans, float rate)
+void RealtimeEffectManager::AddTrack(Track *track, unsigned chans, float rate)
 {
-   VisitGroup(nullptr, [&](RealtimeEffectState &state, bool){
-      state.AddTrack(group, chans, rate);
-   });
+   auto leader = *track->GetOwner()->FindLeader(track);
+   mGroupLeaders.push_back(leader);
+   mChans.insert({leader, chans});
+   mRates.insert({leader, rate});
 
-   mChans.push_back(chans);
-   mRates.push_back(rate);
+   VisitGroup(leader,
+      [&](RealtimeEffectState & state, bool) {
+         state.Initialize(rate);
+         state.AddTrack(leader, chans, rate);
+      }
+   );
 }
 
 void RealtimeEffectManager::Finalize()
@@ -94,12 +101,10 @@ void RealtimeEffectManager::Finalize()
    // It is now safe to clean up
    mLatency = std::chrono::microseconds(0);
 
-   // Tell each effect to clean up as well
-   VisitGroup(nullptr, [](RealtimeEffectState &state, bool){
-      state.Finalize();
-   });
+   VisitAll([](auto &state, bool){ state.Finalize(); });
 
    // Reset processor parameters
+   mGroupLeaders.clear();
    mChans.clear();
    mRates.clear();
 
@@ -165,7 +170,9 @@ void RealtimeEffectManager::ProcessStart()
 //
 // This will be called in a different thread than the main GUI thread.
 //
-size_t RealtimeEffectManager::Process(int group, unsigned chans, float **buffers, size_t numSamples)
+size_t RealtimeEffectManager::Process(Track *track,
+                                      float **buffers,
+                                      size_t numSamples)
 {
    // Protect...
    std::lock_guard<std::mutex> guard(mLock);
@@ -175,11 +182,13 @@ size_t RealtimeEffectManager::Process(int group, unsigned chans, float **buffers
    if (mSuspended)
       return numSamples;
 
+   auto chans = mChans[track];
+
    // Remember when we started so we can calculate the amount of latency we
    // are introducing
    auto start = std::chrono::steady_clock::now();
 
-   // Allocate the in/out buffer arrays
+   // Allocate the in and out buffer arrays
    float **ibuf = (float **) alloca(chans * sizeof(float *));
    float **obuf = (float **) alloca(chans * sizeof(float *));
 
@@ -193,33 +202,28 @@ size_t RealtimeEffectManager::Process(int group, unsigned chans, float **buffers
 
    // Now call each effect in the chain while swapping buffer pointers to feed the
    // output of one effect as the input to the next effect
+   // Tracks how many processors were called
    size_t called = 0;
-   VisitGroup(nullptr, [&](RealtimeEffectState &state, bool bypassed){
-      if (!bypassed) {
-         state.Process(group, chans, ibuf, obuf, numSamples);
+   VisitGroup(track,
+      [&](RealtimeEffectState &state, bool bypassed)
+      {
+         if (bypassed)
+            return;
+
+         state.Process(track, chans, ibuf, obuf, numSamples);
+         for (auto i = 0; i < chans; ++i)
+            std::swap(ibuf[i], obuf[i]);
          called++;
       }
-
-      for (unsigned int j = 0; j < chans; j++)
-      {
-         float *temp;
-         temp = ibuf[j];
-         ibuf[j] = obuf[j];
-         obuf[j] = temp;
-      }
-   });
+   );
 
    // Once we're done, we might wind up with the last effect storing its results
    // in the temporary buffers.  If that's the case, we need to copy it over to
    // the caller's buffers.  This happens when the number of effects processed
    // is odd.
    if (called & 1)
-   {
       for (unsigned int i = 0; i < chans; i++)
-      {
          memcpy(buffers[i], ibuf[i], numSamples * sizeof(float));
-      }
-   }
 
    // Remember the latency
    auto end = std::chrono::steady_clock::now();
@@ -250,8 +254,7 @@ void RealtimeEffectManager::ProcessEnd() noexcept
    }
 }
 
-void RealtimeEffectManager::VisitGroup(Track *leader,
-   std::function<void(RealtimeEffectState &state, bool bypassed)> func)
+void RealtimeEffectManager::VisitGroup(Track *leader, StateVisitor func)
 {
    // Call the function for each effect on the master list
    RealtimeEffectList::Get(mProject).Visit(func);
@@ -259,6 +262,16 @@ void RealtimeEffectManager::VisitGroup(Track *leader,
    // Call the function for each effect on the track list
    if (leader)
      RealtimeEffectList::Get(*leader).Visit(func);
+}
+
+void RealtimeEffectManager::VisitAll(StateVisitor func)
+{
+   // Call the function for each effect on the master list
+   RealtimeEffectList::Get(mProject).Visit(func);
+
+   // And all track lists
+   for (auto leader : mGroupLeaders)
+      RealtimeEffectList::Get(*leader).Visit(func);
 }
 
 auto RealtimeEffectManager::GetLatency() const -> Latency

tenacityteam/tenacity

(cherry picked from commit 8387eac9ed3c4c7a14e8d95e65c3b4c4128cd6a5;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/AudioIO.cpp
@@ -694,25 +694,21 @@ int AudioIO::StartStream(const TransportTracks &tracks,
    {
       if (auto pOwningProject = mOwningProject.lock()) {
          auto & em = RealtimeEffectManager::Get(*pOwningProject);
+
          // Setup for realtime playback at the rate of the realtime
          // stream, not the rate of the track.
          em.Initialize(mRate);
 
-         // The following adds a NEW effect processor for each logical track and the
-         // group determination should mimic what is done in audacityAudioCallback()
-         // when calling RealtimeProcess().
-         int group = 0;
+         // The following adds a new effect processor for each logical track.
          for (size_t i = 0, cnt = mPlaybackTracks.size(); i < cnt;)
          {
-            const WaveTrack *vt = mPlaybackTracks[i].get();
-
-            // TODO: more-than-two-channels
+            auto vt = mPlaybackTracks[i].get();
             unsigned chanCnt = TrackList::Channels(vt).size();
             i += chanCnt;
 
             // Setup for realtime playback at the rate of the realtime
             // stream, not the rate of the track.
-            em.AddTrack(group++, std::min(2u, chanCnt), mRate);
+            em.AddTrack(vt, std::min(mNumPlaybackChannels, chanCnt), mRate);
          }
       }
    }
@@ -2052,38 +2048,45 @@ bool AudioIoCallback::FillOutputBuffers(
          // Last channel of a track seen now
          len = mMaxFramesOutput;
 
-         if( !dropQuickly && selected )
-            len = scope.Process(group, chanCnt, mScratchBuffers.data(), len);
-         group++;
+         // Do realtime effects
+         if( !dropQuickly && len > 0 ) {
+            scope.Process(mTrackChannelsBuffer[0], mScratchBuffers.data(), len);
+
+            // Mix the results with the existing output (software playthrough) and
+            // apply panning.  If post panning effects are desired, the panning would
+            // need to be be split out from the mixing and applied in a separate step.
+            for (auto c = 0; c < chanCnt; ++c)
+            {
+               // Our channels aren't silent.  We need to pass their data on.
+               //
+               // Note that there are two kinds of channel count.
+               // c and chanCnt are counting channels in the Tracks.
+               // chan (and numPlayBackChannels) is counting output channels on the device.
+               // chan = 0 is left channel
+               // chan = 1 is right channel.
+               //
+               // Each channel in the tracks can output to more than one channel on the device.
+               // For example mono channels output to both left and right output channels.
+               if (len > 0) for (int c = 0; c < chanCnt; c++)
+               {
+                  vt = mTrackChannelsBuffer[c];
+
+                  if (vt->GetChannelIgnoringPan() == Track::LeftChannel ||
+                        vt->GetChannelIgnoringPan() == Track::MonoChannel )
+                     AddToOutputChannel( 0, outputMeterFloats, outputFloats,
+                        mScratchBuffers[c], drop, len, *vt);
+
+                  if (vt->GetChannelIgnoringPan() == Track::RightChannel ||
+                        vt->GetChannelIgnoringPan() == Track::MonoChannel  )
+                     AddToOutputChannel( 1, outputMeterFloats, outputFloats,
+                        mScratchBuffers[c], drop, len, *vt);
+               }
+            }
+         }
 
          CallbackCheckCompletion(mCallbackReturn, len);
          if (dropQuickly) // no samples to process, they've been discarded
             continue;
-
-         // Our channels aren't silent.  We need to pass their data on.
-         //
-         // Note that there are two kinds of channel count.
-         // c and chanCnt are counting channels in the Tracks.
-         // chan (and numPlayBackChannels) is counting output channels on the device.
-         // chan = 0 is left channel
-         // chan = 1 is right channel.
-         //
-         // Each channel in the tracks can output to more than one channel on the device.
-         // For example mono channels output to both left and right output channels.
-         if (len > 0) for (int c = 0; c < chanCnt; c++)
-         {
-            vt = mTrackChannelsBuffer[c];
-
-            if (vt->GetChannelIgnoringPan() == Track::LeftChannel ||
-                  vt->GetChannelIgnoringPan() == Track::MonoChannel )
-               AddToOutputChannel( 0, outputMeterFloats, outputFloats,
-                  mScratchBuffers[c], drop, len, *vt);
-
-            if (vt->GetChannelIgnoringPan() == Track::RightChannel ||
-                  vt->GetChannelIgnoringPan() == Track::MonoChannel  )
-               AddToOutputChannel( 1, outputMeterFloats, outputFloats,
-                  mScratchBuffers[c], drop, len, *vt);
-         }
 
          chanCnt = 0;
       }

src/effects/RealtimeEffectManager.cpp
@@ -16,6 +16,7 @@
 // Tenacity libraries
 #include <lib-components/EffectInterface.h>
 #include <lib-project/Project.h>
+#include <lib-track/Track.h>
 #include <lib-utility/MemoryX.h>
 
 #include <atomic>
@@ -68,25 +69,30 @@ void RealtimeEffectManager::Initialize(double rate)
    // (Re)Set processor parameters
    mChans.clear();
    mRates.clear();
+   mGroupLeaders.clear();
 
    // RealtimeAdd/RemoveEffect() needs to know when we're active so it can
    // initialize newly added effects
    mActive = true;
 
-   // Tell each effect to get ready for action
+   // Tell each effect of the master list to get ready for action
    VisitGroup(nullptr, [rate](RealtimeEffectState &state, bool){
       state.Initialize(rate);
    });
 }
 
-void RealtimeEffectManager::AddTrack(int group, unsigned chans, float rate)
+void RealtimeEffectManager::AddTrack(Track *track, unsigned chans, float rate)
 {
-   VisitGroup(nullptr, [&](RealtimeEffectState &state, bool){
-      state.AddTrack(group, chans, rate);
-   });
+   auto leader = *track->GetOwner()->FindLeader(track);
+   mGroupLeaders.push_back(leader);
+   mChans.insert({leader, chans});
+   mRates.insert({leader, rate});
 
-   mChans.push_back(chans);
-   mRates.push_back(rate);
+   VisitGroup(leader,
+      [&](RealtimeEffectState & state, bool) {
+         state.AddTrack(leader, chans, rate);
+      }
+   );
 }
 
 void RealtimeEffectManager::Finalize()
@@ -94,12 +100,13 @@ void RealtimeEffectManager::Finalize()
    // Make sure nothing is going on
    Suspend();
 
-   // Tell each effect to clean up as well
-   VisitGroup(nullptr, [](RealtimeEffectState &state, bool){
-      state.Finalize();
-   });
+   // It is now safe to clean up
+   mLatency = std::chrono::microseconds(0);
+
+   VisitAll([](auto &state, bool){ state.Finalize(); });
 
    // Reset processor parameters
+   mGroupLeaders.clear();
    mChans.clear();
    mRates.clear();
 
@@ -167,7 +174,9 @@ void RealtimeEffectManager::ProcessStart()
 //
 // This will be called in a different thread than the main GUI thread.
 //
-size_t RealtimeEffectManager::Process(int group, unsigned chans, float **buffers, size_t numSamples)
+size_t RealtimeEffectManager::Process(Track *track,
+                                      float **buffers,
+                                      size_t numSamples)
 {
    using namespace std::chrono;
 
@@ -179,6 +188,8 @@ size_t RealtimeEffectManager::Process(int group, unsigned chans, float **buffers
    if (mSuspended)
       return numSamples;
 
+   auto chans = mChans[track];
+
    // AK: If we have more channels than our input and output bufffers'
    // capacities, increase their capacities when necessary.
    if (mInputBuffers.capacity() < chans)
@@ -188,6 +199,7 @@ size_t RealtimeEffectManager::Process(int group, unsigned chans, float **buffers
    {
       mOutputBuffers.reserve(chans);
    }
+
 
    // Remember when we started so we can calculate the amount of latency we
    // are introducing
@@ -207,19 +219,21 @@ size_t RealtimeEffectManager::Process(int group, unsigned chans, float **buffers
 
    // Now call each effect in the chain while swapping buffer pointers to feed the
    // output of one effect as the input to the next effect
+   // Tracks how many processors were called
    size_t called = 0;
-   VisitGroup(nullptr, [&](RealtimeEffectState &state, bool bypassed){
-      if (!bypassed) {
-         state.Process(group, chans, mInputBuffers.data(),
+   VisitGroup(track,
+      [&](RealtimeEffectState &state, bool bypassed)
+      {
+         if (bypassed)
+            return;
+
+         state.Process(track, chans, mInputBuffers.data(),
                        mOutputBuffers.data(), numSamples);
+         for (auto i = 0; i < chans; ++i)
+            std::swap(mInputBuffers[i], mOutputBuffers[i]);
          called++;
       }
-
-      for (unsigned int j = 0; j < chans; j++)
-      {
-         std::swap(mInputBuffers[j], mOutputBuffers[j]);
-      }
-   });
+   );
 
    // Once we're done, we might wind up with the last effect storing its results
    // in the temporary buffers.  If that's the case, we need to copy it over to
@@ -261,8 +275,7 @@ void RealtimeEffectManager::ProcessEnd() noexcept
    }
 }
 
-void RealtimeEffectManager::VisitGroup(Track *leader,
-   std::function<void(RealtimeEffectState &state, bool bypassed)> func)
+void RealtimeEffectManager::VisitGroup(Track *leader, StateVisitor func)
 {
    // Call the function for each effect on the master list
    RealtimeEffectList::Get(mProject).Visit(func);
@@ -270,6 +283,16 @@ void RealtimeEffectManager::VisitGroup(Track *leader,
    // Call the function for each effect on the track list
    if (leader)
      RealtimeEffectList::Get(*leader).Visit(func);
+}
+
+void RealtimeEffectManager::VisitAll(StateVisitor func)
+{
+   // Call the function for each effect on the master list
+   RealtimeEffectList::Get(mProject).Visit(func);
+
+   // And all track lists
+   for (auto leader : mGroupLeaders)
+      RealtimeEffectList::Get(*leader).Visit(func);
 }
 
 auto RealtimeEffectManager::GetLatency() const -> Latency