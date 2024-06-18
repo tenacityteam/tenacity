audacity/audacity

Require InitializationScope before there is ProcessingScope...
... Just by requiring a reference to initialization scope (unused) when
constructing processing scope; but where we use it, do proper null checks before
a dereference.

src/effects/RealtimeEffectManager.h
@@ -161,7 +161,9 @@ private:
 class ProcessingScope {
 public:
    ProcessingScope() {}
-   explicit ProcessingScope(std::weak_ptr<AudacityProject> wProject)
+   //! Require a prior InializationScope to ensure correct nesting
+   explicit ProcessingScope(InitializationScope &,
+      std::weak_ptr<AudacityProject> wProject)
       : mwProject{ move(wProject) }
    {
       if (auto pProject = mwProject.lock())

tenacityteam/tenacity

(cherry picked from commit fa86bfdb1d9a63658d78b5236f4c6832f4e1be1f;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/effects/RealtimeEffectManager.h
@@ -167,7 +167,9 @@ private:
 class ProcessingScope {
 public:
    ProcessingScope() {}
-   explicit ProcessingScope(std::weak_ptr<TenacityProject> wProject)
+   //! Require a prior InializationScope to ensure correct nesting
+   explicit ProcessingScope(InitializationScope &,
+      std::weak_ptr<TenacityProject> wProject)
       : mwProject{ move(wProject) }
    {
       if (auto pProject = mwProject.lock())