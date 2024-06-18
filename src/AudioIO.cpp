audacity/audacity

Require a scope object to initialize and finalize realtime...
... Moving some steps into TransportState.  Others might be moved later.

src/AudioIO.cpp
@@ -143,6 +143,27 @@ AudioIO *AudioIO::Get()
 }
 
 struct AudioIoCallback::TransportState {
+   TransportState(std::weak_ptr<AudacityProject> wOwningProject,
+      const WaveTrackArray &playbackTracks,
+      unsigned numPlaybackChannels, double rate)
+   {
+      if (auto pOwningProject = wOwningProject.lock();
+          pOwningProject && numPlaybackChannels > 0) {
+         // Setup for realtime playback at the rate of the realtime
+         // stream, not the rate of the track.
+         mpRealtimeInitialization.emplace(move(wOwningProject), rate);
+         // The following adds a new effect processor for each logical track.
+         for (size_t i = 0, cnt = playbackTracks.size(); i < cnt;) {
+            auto vt = playbackTracks[i].get();
+            unsigned chanCnt = TrackList::Channels(vt).size();
+            i += chanCnt; // Visit leaders only
+            mpRealtimeInitialization->AddTrack(
+               vt, std::min(numPlaybackChannels, chanCnt), rate);
+         }
+      }
+   }
+
+   std::optional<RealtimeEffects::InitializationScope> mpRealtimeInitialization;
 };
 
 // static
@@ -918,30 +939,8 @@ int AudioIO::StartStream(const TransportTracks &tracks,
          return 0;
    }
 
-   if (mNumPlaybackChannels > 0)
-   {
-      if (auto pOwningProject = mOwningProject.lock()) {
-         auto & em = RealtimeEffectManager::Get(*pOwningProject);
-
-         // Setup for realtime playback at the rate of the realtime
-         // stream, not the rate of the track.
-         em.Initialize(mRate);
-
-         // The following adds a new effect processor for each logical track.
-         for (size_t i = 0, cnt = mPlaybackTracks.size(); i < cnt;)
-         {
-            auto vt = mPlaybackTracks[i].get();
-            unsigned chanCnt = TrackList::Channels(vt).size();
-            i += chanCnt;
-
-            // Setup for realtime playback at the rate of the realtime
-            // stream, not the rate of the track.
-            em.AddTrack(vt, std::min(mNumPlaybackChannels, chanCnt), mRate);
-         }
-      }
-   }
-
-   mpTransportState = std::make_unique<TransportState>();
+   mpTransportState = std::make_unique<TransportState>( mOwningProject,
+      mPlaybackTracks, mNumPlaybackChannels, mRate);
 
 #ifdef EXPERIMENTAL_AUTOMATED_INPUT_LEVEL_ADJUSTMENT
    AILASetStartTime();
@@ -1270,12 +1269,6 @@ bool AudioIO::AllocateBuffers(
 
 void AudioIO::StartStreamCleanup(bool bOnlyBuffers)
 {
-   if (mNumPlaybackChannels > 0)
-   {
-      if (auto pOwningProject = mOwningProject.lock())
-         RealtimeEffectManager::Get(*pOwningProject).Finalize();
-   }
-
    mpTransportState.reset();
 
    mPlaybackBuffers.reset();
@@ -1424,12 +1417,6 @@ void AudioIO::StopStream()
 
    // No longer need effects processing. This must be done after the stream is stopped
    // to prevent the callback from being invoked after the effects are finalized.
-   if (mNumPlaybackChannels > 0)
-   {
-      if (auto pOwningProject = mOwningProject.lock())
-         RealtimeEffectManager::Get(*pOwningProject).Finalize();
-   }
-
    mpTransportState.reset();
 
    for( auto &ext : Extensions() )

src/effects/RealtimeEffectManager.h
@@ -28,6 +28,7 @@ class RealtimeEffectState;
 class Track;
 
 namespace RealtimeEffects {
+   class InitializationScope;
    class SuspensionScope;
    class ProcessingScope;
 }
@@ -47,12 +48,6 @@ public:
 
    // Realtime effect processing
    bool IsActive() const noexcept;
-   //! Main thread begins to define a set of tracks for playback
-   void Initialize(double rate);
-   //! Main thread adds one track (passing the first of one or more channels)
-   void AddTrack(Track *track, unsigned chans, float rate);
-   //! Main thread cleans up after playback
-   void Finalize() noexcept;
    void Suspend();
    void Resume() noexcept;
    Latency GetLatency() const;
@@ -67,6 +62,14 @@ public:
    void RemoveState(RealtimeEffectList &states, RealtimeEffectState &state);
 
 private:
+
+   friend RealtimeEffects::InitializationScope;
+   //! Main thread begins to define a set of tracks for playback
+   void Initialize(double rate);
+   //! Main thread adds one track (passing the first of one or more channels)
+   void AddTrack(Track *track, unsigned chans, float rate);
+   //! Main thread cleans up after playback
+   void Finalize() noexcept;
 
    friend RealtimeEffects::ProcessingScope;
    void ProcessStart();
@@ -102,6 +105,35 @@ private:
 };
 
 namespace RealtimeEffects {
+//! Brackets processing setup and cleanup in the main thread
+class InitializationScope {
+public:
+   InitializationScope() {}
+   explicit InitializationScope(
+      std::weak_ptr<AudacityProject> wProject, double rate)
+      : mwProject{ move(wProject) }
+   {
+      if (auto pProject = mwProject.lock())
+         RealtimeEffectManager::Get(*pProject).Initialize(rate);
+   }
+   InitializationScope( InitializationScope &&other ) = default;
+   InitializationScope& operator=( InitializationScope &&other ) = default;
+   ~InitializationScope()
+   {
+      if (auto pProject = mwProject.lock())
+         RealtimeEffectManager::Get(*pProject).Finalize();
+   }
+
+   void AddTrack(Track *track, unsigned chans, float rate)
+   {
+      if (auto pProject = mwProject.lock())
+         RealtimeEffectManager::Get(*pProject).AddTrack(track, chans, rate);
+   }
+
+private:
+   std::weak_ptr<AudacityProject> mwProject;
+};
+
 //! Brackets one suspension of processing in the main thread
 class SuspensionScope {
 public:

tenacityteam/tenacity

(cherry picked from commit 0abf4fe3413d445e7b5a2d8398ee455f7eba1819;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

src/AudioIO.cpp
@@ -137,6 +137,27 @@ AudioIO *AudioIO::Get()
 }
 
 struct AudioIoCallback::TransportState {
+   TransportState(std::weak_ptr<TenacityProject> wOwningProject,
+      const WaveTrackArray &playbackTracks,
+      unsigned numPlaybackChannels, double rate)
+   {
+      if (auto pOwningProject = wOwningProject.lock();
+          pOwningProject && numPlaybackChannels > 0) {
+         // Setup for realtime playback at the rate of the realtime
+         // stream, not the rate of the track.
+         mpRealtimeInitialization.emplace(move(wOwningProject), rate);
+         // The following adds a new effect processor for each logical track.
+         for (size_t i = 0, cnt = playbackTracks.size(); i < cnt;) {
+            auto vt = playbackTracks[i].get();
+            unsigned chanCnt = TrackList::Channels(vt).size();
+            i += chanCnt; // Visit leaders only
+            mpRealtimeInitialization->AddTrack(
+               vt, std::min(numPlaybackChannels, chanCnt), rate);
+         }
+      }
+   }
+
+   std::optional<RealtimeEffects::InitializationScope> mpRealtimeInitialization;
 };
 
 // static
@@ -693,30 +714,8 @@ int AudioIO::StartStream(const TransportTracks &tracks,
    if ( ! AllocateBuffers( options, tracks, t0, t1, options.rate ) )
       return 0;
 
-   if (mNumPlaybackChannels > 0)
-   {
-      if (auto pOwningProject = mOwningProject.lock()) {
-         auto & em = RealtimeEffectManager::Get(*pOwningProject);
-
-         // Setup for realtime playback at the rate of the realtime
-         // stream, not the rate of the track.
-         em.Initialize(mRate);
-
-         // The following adds a new effect processor for each logical track.
-         for (size_t i = 0, cnt = mPlaybackTracks.size(); i < cnt;)
-         {
-            auto vt = mPlaybackTracks[i].get();
-            unsigned chanCnt = TrackList::Channels(vt).size();
-            i += chanCnt;
-
-            // Setup for realtime playback at the rate of the realtime
-            // stream, not the rate of the track.
-            em.AddTrack(vt, std::min(mNumPlaybackChannels, chanCnt), mRate);
-         }
-      }
-   }
-
-   mpTransportState = std::make_unique<TransportState>();
+   mpTransportState = std::make_unique<TransportState>( mOwningProject,
+      mPlaybackTracks, mNumPlaybackChannels, mRate);
 
    if (options.pStartTime)
    {
@@ -1040,12 +1039,6 @@ bool AudioIO::AllocateBuffers(
 
 void AudioIO::StartStreamCleanup(bool bOnlyBuffers)
 {
-   if (mNumPlaybackChannels > 0)
-   {
-      if (auto pOwningProject = mOwningProject.lock())
-         RealtimeEffectManager::Get(*pOwningProject).Finalize();
-   }
-
    mpTransportState.reset();
 
    mPlaybackBuffers.reset();
@@ -1163,12 +1156,6 @@ void AudioIO::StopStream()
 
    // No longer need effects processing. This must be done after the stream is stopped
    // to prevent the callback from being invoked after the effects are finalized.
-   if (mNumPlaybackChannels > 0)
-   {
-      if (auto pOwningProject = mOwningProject.lock())
-         RealtimeEffectManager::Get(*pOwningProject).Finalize();
-   }
-
    mpTransportState.reset();
 
    for( auto &ext : Extensions() )

src/effects/RealtimeEffectManager.h
@@ -29,6 +29,7 @@ class RealtimeEffectState;
 class Track;
 
 namespace RealtimeEffects {
+   class InitializationScope;
    class SuspensionScope;
    class ProcessingScope;
 }
@@ -48,12 +49,6 @@ public:
 
    // Realtime effect processing
    bool IsActive() const noexcept;
-   //! Main thread begins to define a set of tracks for playback
-   void Initialize(double rate);
-   //! Main thread adds one track (passing the first of one or more channels)
-   void AddTrack(Track *track, unsigned chans, float rate);
-   //! Main thread cleans up after playback
-   void Finalize() noexcept;
    void Suspend();
    void Resume() noexcept;
    Latency GetLatency() const;
@@ -68,6 +63,14 @@ public:
    void RemoveState(RealtimeEffectList &states, RealtimeEffectState &state);
 
 private:
+
+   friend RealtimeEffects::InitializationScope;
+   //! Main thread begins to define a set of tracks for playback
+   void Initialize(double rate);
+   //! Main thread adds one track (passing the first of one or more channels)
+   void AddTrack(Track *track, unsigned chans, float rate);
+   //! Main thread cleans up after playback
+   void Finalize() noexcept;
 
    friend RealtimeEffects::ProcessingScope;
    void ProcessStart();
@@ -108,6 +111,35 @@ private:
 };
 
 namespace RealtimeEffects {
+//! Brackets processing setup and cleanup in the main thread
+class InitializationScope {
+public:
+   InitializationScope() {}
+   explicit InitializationScope(
+      std::weak_ptr<TenacityProject> wProject, double rate)
+      : mwProject{ move(wProject) }
+   {
+      if (auto pProject = mwProject.lock())
+         RealtimeEffectManager::Get(*pProject).Initialize(rate);
+   }
+   InitializationScope( InitializationScope &&other ) = default;
+   InitializationScope& operator=( InitializationScope &&other ) = default;
+   ~InitializationScope()
+   {
+      if (auto pProject = mwProject.lock())
+         RealtimeEffectManager::Get(*pProject).Finalize();
+   }
+
+   void AddTrack(Track *track, unsigned chans, float rate)
+   {
+      if (auto pProject = mwProject.lock())
+         RealtimeEffectManager::Get(*pProject).AddTrack(track, chans, rate);
+   }
+
+private:
+   std::weak_ptr<TenacityProject> mwProject;
+};
+
 //! Brackets one suspension of processing in the main thread
 class SuspensionScope {
 public: