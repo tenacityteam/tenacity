audacity/audacity

Fix GlobalHook implementation for Windows ...
... Now it works just like subclasses of ClientData::Site :

Use GlobalHook by deriving a struct from it, and give the correct visibility
to the struct, which will guarantee generation of unique non-inline functions in
one library (or the executable), with the needed static local variables.

The first template parameter is thus now another "CRTP" parameter.

libraries/lib-transactions/TransactionScope.h
@@ -28,8 +28,9 @@ class TRANSACTIONS_API TransactionScope
 {
 public:
    //! Type of function supplying implementation of steps
-   using Factory = GlobalHook< TransactionScope,
-      std::unique_ptr<TransactionScopeImpl>(AudacityProject &) >;
+   struct TRANSACTIONS_API Factory : GlobalHook<Factory,
+      std::unique_ptr<TransactionScopeImpl>(AudacityProject &)
+   > {};
 
    //! Construct from a project
    /*!

src/KeyboardCapture.h
@@ -40,7 +40,9 @@ namespace KeyboardCapture
 
    //! Pre-filter is called before passing the event to the captured window
    /*! If it returns false, then skip the event entirely */
-   using PreFilter = GlobalHook<struct PreFilterTag, bool( wxKeyEvent& )>;
+   struct AUDACITY_DLL_API PreFilter : GlobalHook<PreFilter,
+      bool( wxKeyEvent& )
+   >{};
    
    //! Post-filter is conditionally called after passing the event to the window
    /*!
@@ -50,7 +52,9 @@ namespace KeyboardCapture
        wxKEY_DOWN or a wxKEY_UP event; if it returns false, then the event is
        skipped
     */
-   using PostFilter = GlobalHook<struct PostFilterTag, bool( wxKeyEvent& )>;
+   struct AUDACITY_DLL_API PostFilter : GlobalHook<PostFilter,
+      bool( wxKeyEvent& )
+   >{};
 
    /// \brief a function useful to implement a focus event handler
    /// The window releases the keyboard if the event is for killing focus,

src/SampleBlock.h
@@ -105,8 +105,9 @@ class SampleBlockFactory
 {
 public:
    //! Global factory of per-project factories of sample blocks
-   using Factory = GlobalHook<SampleBlockFactory,
-      SampleBlockFactoryPtr( AudacityProject& )>;
+   struct AUDACITY_DLL_API Factory : GlobalHook<Factory,
+      SampleBlockFactoryPtr( AudacityProject& )
+   >{};
 
    // Invoke the installed factory (throw an exception if none was installed)
    static SampleBlockFactoryPtr New( AudacityProject &project );

src/commands/CommandManager.h
@@ -65,7 +65,9 @@ class AUDACITY_DLL_API CommandManager final
 
    // Interception of menu item handling.
    // If it returns true, bypass the usual dispatch of commands.
-   using GlobalMenuHook = GlobalHook<CommandManager, bool(const CommandID&)>;
+   struct AUDACITY_DLL_API GlobalMenuHook : GlobalHook<GlobalMenuHook,
+      bool(const CommandID&)
+   >{};
 
    //
    // Constructor / Destructor

src/toolbars/ToolManager.h
@@ -56,7 +56,9 @@ class AUDACITY_DLL_API ToolManager final
 
  public:
    // a hook function to break dependency of ToolManager on ProjectWindow
-   using TopPanelHook = GlobalHook< ToolManager, wxWindow*( wxWindow& ) >;
+   struct AUDACITY_DLL_API TopPanelHook : GlobalHook<TopPanelHook,
+      wxWindow*( wxWindow& )
+   >{};
 
    static ToolManager &Get( AudacityProject &project );
    static const ToolManager &Get( const AudacityProject &project );

src/tracks/ui/CommonTrackPanelCell.h
@@ -31,9 +31,9 @@ class AUDACITY_DLL_API CommonTrackPanelCell /* not final */
 {
 public:
    // Function to dispatch mouse wheel events
-   using MouseWheelHook = GlobalHook< CommonTrackPanelCell,
+   struct AUDACITY_DLL_API MouseWheelHook : GlobalHook<MouseWheelHook,
       unsigned(const TrackPanelMouseEvent &evt, AudacityProject *pProject)
-   >;
+   >{};
 
    CommonTrackPanelCell()
    {}

tenacityteam/tenacity

(cherry picked from commit 2be68398b8e534d359b550408cd66fc09f28bc9b;
modified for Tenacity)
Signed-off-by: Avery King <avery98@pm.me>

libraries/lib-transactions/TransactionScope.h
@@ -29,8 +29,9 @@ class TRANSACTIONS_API TransactionScope
 {
 public:
    //! Type of function supplying implementation of steps
-   using Factory = GlobalHook< TransactionScope,
-      std::unique_ptr<TransactionScopeImpl>(TenacityProject &) >;
+   struct TRANSACTIONS_API Factory : GlobalHook<Factory,
+      std::unique_ptr<TransactionScopeImpl>(TenacityProject &)
+   > {};
 
    //! Construct from a project
    /*!

src/KeyboardCapture.h
@@ -39,7 +39,9 @@ namespace KeyboardCapture
 
    //! Pre-filter is called before passing the event to the captured window
    /*! If it returns false, then skip the event entirely */
-   using PreFilter = GlobalHook<struct PreFilterTag, bool( wxKeyEvent& )>;
+   struct TENACITY_DLL_API PreFilter : GlobalHook<PreFilter,
+      bool( wxKeyEvent& )
+   >{};
    
    //! Post-filter is conditionally called after passing the event to the window
    /*!
@@ -49,7 +51,9 @@ namespace KeyboardCapture
        wxKEY_DOWN or a wxKEY_UP event; if it returns false, then the event is
        skipped
     */
-   using PostFilter = GlobalHook<struct PostFilterTag, bool( wxKeyEvent& )>;
+   struct TENACITY_DLL_API PostFilter : GlobalHook<PostFilter,
+      bool( wxKeyEvent& )
+   >{};
 
    /// \brief a function useful to implement a focus event handler
    /// The window releases the keyboard if the event is for killing focus,

src/SampleBlock.h
@@ -106,8 +106,9 @@ class SampleBlockFactory
 {
 public:
    //! Global factory of per-project factories of sample blocks
-   using Factory = GlobalHook<SampleBlockFactory,
-      SampleBlockFactoryPtr( TenacityProject& )>;
+   struct TENACITY_DLL_API Factory : GlobalHook<Factory,
+      SampleBlockFactoryPtr( TenacityProject& )
+   >{};
 
    // Invoke the installed factory (throw an exception if none was installed)
    static SampleBlockFactoryPtr New( TenacityProject &project );

src/commands/CommandManager.h
@@ -64,7 +64,9 @@ class TENACITY_DLL_API CommandManager final
 
    // Interception of menu item handling.
    // If it returns true, bypass the usual dispatch of commands.
-   using GlobalMenuHook = GlobalHook<CommandManager, bool(const CommandID&)>;
+   struct TENACITY_DLL_API GlobalMenuHook : GlobalHook<GlobalMenuHook,
+      bool(const CommandID&)
+   >{};
 
    //
    // Constructor / Destructor

src/toolbars/ToolManager.h
@@ -58,7 +58,9 @@ class TENACITY_DLL_API ToolManager final
 
  public:
    // a hook function to break dependency of ToolManager on ProjectWindow
-   using TopPanelHook = GlobalHook< ToolManager, wxWindow*( wxWindow& ) >;
+   struct TENACITY_DLL_API TopPanelHook : GlobalHook<TopPanelHook,
+      wxWindow*( wxWindow& )
+   >{};
 
    static ToolManager &Get( TenacityProject &project );
    static const ToolManager &Get( const TenacityProject &project );

src/tracks/ui/CommonTrackPanelCell.h
@@ -31,9 +31,9 @@ class TENACITY_DLL_API CommonTrackPanelCell /* not final */
 {
 public:
    // Function to dispatch mouse wheel events
-   using MouseWheelHook = GlobalHook< CommonTrackPanelCell,
+   struct TENACITY_DLL_API MouseWheelHook : GlobalHook<MouseWheelHook,
       unsigned(const TrackPanelMouseEvent &evt, TenacityProject *pProject)
-   >;
+   >{};
 
    CommonTrackPanelCell()
    {}