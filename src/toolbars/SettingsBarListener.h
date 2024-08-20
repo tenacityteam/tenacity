5dc2d61484e6de58580a05703054dfd20f94b622
Avery King <avery98@pm.me>
2024-08-19 19:46:25 -0700

    Duplicate SelectionBar in SettingsBar.{h,cpp}
    Also duplicate SelectionBarListener in SettingsBarListener.h
    
    Signed-off-by: Avery King <avery98@pm.me>

---
+++ src/toolbars/SettingsBarListener.h
@@ -0,0 +1,46 @@
+/**********************************************************************
+
+  Audacity: A Digital Audio Editor
+
+  SelectionBarListener.h
+
+  Dominic Mazzoni
+
+**********************************************************************/
+
+#ifndef __AUDACITY_SELECTION_BAR_LISTENER__
+#define __AUDACITY_SELECTION_BAR_LISTENER__
+
+// Tenacity libraries
+#include <lib-components/ComponentInterfaceSymbol.h>
+
+class SelectedRegion;
+
+class TENACITY_DLL_API SelectionBarListener /* not final */ {
+
+ public:
+
+   SelectionBarListener(){};
+   virtual ~SelectionBarListener(){};
+
+   virtual double AS_GetRate() = 0;
+   virtual void AS_SetRate(double rate) = 0;
+   virtual int AS_GetSnapTo() = 0;
+   virtual void AS_SetSnapTo(int snap) = 0;
+   virtual const NumericFormatSymbol & AS_GetSelectionFormat() = 0;
+   virtual void AS_SetSelectionFormat(const NumericFormatSymbol & format) = 0;
+   virtual void AS_ModifySelection(double &start, double &end, bool done) = 0;
+};
+
+class TENACITY_DLL_API TimeToolBarListener /* not final */ {
+
+ public:
+
+   TimeToolBarListener(){};
+   virtual ~TimeToolBarListener(){};
+
+   virtual const NumericFormatSymbol & TT_GetAudioTimeFormat() = 0;
+   virtual void TT_SetAudioTimeFormat(const NumericFormatSymbol & format) = 0;
+};
+
+#endif
