alex74747/audacity

EffectUI no longer directly depends on Effect...
... after removing one special-case mention of Nyquist:  see code comments.

Only Nyquist effects overrode HelpPage() as possibly non-null.

But there is still a transitive dependency through EffectManager.

src/effects/EffectUI.cpp
@@ -15,7 +15,6 @@
 #include "EffectUI.h"
 
 #include "widgets/BasicMenu.h"
-#include "Effect.h"
 #include "EffectManager.h"
 #include "../PluginManager.h"
 #include "../ProjectHistory.h"
@@ -1169,10 +1168,18 @@ void EffectUIHost::OnCancel(wxCommandEvent & WXUNUSED(evt))
 
 void EffectUIHost::OnHelp(wxCommandEvent & WXUNUSED(event))
 {
-   if (mEffect.GetDefinition().GetFamily() == NYQUISTEFFECTS_FAMILY && (mEffect.GetDefinition().ManualPage().empty())) {
+   if (mEffect.GetDefinition().ManualPage().empty()) {
+      // No manual page, so use help page
+
+      const auto &helpPage = mEffect.GetDefinition().HelpPage();
+      // It was guaranteed in Initialize() that there is no help
+      // button when neither manual nor help page is specified, so this
+      // event handler it not reachable in that case.
+      wxASSERT(!helpPage.empty());
+
       // Old ShowHelp required when there is no on-line manual.
       // Always use default web browser to allow full-featured HTML pages.
-      HelpSystem::ShowHelp(FindWindow(wxID_HELP), mEffect.GetDefinition().HelpPage(), wxEmptyString, true, true);
+      HelpSystem::ShowHelp(FindWindow(wxID_HELP), helpPage, wxEmptyString, true, true);
    }
    else {
       // otherwise use the NEW ShowHelp

tenacityteam/tenacity

(cherry picked from commit 0d63dc1f8808789249d298e52d7f411dc9bc86d7)
Signed-off-by: Avery King <avery98@pm.me>

src/effects/EffectUI.cpp
@@ -14,7 +14,7 @@
 
 #include "EffectUI.h"
 
-#include "Effect.h"
+#include "ConfigInterface.h"
 #include "EffectManager.h"
 #include "../PluginManager.h"
 #include "../ProjectHistory.h"
@@ -592,10 +592,18 @@ void EffectUIHost::OnCancel(wxCommandEvent & /* evt */)
 
 void EffectUIHost::OnHelp(wxCommandEvent & /* event */)
 {
-   if (mEffectUIHost.GetDefinition().GetFamily() == NYQUISTEFFECTS_FAMILY && (mEffectUIHost.GetDefinition().ManualPage().empty())) {
+   if (mEffectUIHost.GetDefinition().ManualPage().empty()) {
+      // No manual page, so use help page
+
+      const auto &helpPage = mEffectUIHost.GetDefinition().HelpPage();
+      // It was guaranteed in Initialize() that there is no help
+      // button when neither manual nor help page is specified, so this
+      // event handler it not reachable in that case.
+      wxASSERT(!helpPage.empty());
+
       // Old ShowHelp required when there is no on-line manual.
       // Always use default web browser to allow full-featured HTML pages.
-      HelpSystem::ShowHelp(FindWindow(wxID_HELP), mEffectUIHost.GetDefinition().HelpPage(), wxEmptyString, true);
+      HelpSystem::ShowHelp(FindWindow(wxID_HELP), helpPage, wxEmptyString, true);
    }
    else {
       // otherwise use the NEW ShowHelp