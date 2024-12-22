7c332504ff904a5b9fedbf0a47a4ee16c81a968e
Avery King <avery98@pm.me>
2024-12-22 15:10:28 -0800

    Fix ExportOptionsDialog help button
    The help button now officially works. Also in this commit I simplified
    the button layout code :D
    
    Signed-off-by: Avery King <avery98@pm.me>

--- src/export/ExportDialog.cpp
+++ src/export/ExportDialog.cpp
@@ -20,6 +20,7 @@
 #include "../shuttle/ShuttleGui.h"
 #include "../theme/AllThemeResources.h"
 #include "../theme/Theme.h"
+#include "../widgets/HelpSystem.h"
 
 //// ExportOptionsDialog //////////////////////////////////////////////////////
 
@@ -32,6 +33,11 @@ ExportOptionsDialog::ExportOptionsDialog(ExportPluginArray& plugins)
 mPlugins(plugins),
 mBook(nullptr)
 {
+    // Events
+    Bind(wxEVT_BUTTON, &ExportOptionsDialog::OnOK, this, wxID_OK);
+    Bind(wxEVT_BUTTON, &ExportOptionsDialog::OnCancel, this, wxID_CANCEL);
+    Bind(wxEVT_BUTTON, &ExportOptionsDialog::OnHelp, this, wxID_HELP);
+
     Populate();
 }
 
@@ -62,21 +68,7 @@ void ExportOptionsDialog::Populate()
         }
         S.EndHorizontalLay();
 
-        S.StartHorizontalLay(wxALIGN_RIGHT);
-        {
-            auto okButton     = S.AddButton(XO("OK"));
-            auto cancelButton = S.AddButton(XO("Cancel"));
-            auto helpButton   = new wxBitmapButton(S.GetParent(), wxID_HELP, theTheme.Bitmap( bmpHelpIcon ));
-
-            okButton->Bind(wxEVT_BUTTON, &ExportOptionsDialog::OnOK, this);
-            cancelButton->Bind(wxEVT_BUTTON, &ExportOptionsDialog::OnCancel, this);
-
-            helpButton->SetToolTip( XO("Help").Translation() );
-            helpButton->SetLabel(XO("Help").Translation()); // for screen readers
-            // S.Position(wxALIGN_TOP).AddWindow(helpButton);
-            S.AddWindow(helpButton);
-        }
-        S.EndHorizontalLay();
+        S.AddStandardButtons(eOkButton | eCancelButton | eHelpButton);
     }
     S.EndStatic();
 }
@@ -89,6 +81,11 @@ void ExportOptionsDialog::OnOK(wxCommandEvent&)
 void ExportOptionsDialog::OnCancel(wxCommandEvent&)
 {
     EndModal(wxID_CANCEL);
+}
+
+void ExportOptionsDialog::OnHelp(wxCommandEvent&)
+{
+    HelpSystem::ShowHelp(this, L"Importing_and_Exporting", true);
 }
 
 void ExportOptionsDialog::SetIndex(int index)
