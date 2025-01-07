220f6420a773f8ef91dd8d9ff43fd28e04a07a43
Avery King <avery98@pm.me>
2025-01-07 14:07:49 -0800

    Update theme resource list
    Signed-off-by: Avery King <gperson@disroot.org>

--- libraries/lib-theme/ThemeResourceList.h
+++ libraries/lib-theme/ThemeResourceList.h
@@ -15,11 +15,32 @@
 #include <list>
 #include <string>
 
-/** @brief A list of theme resource names known to Tenacity.
+/** @brief A list of image theme resource names known to Tenacity.
  * 
- * This list does _not_ define the only supported resource names in Tenacity.
- * Other resources known by different names can be loaded by Tenacity. Rather,
- * this list serves as a list of known theme resources.
+ * This list does _not_ define the only supported image resource names in
+ * Tenacity. Other resources known by different names can be loaded by
+ * Tenacity. Rather, this list serves as an optimization so Tenacity doesn't
+ * load every single theme resource from a theme package.
+ * 
+ * Also note the type of this list. We keep expected image sizes here because
+ * 
  * 
 */
-extern const std::list<std::string> KnownThemeResourceNames;
+extern const std::list<
+    std::tuple<
+        std::string, // Image name
+        size_t,      // Expected image width
+        size_t       // Expected image height
+    >
+> KnownImageResourceNames;
+
+/** @brief A list of color theme resource names known to Tenacity.
+ * 
+ * This list does _not_ define the only support color resource names in
+ * Tenacity. Other resources known by different names can be loaded by
+ * Tenacity. Rather, this list serves as an optimization so Tenacity doesn't
+ * load every single theme resource from a theme package.
+ * 
+ */
+extern const std::list<std::string> KnownColorResourceNames;
+
