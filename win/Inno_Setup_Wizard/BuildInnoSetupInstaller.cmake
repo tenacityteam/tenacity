875b726544df447838250e3b4e4a9f0f3d681c23
Avery King <gperson@disroot.org>
2025-10-09 10:39:58 -0700

    Inno Setup: Fix incorrect file name
    If TENACITY_ARCH_LABEL is set, building the package will fail. Don't set
    the installation prefix on the package name.
    
    Also add a TODO note to get rid of TENACITY_ARCH_LABEL. Eventually we'll
    try to make our CI environment closer to the build instructions by
    getting rid of certain environment variables, simplifying steps, etc.
    
    Signed-off-by: Avery King <gperson@disroot.org>

--- win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake
+++ win/Inno_Setup_Wizard/BuildInnoSetupInstaller.cmake
@@ -45,7 +45,7 @@ else()
 endif()
 
 # Set the packages we're going to build
-set(CPACK_EXTERNAL_BUILT_PACKAGES "${OUTPUT_DIR}/Output/${CPACK_PACKAGE_FILE_NAME}-${INSTALLER_SUFFIX}.exe")
+set(CPACK_EXTERNAL_BUILT_PACKAGES "${OUTPUT_DIR}/Output/${CPACK_PACKAGE_FILE_NAME}.exe")
 
 if( CPACK_TENACITY_INNO_SETUP_SIGN )
     set( SIGN_TOOL "SignTool=byparam powershell -ExecutionPolicy Bypass -File \$q${CPACK_TENACITY_SOURCE_DIR}/scripts/build/windows/PfxSign.ps1\$q -File $f")
