audacity/audacity

First simple demo implementation of Spectrogram Wavelet analysis

src/tracks/playabletrack/wavetrack/ui/SpectrumCache.cpp
@@ -72,6 +72,11 @@ void ComputeSpectrogramGainFactors
 
 }
 
+bool SpecCache::isWaveletAnalysis(const SpectrogramSettings& settings) const
+{
+    return settings.algorithm == SpectrogramSettings::algWavelet;
+}
+
 bool SpecCache::Matches(
    int dirty_, double samplesPerPixel,
    const SpectrogramSettings& settings) const
@@ -81,6 +86,15 @@ bool SpecCache::Matches(
    // a sample period.
    const bool sppMatch = (fabs(samplesPerPixel - spp) * len < 1.0);
 
+   if (isWaveletAnalysis(settings))
+   {
+       return
+          sppMatch &&
+          dirty == dirty_ &&
+          frequencyGain == settings.frequencyGain &&
+          algorithm == settings.algorithm &&
+          windowSize == settings.WindowSize();
+   }
    return
       sppMatch &&
       dirty == dirty_ &&
@@ -315,11 +329,136 @@ bool SpecCache::CalculateOneSpectrum(
    return result;
 }
 
+bool SpecCache::CalculateOneWaveletSpectrum(
+   const SpectrogramSettings& settings, const WaveChannelInterval& clip,
+   const int xx, double pixelsPerSecond, int lowerBoundX, int upperBoundX,
+   const std::vector<float>& gainFactors,
+   float* __restrict out) const
+{
+   bool result = false;
+   sampleCount from;
+
+   const auto numSamples = clip.GetSequence().GetNumSamples();
+   const auto sampleRate = clip.GetRate();
+   const auto stretchRatio = clip.GetStretchRatio();
+   const auto samplesPerPixel = sampleRate / pixelsPerSecond / stretchRatio;
+
+   // not reassignment, xx is surely within bounds.
+   wxASSERT(xx >= 0);
+   if (xx > (int)len)
+      from = sampleCount(where[len].as_double() + (xx - len) * samplesPerPixel);
+   else
+      from = where[xx];
+
+   size_t nBins = settings.NBins();
+
+   if (from < 0 || from >= numSamples) {
+      if (xx >= 0 && xx < (int)len) {
+         // Pixel column is out of bounds of the clip!  Should not happen.
+         float *const results = &out[nBins * xx];
+         std::fill(results, results + nBins, 0.0f);
+      }
+   }
+   else {
+      // Iterate frequencies in turn
+      // Note that as opposed to Fourier Transform, using wavelets implies a window length that varies with frequency
+      // We extract samples corresponding to max wavelet length (the first)
+      
+      // Locate longest wavelet (first one) 
+      auto longestWaveletLen = settings.waveletMaxLength;
+      wxASSERT(longestWaveletLen);
+       
+      auto myLen = 2 * longestWaveletLen - 1;
+      std::vector<float> scratch(myLen);
+
+      // Take a window of the track centered at this sample.
+      from -= myLen >> 1;
+      float * adj = &scratch[0];
+      if (from < 0) {
+         // Near the start of the clip, pad left with zeroes as needed.
+         for (auto ii = from; ii < 0; ++ii)
+             *adj++ = 0;
+         myLen += from.as_long_long(); // add a negative
+         from = 0;
+      }
+      if (from + myLen >= numSamples) {
+         // Near the end of the clip, pad right with zeroes as needed.
+         // newlen is bounded by myLen:
+         auto newlen = ( numSamples - from ).as_size_t();
+         for (decltype(myLen) ii = newlen; ii < myLen; ++ii)
+             adj[ii] = 0;
+         myLen = newlen;
+      }
+      std::vector<float> floats;
+      if (myLen > 0) {
+         constexpr auto iChannel = 0u;
+         constexpr auto mayThrow = false; // Don't throw just for display
+         mSampleCacheHolder.emplace(
+             clip.GetSampleView(from, myLen, mayThrow));
+         floats.resize(myLen);
+         mSampleCacheHolder->Copy(floats.data(), myLen);
+         float *useBuffer = floats.data();
+         memcpy(adj, useBuffer, myLen * sizeof(float));
+      }
+
+      // Done preparing samples. Now onward with calculations
+      float * middleSample = &scratch[longestWaveletLen - 1];
+
+      float *const results = &out[nBins * xx];
+      for (size_t iBin = 0; iBin < nBins; iBin++)
+      {
+          if (settings.waveletSizes[iBin] == 0)
+          {
+             results[iBin] = -160.0;
+             continue;
+          }
+         
+          float *right = middleSample;
+          float *left = middleSample;
+          // Do the convolution
+          float * wRe = &settings.waveletsRe[iBin][0];
+          float * wIm = &settings.waveletsIm[iBin][0];
+          double sumRe = *middleSample * *wRe;
+          double sumIm = 0.0;
+          wRe++, wIm++, right++, left--;
+          auto halfLen = settings.waveletSizes[iBin];;
+          for (auto ii = 1; ii < halfLen; ii++)
+          {
+              sumRe +=  (*right + *left) * *wRe;
+              sumIm +=  (*left - *right) * *wIm;
+              wRe++, wIm++, right++, left--;
+          }
+
+          // Calculate magnitude
+          float power = sumRe * sumRe + sumIm * sumIm;
+          
+
+          // Do the log thing
+          if (power <= 0)
+             power = -160.0;
+          else
+             power = 10.0*log10f(power);
+
+          results[iBin] = power;
+      }
+
+
+       if (!gainFactors.empty()) {
+          // Apply a frequency-dependent gain factor
+          for (size_t ii = 0; ii < nBins; ++ii)
+             results[ii] += gainFactors[ii];
+       }
+       result = true;
+   }
+
+   return result;
+}
+
 void SpecCache::Grow(
    size_t len_, SpectrogramSettings& settings, double samplesPerPixel,
-   double start_)
+   double start_, double sampleFrequency)
 {
-   settings.CacheWindows();
+   settings.CacheWindows(sampleFrequency);
 
    // len columns, and so many rows, column-major.
    // Don't take column literally -- this isn't pixel data yet, it's the
@@ -458,6 +597,33 @@ void SpecCache::Populate(
    }
 }
 
+void SpecCache::PopulateWavelet(
+   const SpectrogramSettings& settings, const WaveChannelInterval& clip,
+   int copyBegin, int copyEnd, size_t numPixels, double pixelsPerSecond)
+{
+   const auto sampleRate = clip.GetRate();
+   const int &frequencyGainSetting = settings.frequencyGain;
+
+   const auto nBins = settings.NBins();
+
+   std::vector<float> gainFactors;
+   ComputeSpectrogramGainFactors(nBins * 2, sampleRate, frequencyGainSetting, gainFactors);
+
+   // Loop over the ranges before and after the copied portion and compute anew.
+   // One of the ranges may be empty.
+   for (int jj = 0; jj < 2; ++jj) {
+      const int lowerBoundX = jj == 0 ? 0 : copyEnd;
+      const int upperBoundX = jj == 0 ? copyBegin : (int)numPixels;
+
+      for (auto xx = lowerBoundX; xx < upperBoundX; ++xx)
+      {
+         CalculateOneWaveletSpectrum(
+            settings, clip, xx, pixelsPerSecond, lowerBoundX, upperBoundX,
+            gainFactors, &freq[0]);
+      }
+   }
+}
+
 bool WaveClipSpectrumCache::GetSpectrogram(
    const WaveChannelInterval &clip,
    const float*& spectrogram, SpectrogramSettings& settings,
@@ -535,7 +701,7 @@ bool WaveClipSpectrumCache::GetSpectrogram(
    }
 
    // Resize the cache, keep the contents unchanged.
-   mSpecCache->Grow(numPixels, settings, samplesPerPixel, t0);
+   mSpecCache->Grow(numPixels, settings, samplesPerPixel, t0, sampleRate);
 
    // Reassignment accumulates, so it needs a zeroed buffer
    if (settings.algorithm == SpectrogramSettings::algReassignment)
@@ -562,9 +728,14 @@ bool WaveClipSpectrumCache::GetSpectrogram(
       mSpecCache->where, numPixels, addBias, correction, t0, sampleRate,
       stretchRatio, samplesPerPixel);
 
-   mSpecCache->Populate(
-      settings, clip, copyBegin, copyEnd, numPixels, pixelsPerSecond);
-
+   if (mSpecCache->isWaveletAnalysis(settings))
+   {
+       mSpecCache->PopulateWavelet(settings, clip, copyBegin, copyEnd, numPixels, pixelsPerSecond);
+   }
+   else
+   {
+       mSpecCache->Populate(settings, clip, copyBegin, copyEnd, numPixels, pixelsPerSecond);
+   }
    mSpecCache->dirty = mDirty;
    spectrogram = &mSpecCache->freq[0];
    where = &mSpecCache->where[0];

tenacityteam/tenacity

Some modifications made for Tenacity to account for our Spectrogram
changes also.
(cherry picked from commit ff3e195365d6ff6a526c2567c97c48aeaabc4fc6)
Co-authored-by: Klaus Gram-Hansen <klaus.gramhansen@gmail.com>
Co-authored-by: Matthieu Hodgkinson
Signed-off-by: Avery King <gperson@disroot.org>

src/tracks/playabletrack/wavetrack/ui/SpectrumCache.cpp
@@ -74,6 +74,11 @@ void ComputeSpectrogramGainFactors
 
 }
 
+bool SpecCache::isWaveletAnalysis(const SpectrogramSettings& settings) const
+{
+    return settings.algorithm == SpectrogramSettings::algWavelet;
+}
+
 bool SpecCache::Matches(
    int dirty_, double samplesPerPixel,
    const SpectrogramSettings& settings,
@@ -84,6 +89,15 @@ bool SpecCache::Matches(
    // a sample period.
    const bool sppMatch = (fabs(samplesPerPixel - spp) * len < 1.0);
 
+   if (isWaveletAnalysis(settings))
+   {
+       return
+          sppMatch &&
+          mDirty == dirty_ &&
+          frequencyGain == settings.frequencyGain &&
+          algorithm == settings.algorithm &&
+          windowSize == settings.WindowSize();
+   }
    return
       sppMatch &&
       mDirty == dirty_ &&
@@ -314,6 +328,129 @@ bool SpecCache::CalculateOneSpectrum(
    return result;
 }
 
+bool SpecCache::CalculateOneWaveletSpectrum(
+   const SpectrogramSettings& settings, const WaveChannelInterval& clip,
+   const int xx, double pixelsPerSecond, int lowerBoundX, int upperBoundX,
+   const std::vector<float>& gainFactors,
+   float* __restrict out) const
+{
+   bool result = false;
+   sampleCount from;
+
+   const auto numSamples = clip.GetSequence().GetNumSamples();
+   const auto sampleRate = clip.GetRate();
+   const auto stretchRatio = clip.GetStretchRatio();
+   const auto samplesPerPixel = sampleRate / pixelsPerSecond / stretchRatio;
+   const size_t nBins = settings.NBins();
+   float *const results = &out[nBins * xx];
+
+   // not reassignment, xx is surely within bounds.
+   wxASSERT(xx >= 0);
+   if (xx > (int)len)
+      from = sampleCount(where[len].as_double() + (xx - len) * samplesPerPixel);
+   else
+      from = where[xx];
+
+   if (from < 0 || from >= numSamples) {
+      if (xx >= 0 && xx < (int)len) {
+         // Pixel column is out of bounds of the clip!  Should not happen.
+         std::fill(results, results + nBins, 0.0f);
+      }
+   }
+   else
+   {
+      // Iterate frequencies in turn
+      // Note that as opposed to Fourier Transform, using wavelets implies a window length that varies with frequency
+      // We extract samples corresponding to max wavelet length (the first)
+    
+      // Locate longest wavelet (first one)
+      const auto longestWaveletLen = settings.waveletMaxLength;
+      wxASSERT(longestWaveletLen);
+       
+      auto myLen = 2 * longestWaveletLen - 1;
+      std::vector<float> scratch(myLen);
+
+      // Take a window of the track centered at this sample.
+      from -= longestWaveletLen;
+      float * adj = &scratch[0];
+      if (from < 0) {
+         // Near the start of the clip, pad left with zeroes as needed.
+         for (auto ii = from; ii < 0; ++ii)
+             *adj++ = 0;
+         myLen += from.as_long_long(); // add a negative
+         from = 0;
+      }
+      if (from + myLen >= numSamples) {
+         // Near the end of the clip, pad right with zeroes as needed.
+         // newlen is bounded by myLen:
+         auto newlen = ( numSamples - from ).as_size_t();
+         for (decltype(myLen) ii = newlen; ii < myLen; ++ii)
+             adj[ii] = 0;
+         myLen = newlen;
+      }
+      std::vector<float> floats;
+      if (myLen > 0) {
+         constexpr auto iChannel = 0u;
+         constexpr auto mayThrow = false; // Don't throw just for display
+         mSampleCacheHolder.emplace(
+             clip.GetSampleView(from, myLen, mayThrow));
+         floats.resize(myLen);
+         mSampleCacheHolder->Copy(floats.data(), myLen);
+         float *useBuffer = floats.data();
+         memcpy(adj, useBuffer, myLen * sizeof(float));
+      }
+
+      // Done preparing samples. Now onward with calculations
+      const float * middleSample = &scratch[longestWaveletLen - 1];
+      for (size_t iBin = 0; iBin < nBins; iBin++)
+      {
+          if (settings.waveletSizes[iBin] == 0)
+          {
+             results[iBin] = -160.0;
+             continue;
+          }
+         
+          const float *right = middleSample;
+          const float *left = middleSample;
+          // Do the convolution
+          const float * wRe = &settings.waveletsRe[iBin][0];
+          const float * wIm = &settings.waveletsIm[iBin][0];
+          double sumRe = *middleSample * *wRe;
+          double sumIm = 0.0;
+          wRe++, wIm++, right++, left--;
+          const auto halfLen = settings.waveletSizes[iBin];
+          for (auto ii = 1; ii < halfLen; ii++)
+          {
+              sumRe +=  (*right + *left) * *wRe;
+              sumIm +=  (*left - *right) * *wIm;
+              wRe++, wIm++, right++, left--;
+          }
+
+          // Calculate magnitude
+          float power = sumRe * sumRe + sumIm * sumIm;
+          
+          
+          // Do the log thing
+          if (power <= 0)
+             power = -160.0;
+          else
+             power = 10.0*log10f(power);
+
+          results[iBin] = power;
+      }
+
+
+       if (!gainFactors.empty()) {
+          // Apply a frequency-dependent gain factor
+          for (size_t ii = 0; ii < nBins; ++ii)
+             results[ii] += gainFactors[ii];
+       }
+       result = true;
+   }
+
+   return result;
+}
+
 void SpecCache::Resize(
    size_t len_, SpectrogramSettings& settings, double samplesPerPixel,
    sampleCount start_, double correction, const WaveChannelInterval& clip)
@@ -376,11 +513,18 @@ void SpecCache::Populate(
 
       for (auto xx = lowerBoundX; xx < upperBoundX; ++xx)
       {
-         float* buffer = &scratch[0];
+         if (isWaveletAnalysis(settings))
+         {
+             CalculateOneWaveletSpectrum(settings, clip, xx, pixelsPerSecond, lowerBoundX, upperBoundX,
+                                         gainFactors, &freq[0]);
+         } else
+         {
+            float* buffer = &scratch[0];
 
-         CalculateOneSpectrum(
-            settings, clip, xx, pixelsPerSecond, lowerBoundX, upperBoundX,
-            gainFactors, buffer, &freq[0]);
+            CalculateOneSpectrum(
+               settings, clip, xx, pixelsPerSecond, lowerBoundX, upperBoundX,
+               gainFactors, buffer, &freq[0]);
+         }
       }
 
       if (reassignment) {