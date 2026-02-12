a539cbf744dedd320c787adbaa6c4f12424d00e2
Avery King <gperson@disroot.org>
2026-02-11 21:48:09 -0800

    Theme: Remove dependency on ThemePackage
    This means there's no way to load a theme from a theme package, but this
    will be figured out in the next commit.
    
    Signed-off-by: Avery King <gperson@disroot.org>

--- libraries/lib-theme/Theme.cpp
+++ libraries/lib-theme/Theme.cpp
@@ -12,15 +12,9 @@
 
 #include "Theme.h"
 #include "exceptions/ArchiveError.h"
-#include "exceptions/InvalidState.h"
 using namespace ThemeExceptions;
 
 #include <utility>
-
-Theme::Theme(ThemePackage&& package)
-{
-    mPackage = std::forward<ThemePackage>(package);
-}
 
 Theme& Theme::operator=(const Theme& other)
 {
@@ -31,7 +25,6 @@ Theme& Theme::operator=(const Theme& other)
 Theme& Theme::operator=(Theme&& other)
 {
     mResources = std::move(other.mResources);
-    mPackage = std::move(other.mPackage);
     return *this;
 }
 
@@ -50,62 +43,14 @@ std::string Theme::GetName()
     return mName;
 }
 
-void Theme::SetPackage(ThemePackage&& package)
-{
-    mPackage = std::forward<ThemePackage>(package);
-}
-
-const ThemePackage& Theme::GetPackage()
-{
-    if (!mPackage)
-    {
-        throw InvalidState();
-    }
-
-    return mPackage.value();
-}
-
-void Theme::LoadAttributesFromPackage()
-{
-    if (!mPackage)
-    {
-        throw InvalidState();
-    }
-
-    // Load theme name
-    const auto& value = mPackage->GetAttribute("name");
-    mName = value.GetString();
-}
-
-ThemePackage&& Theme::ReleasePackage()
-{
-    return std::move(mPackage.value());
-}
-
 const std::any& Theme::GetResource(const std::string& name)
 {
     // First, search our map.
     auto exists = mResources.find(name);
     if (exists == mResources.end())
     {
-        // The resource doesn't exist in our resource map, so load it from the
-        // backing package if it exists.
-        if (mPackage)
-        {
-            ThemePackage& package = mPackage.value();
-            try
-            {
-                auto resource = package.LoadResource(name);
-                mResources[name] = resource;
-            } catch(ArchiveError& ae)
-            {
-                // Rethrow the exception.
-                throw;
-            }
-        } else
-        {
-            throw ArchiveError(ArchiveError::Type::ResourceNotFound);
-        }
+        // TODO: Find better exception to throw than ArchiveError
+        throw ArchiveError(ArchiveError::Type::ResourceNotFound);
     }
 
     // At this point, we should've added the resource to our resource map, so
