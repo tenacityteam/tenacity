ea1777a49cd052dae954c304c5c766354b43b535
Dr. Juan Gabriel Colonna <juancolonna@gmail.com>
2026-05-03 15:35:28 +1200

    ProjectFileManager: remove unused newRate in AddImportedTracks
    Cleanup: remove dead newRate code from AddImportedTracks
    (cherry picked from commit 7a022219e3c2bb4e92d8b78a4b11d23b86835a2b)

--- src/ProjectFileManager.cpp
+++ src/ProjectFileManager.cpp
@@ -1240,7 +1240,6 @@ ProjectFileManager::AddImportedTracks(const FilePath &fileName,
    wxFileName fn(fileName);
 
    bool initiallyEmpty = tracks.empty();
-   double newRate = 0;
    wxString trackNameBase = fn.GetName();
    int i = -1;
 
@@ -1286,8 +1285,6 @@ ProjectFileManager::AddImportedTracks(const FilePath &fileName,
          newTrack->SetName(trackNameBase);
 
       newTrack->TypeSwitch([&](WaveTrack &wt) {
-         if (newRate == 0)
-            newRate = wt.GetRate();
          const auto trackName = wt.GetName();
          for (const auto &interval : wt.Intervals())
             interval->SetName(trackName);
