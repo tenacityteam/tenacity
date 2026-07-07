ba2ce24629a556f01124a47047c8a791f642e637
Avery King <gperson@disroot.org>
2026-07-06 22:29:27 -0700

    Theme: Add includes in Types.h
    Make clang-tidy happy for now.
    
    Signed-off-by: Avery King <gperson@disroot.org>

--- libraries/lib-theme/Types.h
+++ libraries/lib-theme/Types.h
@@ -12,6 +12,11 @@
 
 #pragma once
 
+#include <any>
+#include <list>
+#include <string>
+#include <unordered_map>
+
 using ThemeResourceMap  = std::unordered_map<std::string, std::any>;
 using ThemeResourceList = std::list<std::string, std::any>;
 
